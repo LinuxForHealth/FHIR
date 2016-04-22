e375bea18cd3c510e97cd60f93da292c580019b8
Harini Jagannathan <hjagann@us.ibm.com>
2016-04-22 13:58:26 -0400

    issue  161843 - clean up unused RestAssured code in FHIRServerTestBase and move some common modules to fhir-core [ID = dc1fff679f5da0988f610919605fd343a5e3fbbd]

--- fhir-core/src/main/java/com/ibm/watsonhealth/fhir/core/FHIRUtilities.java
+++ fhir-core/src/main/java/com/ibm/watsonhealth/fhir/core/FHIRUtilities.java
@@ -6,10 +6,17 @@
 
 package com.ibm.watsonhealth.fhir.core;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * A collection of miscellaneous utility functions used by the various fhir-* projects.
  */
 public class FHIRUtilities {
+	
+	protected static final String NL = System.getProperty("line.separator");
     
     public static String getObjectHandle(Object o) {
         return Integer.toHexString(System.identityHashCode(o));
@@ -28,5 +35,54 @@ public class FHIRUtilities {
         }
 
         return sb.toString();
+    }
+    
+    // Read data for a resource from a control document (JSON/XML file)
+    public static String readFromFile(String filePath) {
+        try {
+            StringBuffer buffer = new StringBuffer();
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                		FHIRUtilities.class.getClassLoader().getResourceAsStream(filePath)
+                )
+            );
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                buffer.append(line);
+                buffer.append(NL);
+            }
+            in.close();
+            return buffer.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static String stripNamespaceIfPresentInDiv(String str) {
+        int startIndex = str.indexOf("<div xmlns=");
+        if (startIndex != -1) {
+            int endIndex = str.indexOf(">", startIndex);
+            String subString = str.substring(startIndex, endIndex + 1);
+            return str.replace(subString, "<div>");
+        }
+        return str;
+    }
+    
+    public static String stripNewLineWhitespaceIfPresentInDiv(String str) {
+        int startIndex = str.indexOf("<div>");
+        if (startIndex != -1) {
+            int endIndex = str.indexOf("</div>", startIndex);
+            if(endIndex != -1) {
+            	String divContent = str.substring(startIndex+5, endIndex);
+                String divWithoutNewLine = divContent.replace("\\n", "");
+                String strNewLineStrippedInDiv = str.replace(divContent, divWithoutNewLine);
+                String divWithoutNewLineWhiteSpace = divWithoutNewLine.replace(" ", "");
+                return strNewLineStrippedInDiv.replace(divWithoutNewLine, divWithoutNewLineWhiteSpace);
+            }
+        }
+        return str;
     }
 }
