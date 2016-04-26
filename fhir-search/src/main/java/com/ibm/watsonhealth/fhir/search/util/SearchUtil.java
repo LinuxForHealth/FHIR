0bb7db2d8106606ea004ec0379615c20f0b9da3d
John Timm <johntimm@us.ibm.com>
2016-04-26 15:27:15 -0400

    issue  158311 - basic support for making extensions searchable. [ID = 39c284d27446f8b7b0d96fd9860302a5dd663a0f]

--- fhir-search/src/main/java/com/ibm/watsonhealth/fhir/search/util/SearchUtil.java
+++ fhir-search/src/main/java/com/ibm/watsonhealth/fhir/search/util/SearchUtil.java
@@ -15,6 +15,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.logging.Logger;
 
 import javax.xml.bind.Binder;
 import javax.xml.bind.JAXBException;
@@ -45,7 +46,9 @@ import com.ibm.watsonhealth.fhir.search.ParameterValue;
 import com.ibm.watsonhealth.fhir.search.ParameterValue.Prefix;
 
 public class SearchUtil {
-	private static final Map<String, Map<String, SearchParameter>> searchParameterMap = buildSearchParameterMap();
+    private static final Logger log = Logger.getLogger(SearchUtil.class.getName());
+    
+    private static final Map<String, Map<String, SearchParameter>> searchParameterMap = buildSearchParameterMap();
 	private static final XPath xpath = createXPath();
 	private static final Map<String, XPathExpression> expressionMap = new HashMap<String, XPathExpression>();
 	private static final DatatypeFactory datatypeFactory = createDatatypeFactory();
@@ -106,27 +109,36 @@ public class SearchUtil {
 	private static Map<String, Map<String, SearchParameter>> buildSearchParameterMap() {
 		Map<String, Map<String, SearchParameter>> searchParameterMap = new HashMap<String, Map<String, SearchParameter>>();
 		try {
-			InputStream stream = SearchUtil.class.getClassLoader().getResourceAsStream("search-parameters.xml");
-			Bundle bundle = FHIRUtil.read(Bundle.class, Format.XML, stream);
-			for (BundleEntry entry : bundle.getEntry()) {
-				ResourceContainer container = entry.getResource();
-				SearchParameter parameter = container.getSearchParameter();
-				if (parameter != null) {
-					String base = parameter.getBase().getValue();
-					Map<String, SearchParameter> map = searchParameterMap.get(base);
-					if (map == null) {
-						map = new TreeMap<String, SearchParameter>();
-						searchParameterMap.put(base, map);
-					}
-					String name = parameter.getName().getValue();
-					map.put(name, parameter);
-				}
-			}
-			return searchParameterMap;
+            populateSearchParameterMap(searchParameterMap, "search-parameters.xml");
 		} catch (JAXBException e) {
 			throw new Error(e);
 		}
+        try {
+            populateSearchParameterMap(searchParameterMap, "extension-search-parameters.xml");
+        } catch (Exception e) {
+            log.fine("An error occured loading FHIR extension search parameters.");
+        }
+		return searchParameterMap;
 	}
+	
+	private static void populateSearchParameterMap(Map<String, Map<String, SearchParameter>> searchParameterMap, String searchParameterFile) throws JAXBException {
+        InputStream stream = SearchUtil.class.getClassLoader().getResourceAsStream(searchParameterFile);
+        Bundle bundle = FHIRUtil.read(Bundle.class, Format.XML, stream);
+        for (BundleEntry entry : bundle.getEntry()) {
+            ResourceContainer container = entry.getResource();
+            SearchParameter parameter = container.getSearchParameter();
+            if (parameter != null) {
+                String base = parameter.getBase().getValue();
+                Map<String, SearchParameter> map = searchParameterMap.get(base);
+                if (map == null) {
+                    map = new TreeMap<String, SearchParameter>();
+                    searchParameterMap.put(base, map);
+                }
+                String name = parameter.getName().getValue();
+                map.put(name, parameter);
+            }
+        }
+    }
 	
 	public static List<SearchParameter> getSearchParameters(Class<? extends Resource> resourceType) {
 		Map<String, SearchParameter> map = searchParameterMap.get(resourceType.getSimpleName());
@@ -190,113 +202,117 @@ public class SearchUtil {
 		List<Parameter> parameters = new ArrayList<Parameter>();
 		
 		for (String name : queryParameters.keySet()) {
-			// parse name into parameter name
-			String parameterName = name;
-			Modifier modifier = null;
-			String modifierResourceTypeName = null;
-			if (parameterName.contains(":")) {
-				String mod = parameterName.substring(parameterName.indexOf(":") + 1);
-				if (FHIRUtil.isValidResourceTypeName(mod)) {
-					modifier = Modifier.TYPE;
-					modifierResourceTypeName = mod;
-				} else {
-					modifier = Modifier.fromValue(mod);
-				}
-				parameterName = parameterName.substring(0, parameterName.indexOf(":"));
-			}
-			
-			// get the definition for this search parameter based on resource type and parameter name
-			SearchParameter searchParameter = parameterName.startsWith("_") ? 
-					SearchUtil.getSearchParameter(Resource.class, parameterName) : 
-					SearchUtil.getSearchParameter(resourceType, parameterName);
-					
-			// get the type of parameter so that we can use it to parse the value
-			Type type = Type.fromValue(searchParameter.getType().getValue());
-			
-			// parse values
-			for (String value : queryParameters.get(name)) {
-				Parameter parameter = new Parameter(type, parameterName, modifier, modifierResourceTypeName);
-				parameters.add(parameter);
-				for (String v : value.split(",")) {
-					ParameterValue parameterValue = new ParameterValue();
-					Prefix prefix = null;
-					switch (type) {
-					case DATE: {
-						// date
-						// [parameter]=[prefix][value]
-						prefix = getPrefix(v);
-						if (prefix != null) {
-							v = v.substring(2);
-							parameterValue.setPrefix(prefix);
-						}
-						parameterValue.setValueDate(datatypeFactory.newXMLGregorianCalendar(v));
-						break;
-					}
-					case NUMBER: {
-						// number
-						// [parameter]=[prefix][value]
-						prefix = getPrefix(v);
-						if (prefix != null) {
-							v = v.substring(2);
-							parameterValue.setPrefix(prefix);
-						}
-						parameterValue.setValueNumber(Double.parseDouble(v));
-						break;
-					}
-					case REFERENCE: {
-						// reference
-						// [parameter]=[url]
-						// [parameter]=[type]/[id]
-						// [parameter]=[id]
-						parameterValue.setValueString(v);
-						break;
-					}
-					case QUANTITY: {
-						// quantity
-						// [parameter]=[prefix][number]|[system]|[code]
-						prefix = getPrefix(v);
-						if (prefix != null) {
-							v = v.substring(2);
-							parameterValue.setPrefix(prefix);
-						}
-						String[] parts = v.split("|");
-						String number = parts[0];
-						parameterValue.setValueNumber(Double.parseDouble(number));
-						String system = parts[1];	// could be empty string
-						parameterValue.setValueSystem(system);
-						String code = parts[2];
-						parameterValue.setValueCode(code);
-						break;
-					}
-					case STRING: {
-						// string
-						// [parameter]=[value]
-						parameterValue.setValueString(v);
-						break;
-					}
-					case TOKEN: {
-						// token
-						// [parameter]=[system]|[code]
-						String[] parts = v.split("\\|");
-						if (parts.length == 2) {
-							parameterValue.setValueSystem(parts[0]);
-							parameterValue.setValueCode(parts[1]);
-						} else {
-							parameterValue.setValueCode(v);
-						}
-						break;
-					}
-					case URI: {
-						// [parameter]=[value]
-						parameterValue.setValueString(v);
-						break;
-					}
-					default:
-						break;
-					}
-					parameter.getValues().add(parameterValue);
-				}
-			}
+		    try {
+	            // parse name into parameter name
+	            String parameterName = name;
+	            Modifier modifier = null;
+	            String modifierResourceTypeName = null;
+	            if (parameterName.contains(":")) {
+	                String mod = parameterName.substring(parameterName.indexOf(":") + 1);
+	                if (FHIRUtil.isValidResourceTypeName(mod)) {
+	                    modifier = Modifier.TYPE;
+	                    modifierResourceTypeName = mod;
+	                } else {
+	                    modifier = Modifier.fromValue(mod);
+	                }
+	                parameterName = parameterName.substring(0, parameterName.indexOf(":"));
+	            }
+	            
+	            // get the definition for this search parameter based on resource type and parameter name
+	            SearchParameter searchParameter = parameterName.startsWith("_") ? 
+	                    SearchUtil.getSearchParameter(Resource.class, parameterName) : 
+	                    SearchUtil.getSearchParameter(resourceType, parameterName);
+	                    
+	            // get the type of parameter so that we can use it to parse the value
+	            Type type = Type.fromValue(searchParameter.getType().getValue());
+	            
+	            // parse values
+	            for (String value : queryParameters.get(name)) {
+	                Parameter parameter = new Parameter(type, parameterName, modifier, modifierResourceTypeName);
+	                parameters.add(parameter);
+	                for (String v : value.split(",")) {
+	                    ParameterValue parameterValue = new ParameterValue();
+	                    Prefix prefix = null;
+	                    switch (type) {
+	                    case DATE: {
+	                        // date
+	                        // [parameter]=[prefix][value]
+	                        prefix = getPrefix(v);
+	                        if (prefix != null) {
+	                            v = v.substring(2);
+	                            parameterValue.setPrefix(prefix);
+	                        }
+	                        parameterValue.setValueDate(datatypeFactory.newXMLGregorianCalendar(v));
+	                        break;
+	                    }
+	                    case NUMBER: {
+	                        // number
+	                        // [parameter]=[prefix][value]
+	                        prefix = getPrefix(v);
+	                        if (prefix != null) {
+	                            v = v.substring(2);
+	                            parameterValue.setPrefix(prefix);
+	                        }
+	                        parameterValue.setValueNumber(Double.parseDouble(v));
+	                        break;
+	                    }
+	                    case REFERENCE: {
+	                        // reference
+	                        // [parameter]=[url]
+	                        // [parameter]=[type]/[id]
+	                        // [parameter]=[id]
+	                        parameterValue.setValueString(v);
+	                        break;
+	                    }
+	                    case QUANTITY: {
+	                        // quantity
+	                        // [parameter]=[prefix][number]|[system]|[code]
+	                        prefix = getPrefix(v);
+	                        if (prefix != null) {
+	                            v = v.substring(2);
+	                            parameterValue.setPrefix(prefix);
+	                        }
+	                        String[] parts = v.split("|");
+	                        String number = parts[0];
+	                        parameterValue.setValueNumber(Double.parseDouble(number));
+	                        String system = parts[1];   // could be empty string
+	                        parameterValue.setValueSystem(system);
+	                        String code = parts[2];
+	                        parameterValue.setValueCode(code);
+	                        break;
+	                    }
+	                    case STRING: {
+	                        // string
+	                        // [parameter]=[value]
+	                        parameterValue.setValueString(v);
+	                        break;
+	                    }
+	                    case TOKEN: {
+	                        // token
+	                        // [parameter]=[system]|[code]
+	                        String[] parts = v.split("\\|");
+	                        if (parts.length == 2) {
+	                            parameterValue.setValueSystem(parts[0]);
+	                            parameterValue.setValueCode(parts[1]);
+	                        } else {
+	                            parameterValue.setValueCode(v);
+	                        }
+	                        break;
+	                    }
+	                    case URI: {
+	                        // [parameter]=[value]
+	                        parameterValue.setValueString(v);
+	                        break;
+	                    }
+	                    default:
+	                        break;
+	                    }
+	                    parameter.getValues().add(parameterValue);
+	                }
+	            }
+		    } catch (Exception e) {
+		        log.fine("Unable to parse query parameter named: " + name);
+		    }
 		}
 		
 		return parameters;
