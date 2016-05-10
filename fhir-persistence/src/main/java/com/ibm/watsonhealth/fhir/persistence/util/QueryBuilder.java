88a8c58ee0ae937f7e2af72d3829985336ef2296
Mark Discenza <mjdisc@us.ibm.com>
2016-05-10 14:54:07 -0400

    issue  166011 - Add throws clause to QueryBuilder.buildQuery() method. [ID = 9eef8f5b226ee78a0e107d85c8503238cd62f851]

--- fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/util/QueryBuilder.java
+++ fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/util/QueryBuilder.java
@@ -9,8 +9,21 @@ package com.ibm.watsonhealth.fhir.persistence.util;
 import java.util.List;
 
 import com.ibm.watsonhealth.fhir.model.Resource;
+import com.ibm.watsonhealth.fhir.persistence.exception.FHIRPersistenceException;
 import com.ibm.watsonhealth.fhir.search.Parameter;
 
+/**
+ * Defines methods for for building persistence layer queries.
+ * @param <T>
+ */
 public interface QueryBuilder<T> {
-    T buildQuery(Class<? extends Resource> resourceType, List<Parameter> searchParameters);
+	
+	/**
+	 * Build and return query for the passed resource type and search parameters.
+	 * @param resourceType - A FHIR Resource subclass.
+	 * @param searchParameters - A List of Parameters to be used for constructing the query.
+	 * @return T - An instance of <T> representing the constructed query.
+	 * @throws FHIRPersistenceException - Thrown for any non-recoverable failure that occurs during query construction.
+	 */
+    T buildQuery(Class<? extends Resource> resourceType, List<Parameter> searchParameters) throws FHIRPersistenceException;
 }
