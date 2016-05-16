ada4d248a17b5b16b08276307ed18bb501dd2113
Phil Adams <phil_adams@us.ibm.com>
2016-05-16 10:49:12 -0400

    Hide main classes from junit [ID = f0fc1c3a46ad35ca4e0af1bda397ae1e7c712269]

---
+++ fhir-client/src/test/java/com/ibm/watsonhealth/fhir/client/test/mains/Main.java
@@ -0,0 +1,32 @@
+/**
+ * (C) Copyright IBM Corp. 2016,2017,2019
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.watsonhealth.fhir.client.test.mains;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import com.ibm.watsonhealth.fhir.core.MediaType;
+import com.ibm.watsonhealth.fhir.model.Patient;
+import com.ibm.watsonhealth.fhir.model.util.FHIRUtil;
+import com.ibm.watsonhealth.fhir.model.util.FHIRUtil.Format;
+import com.ibm.watsonhealth.fhir.provider.FHIRProvider;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Client client = ClientBuilder.newBuilder()
+		        .register(new FHIRProvider())
+		        .build();
+		WebTarget target = client.target("http://fhirtest.uhn.ca/baseDstu2");
+		Response response = target.path("Patient/5149").request(MediaType.APPLICATION_JSON_FHIR).get();
+		Patient patient = response.readEntity(Patient.class);
+		FHIRUtil.write(patient, Format.JSON, System.out);
+		System.out.println("");
+		FHIRUtil.write(patient, Format.XML, System.out);
+	}
+}
---
+++ fhir-client/src/test/java/com/ibm/watsonhealth/fhir/client/test/mains/UpdateTestMain.java
@@ -0,0 +1,42 @@
+/**
+ * (C) Copyright IBM Corp. 2016,2017,2019
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.watsonhealth.fhir.client.test.mains;
+
+import static com.ibm.watsonhealth.fhir.model.util.FHIRUtil.quantity;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import com.ibm.watsonhealth.fhir.core.MediaType;
+import com.ibm.watsonhealth.fhir.model.Observation;
+import com.ibm.watsonhealth.fhir.model.util.FHIRUtil;
+import com.ibm.watsonhealth.fhir.model.util.FHIRUtil.Format;
+import com.ibm.watsonhealth.fhir.provider.FHIRProvider;
+
+public class UpdateTestMain {
+	public static void main(String[] args) throws Exception {
+		Client client = ClientBuilder.newBuilder()
+		        .register(new FHIRProvider())
+		        .build();
+		
+		WebTarget target = client.target("http://localhost:9080/fhir-server/api");
+		Response response = target.path("Observation/14").request(MediaType.APPLICATION_JSON_FHIR).get();
+		
+		Observation observation = response.readEntity(Observation.class);
+		FHIRUtil.write(observation, Format.JSON, System.out);
+		
+		observation.getComponent().get(0).setValueQuantity(quantity(120.0, "mmHg"));
+		
+		Entity<Observation> observationEntity = Entity.entity(observation, MediaType.APPLICATION_JSON_FHIR);
+		response = target.path("Observation/14").request().put(observationEntity);
+		System.out.println(response.getStatusInfo().getReasonPhrase());
+		System.out.println("location: " + response.getLocation());
+	}
+}
