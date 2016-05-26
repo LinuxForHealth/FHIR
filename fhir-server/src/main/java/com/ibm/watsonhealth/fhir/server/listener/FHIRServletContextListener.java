5e15a469f0c0adabeff7267ecf54e5ae96a165ea
Phil Adams <phil_adams@us.ibm.com>
2016-05-26 17:59:54 -0400

    issue  171149 - Make websocket notifications configurable; re-orgd server test groups [ID = 7dca48d0181fdc0ec2ccbf95a7ac88d3b410a5dd]

--- fhir-server/src/main/java/com/ibm/watsonhealth/fhir/server/listener/FHIRServletContextListener.java
+++ fhir-server/src/main/java/com/ibm/watsonhealth/fhir/server/listener/FHIRServletContextListener.java
@@ -9,17 +9,23 @@ package com.ibm.watsonhealth.fhir.server.listener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.naming.InitialContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.websocket.server.ServerContainer;
 
 import com.ibm.watsonhealth.fhir.persistence.interceptor.impl.FHIRPersistenceInterceptorMgr;
+import com.ibm.watsonhealth.fhir.server.notification.endpoint.FHIRNotificationServiceEndpointConfig;
 import com.ibm.watsonhealth.fhir.persistence.helper.FHIRPersistenceHelper;
 import com.ibm.watsonhealth.fhir.validation.Validator;
 
 @WebListener
 public class FHIRServletContextListener implements ServletContextListener {
 	private static final Logger log = Logger.getLogger(FHIRServletContextListener.class.getName());
+	
+	private static final String ATTRNAME_WEBSOCKET_SERVERCONTAINER = "javax.websocket.server.ServerContainer";
+	private static final String JNDINAME_WEBSOCKET_ENABLED = "com.ibm.watsonhealth.fhir.notification.websocket.enabled";
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
@@ -41,6 +47,19 @@ public class FHIRServletContextListener implements ServletContextListener {
             // Set the shared interceptor mgr.
             event.getServletContext().setAttribute(FHIRPersistenceInterceptorMgr.class.getName(), FHIRPersistenceInterceptorMgr.getInstance());
             log.fine("Set shared persistence interceptor mgr on servlet context.");
+            
+            // Grab the "websocket.enabled" jndi value.
+            Boolean websocketEnabled = getBooleanJNDIValue(JNDINAME_WEBSOCKET_ENABLED, Boolean.FALSE);
+            
+            // If configured, start up our WebSocket endpoint for notifications.
+            if (websocketEnabled) {
+                log.info("Opening websocket for notifications.");
+                ServerContainer container = (ServerContainer) event.getServletContext().getAttribute(ATTRNAME_WEBSOCKET_SERVERCONTAINER);
+                container.addEndpoint(new FHIRNotificationServiceEndpointConfig());
+            } else {
+                log.info("Bypassing websocket notification initialization.");
+            }
+
 		} catch (Exception e) {
 			// ignore exceptions here
 		} finally {
@@ -64,4 +83,26 @@ public class FHIRServletContextListener implements ServletContextListener {
 			}
 		}
 	}
+
+    /**
+     * Retrieves the specified JNDI entry and interprets it as a Boolean value.
+     * @param jndiName the name of the JNDI entry to search for
+     * @param defaultValue the defaultValue to be returned if the JNDI entry isn't found
+     */
+    private Boolean getBooleanJNDIValue(String jndiName, Boolean defaultValue) {
+        Boolean result = defaultValue;
+        try {
+            InitialContext ctx = new InitialContext();
+            Boolean jndiValue = (Boolean) ctx.lookup(jndiName);
+            if (jndiValue != null ) {
+                log.fine("JNDI entry " + jndiName + "=" + jndiValue);
+                result = jndiValue;
+            }
+        } catch (Throwable t) {
+            // Ignore any exceptions while looking up the JNDI entry.
+            log.fine("Caught exception while looking up JNDI entry " + jndiName + ": " + t);
+        }
+        return result;
+    }
+    
 }
