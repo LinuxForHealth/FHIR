3b84bc55ad83d33133a2529a33eb5eee7756e398
Harini Jagannathan <hjagann@us.ibm.com>
2016-06-14 19:52:31 -0400

    issue  174404 - fix build break - set default value to SINCE in history pagination [ID = 07a4a0e2e852a976dbf7b5312af5127e439bf531]

--- fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/context/impl/FHIRHistoryContextImpl.java
+++ fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/context/impl/FHIRHistoryContextImpl.java
@@ -6,12 +6,14 @@
 
 package com.ibm.watsonhealth.fhir.persistence.context.impl;
 
+import static com.ibm.watsonhealth.fhir.model.util.FHIRUtil.instant;
+
 import com.ibm.watsonhealth.fhir.core.context.impl.FHIRPagingContextImpl;
 import com.ibm.watsonhealth.fhir.model.Instant;
 import com.ibm.watsonhealth.fhir.persistence.context.FHIRHistoryContext;
 
 public class FHIRHistoryContextImpl extends FHIRPagingContextImpl implements FHIRHistoryContext {
-    private Instant since = null;
+    private Instant since = instant("1970-01-01T00:00:00Z");
     
     public FHIRHistoryContextImpl() {
     }
