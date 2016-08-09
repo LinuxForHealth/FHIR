ba341aa134dfb571f0be2f7fcede076ff4d1e9b0
Harini Jagannathan <hjagann@us.ibm.com>
2016-08-09 19:39:08 -0400

    issue  190269 - add testcases for new interceptor methods [ID = 5e8281cf0f5fda13ea2891d968dd746ef71ac3de]

--- fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/test/InterceptorTest.java
+++ fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/test/InterceptorTest.java
@@ -6,8 +6,12 @@
 
 package com.ibm.watsonhealth.fhir.test;
 
+import static org.testng.AssertJUnit.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -59,6 +63,110 @@ public class InterceptorTest extends FHIRModelTestBase {
         mgr.fireAfterCreateEvent(event);
         
         assertEquals(4, MyInterceptor.getAfterCreateCount());
+    }
+    
+    @Test
+    public void testBeforeRead() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireBeforeReadEvent(event);
+        
+        assertTrue(event.isStandardResourceType());
+        assertEquals(1, MyInterceptor.getBeforeReadCount());
+    }
+    
+    @Test
+    public void testAfterRead() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient1");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireAfterReadEvent(event);
+        mgr.fireAfterReadEvent(event);
+        mgr.fireAfterReadEvent(event);
+        mgr.fireAfterReadEvent(event);
+        
+        assertTrue(event.isStandardResourceType() == false);
+        assertEquals(4, MyInterceptor.getAfterReadCount());
+    }
+    
+    @Test
+    public void testBeforeVread() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireBeforeVreadEvent(event);
+        
+        assertTrue(event.isStandardResourceType());
+        assertEquals(1, MyInterceptor.getBeforeVreadCount());
+    }
+    
+    @Test
+    public void testAfterVread() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient1");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireAfterVreadEvent(event);
+        mgr.fireAfterVreadEvent(event);
+        
+        assertTrue(event.isStandardResourceType() == false);
+        assertEquals(2, MyInterceptor.getAfterVreadCount());
+    }
+    
+    @Test
+    public void testBeforeHistory() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireBeforeHistoryEvent(event);
+        
+        assertTrue(event.isStandardResourceType());
+        assertEquals(1, MyInterceptor.getBeforeHistoryCount());
+    }
+    
+    @Test
+    public void testAfterHistory() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient1");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireAfterHistoryEvent(event);
+        mgr.fireAfterHistoryEvent(event);
+        mgr.fireAfterHistoryEvent(event);
+        
+        assertTrue(event.isStandardResourceType() == false);
+        assertEquals(3, MyInterceptor.getAfterHistoryCount());
+    }
+    
+    @Test
+    public void testBeforeSearch() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireBeforeSearchEvent(event);
+        
+        assertTrue(event.isStandardResourceType());
+        assertEquals(1, MyInterceptor.getBeforeSearchCount());
+    }
+    
+    @Test
+    public void testAfterSearch() throws Exception {
+    	Map<String, Object> properties = new HashMap<String, Object>();
+    	properties.put(FHIRPersistenceEvent.PROPNAME_RESOURCE_TYPE, "Patient1");
+        FHIRPersistenceEvent event = new FHIRPersistenceEvent(patient, properties);
+        
+        mgr.fireAfterSearchEvent(event);
+        mgr.fireAfterSearchEvent(event);
+        mgr.fireAfterSearchEvent(event);
+        
+        assertTrue(event.isStandardResourceType() == false);
+        assertEquals(3, MyInterceptor.getAfterSearchCount());
     }
 
     @Test
--- fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/test/MyInterceptor.java
+++ fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/test/MyInterceptor.java
@@ -19,6 +19,14 @@ public class MyInterceptor implements FHIRPersistenceInterceptor {
     private static int afterCreateCount = 0;
     private static int beforeUpdateCount = 0;
     private static int afterUpdateCount = 0;
+    private static int beforeReadCount = 0;
+    private static int afterReadCount = 0;
+    private static int beforeVreadCount = 0;
+    private static int afterVreadCount = 0;
+    private static int beforeHistoryCount = 0;
+    private static int afterHistoryCount = 0;
+    private static int beforeSearchCount = 0;
+    private static int afterSearchCount = 0;
 
     public MyInterceptor() {
     }
@@ -66,6 +74,94 @@ public class MyInterceptor implements FHIRPersistenceInterceptor {
             }
         }
     }
+    
+    @Override
+    public void beforeRead(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        beforeReadCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+
+    @Override
+    public void afterRead(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        afterReadCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+    
+    @Override
+    public void beforeVread(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        beforeVreadCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+
+    @Override
+    public void afterVread(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        afterVreadCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+    
+    @Override
+    public void beforeHistory(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        beforeHistoryCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+
+    @Override
+    public void afterHistory(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        afterHistoryCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+    
+    @Override
+    public void beforeSearch(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        beforeSearchCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
+
+    @Override
+    public void afterSearch(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
+        afterSearchCount++;
+        if (event.getFhirResource() instanceof Patient) {
+            Patient patient = (Patient) event.getFhirResource();
+            if (patient.getName().get(0).getFamily().get(0).getValue().equals("Exception")) {
+                throw new FHIRPersistenceInterceptorException("Detected invalid patient");
+            }
+        }
+    }
 
     public static int getBeforeCreateCount() {
         return beforeCreateCount;
@@ -81,5 +177,37 @@ public class MyInterceptor implements FHIRPersistenceInterceptor {
 
     public static int getAfterUpdateCount() {
         return afterUpdateCount;
+    }
+    
+    public static int getBeforeReadCount() {
+        return beforeReadCount;
+    }
+
+    public static int getAfterReadCount() {
+        return afterReadCount;
+    }
+    
+    public static int getBeforeVreadCount() {
+        return beforeVreadCount;
+    }
+
+    public static int getAfterVreadCount() {
+        return afterVreadCount;
+    }
+    
+    public static int getBeforeHistoryCount() {
+        return beforeHistoryCount;
+    }
+
+    public static int getAfterHistoryCount() {
+        return afterHistoryCount;
+    }
+    
+    public static int getBeforeSearchCount() {
+        return beforeSearchCount;
+    }
+
+    public static int getAfterSearchCount() {
+        return afterSearchCount;
     }
 }
