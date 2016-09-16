3cd133fc36e28aa3b8102ca1420eb767f41038a8
Harini Jagannathan <hjagann@us.ibm.com>
2016-09-16 16:27:31 -0400

    issue  196794 - adding testcases for Patient compartment - RiskAssessment resource type [ID = 1422bf75e6c8ec5e32449583c2b93e5e23b29a94]

--- fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/persistence/test/common/AbstractQueryRiskAssmtTest.java
+++ fhir-persistence/src/test/java/com/ibm/watsonhealth/fhir/persistence/test/common/AbstractQueryRiskAssmtTest.java
@@ -67,6 +67,24 @@ public abstract class AbstractQueryRiskAssmtTest extends AbstractPersistenceTest
     }
     
     /**
+     * Tests the FHIRPersistenceCloudantImpl create API for a RiskAssessment.
+     * 
+     * @throws Exception
+     */
+    @Test(groups = { "cloudant", "jpa" })
+    public void testCreateRiskAssessment3() throws Exception {
+        RiskAssessment riskAssmt = readResource(RiskAssessment.class, "RiskAssessment_for_Patient.json");
+
+        persistence.create(getDefaultPersistenceContext(), riskAssmt);
+        assertNotNull(riskAssmt);
+        assertNotNull(riskAssmt.getId());
+        assertNotNull(riskAssmt.getId().getValue());
+        assertNotNull(riskAssmt.getMeta());
+        assertNotNull(riskAssmt.getMeta().getVersionId().getValue());
+        assertEquals("1", riskAssmt.getMeta().getVersionId().getValue());
+    }
+    
+    /**
 	 * Tests a query for a RiskAssessment with condition = 'Condition/stroke' which should yield correct results
 	 * @throws Exception
 	 */
@@ -163,5 +181,34 @@ public abstract class AbstractQueryRiskAssmtTest extends AbstractPersistenceTest
 		int lastPgNum = context.getLastPageNumber();
 		assertEquals(context.getLastPageNumber(), (int) ((count + pageSize - 1) / pageSize));
 		assertTrue((count > 10) ? (lastPgNum > 1) : (lastPgNum == 1));
+	}
+	
+	/*
+	 * 
+	 * Compartment search testcases
+	 * 
+	 */
+	
+	/**
+	 * Tests a query for a RiskAssessment with performer = 'Practitioner/f001' which should yield correct results
+	 * @throws Exception
+	 */
+	@Test(groups = { "jpa" }, dependsOnMethods = { "testCreateRiskAssessment3" })
+	public void testRiskAssessmentQuery_PatCompmt_1() throws Exception {
+		List<Resource> resources = runQueryTest("Patient", "example", RiskAssessment.class, persistence, "performer", "Practitioner/f001");
+		assertNotNull(resources);
+		assertTrue(resources.size() != 0);
+		assertEquals(((RiskAssessment)resources.get(0)).getPerformer().getReference().getValue(),"Practitioner/f001");
+	}
+	
+	/**
+	 * Tests a query for a RiskAssessment with date = "0010-11-22" which should yield no results
+	 * @throws Exception
+	 */
+	@Test(groups = { "jpa" }, dependsOnMethods = { "testCreateRiskAssessment3" })
+	public void testRiskAssessmentQuery_PatCompmt_2() throws Exception {
+		List<Resource> resources = runQueryTest("Patient", "example", RiskAssessment.class, persistence, "date", "0010-11-22");
+		assertNotNull(resources);
+		assertTrue(resources.size() == 0);
 	}
 }
