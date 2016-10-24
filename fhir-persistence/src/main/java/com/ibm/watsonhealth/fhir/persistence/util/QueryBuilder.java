8f69f2be040519b6832d92b1fff5e7dee0e96dd3
Mark Discenza <mjdisc@us.ibm.com>
2016-10-24 13:47:50 -0400

    issue  205919 - refactor QueryBuilder interface [ID = 1481365dfd2499523c41c5c210fb65666e6e3b51]

--- fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/util/QueryBuilder.java
+++ fhir-persistence/src/main/java/com/ibm/watsonhealth/fhir/persistence/util/QueryBuilder.java
@@ -6,11 +6,9 @@
 
 package com.ibm.watsonhealth.fhir.persistence.util;
 
-import java.util.List;
-
 import com.ibm.watsonhealth.fhir.model.Resource;
 import com.ibm.watsonhealth.fhir.persistence.exception.FHIRPersistenceException;
-import com.ibm.watsonhealth.fhir.search.Parameter;
+import com.ibm.watsonhealth.fhir.search.context.FHIRSearchContext;
 
 /**
  * Defines methods for for building persistence layer queries.
@@ -21,9 +19,9 @@ public interface QueryBuilder<T> {
 	/**
 	 * Build and return query for the passed resource type and search parameters.
 	 * @param resourceType - A FHIR Resource subclass.
-	 * @param searchParameters - A List of Parameters to be used for constructing the query.
+	 * @param searchParameters - A search context that contains a List of search parameters to be used for constructing the query.
 	 * @return T - An instance of <T> representing the constructed query.
 	 * @throws FHIRPersistenceException - Thrown for any non-recoverable failure that occurs during query construction.
 	 */
-    T buildQuery(Class<? extends Resource> resourceType, List<Parameter> searchParameters) throws FHIRPersistenceException;
+    T buildQuery(Class<? extends Resource> resourceType, FHIRSearchContext searchContext) throws FHIRPersistenceException;
 }
