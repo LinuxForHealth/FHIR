a806641eb057be410765e9d99dea5d91fa09b4b9
John Timm <johntimm@us.ibm.com>
2019-08-14 17:12:28 -0400

    Issue #356 - Fix FHIRValidateOperationTest [ID = ad86bbe286f80c5e7b37bbd294c5281a98a6218b]

--- fhir-server-test/src/test/java/com/ibm/watsonhealth/fhir/server/test/FHIRValidateOperationTest.java
+++ fhir-server-test/src/test/java/com/ibm/watsonhealth/fhir/server/test/FHIRValidateOperationTest.java
@@ -6,10 +6,11 @@
 
 package com.ibm.watsonhealth.fhir.server.test;
 
-import static com.ibm.watsonhealth.fhir.model.type.String.string;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
@@ -18,116 +19,68 @@ import org.testng.annotations.Test;
 
 import com.ibm.watsonhealth.fhir.core.MediaType;
 import com.ibm.watsonhealth.fhir.model.resource.OperationOutcome;
-import com.ibm.watsonhealth.fhir.model.resource.Patient;
-import com.ibm.watsonhealth.fhir.model.type.Canonical;
-import com.ibm.watsonhealth.fhir.model.type.ContactPoint;
-import com.ibm.watsonhealth.fhir.model.type.ContactPointSystem;
-import com.ibm.watsonhealth.fhir.model.type.ContactPointUse;
-import com.ibm.watsonhealth.fhir.model.type.Date;
-import com.ibm.watsonhealth.fhir.model.type.Extension;
-import com.ibm.watsonhealth.fhir.model.type.HumanName;
-import com.ibm.watsonhealth.fhir.model.type.Meta;
+import com.ibm.watsonhealth.fhir.model.type.IssueSeverity;
 
-public class FHIRValidateOperationTest extends FHIRServerTestBase {
+public class FHIRValidateOperationTest extends FHIRServerTestBase {    
     @Test(groups = { "validate-operation" })
     public void testValidatePatient() {
-        Patient patient = buildPatient();
+        JsonObject patient = buildPatient();
         WebTarget target = getWebTarget();
-        Entity<Patient> entity = Entity.entity(patient, MediaType.APPLICATION_FHIR_JSON);
+        Entity<JsonObject> entity = Entity.entity(patient, MediaType.APPLICATION_JSON);
+        
         Response response = target.path("Resource/$validate").request().post(entity, Response.class);
         assertResponse(response, Response.Status.OK.getStatusCode());
         OperationOutcome operationOutcome = response.readEntity(OperationOutcome.class);
-        String text = operationOutcome.getIssue().get(0).getDetails().getText().getValue();
-        assertEquals("All OK", text);
+        
+        assertEquals(operationOutcome.getIssue().size(), 1);
+        assertEquals(operationOutcome.getIssue().get(0).getSeverity(), IssueSeverity.WARNING);
+        assertTrue(operationOutcome.getIssue().get(0).getDetails().getText().getValue().startsWith("dom-6"));
     }
 
     @Test(groups = { "validate-operation" })
     public void testValidateInvalidPatient() {
-        Patient invalidPatient = buildInvalidPatient();
-
+        JsonObject patient = buildInvalidPatient();
         WebTarget target = getWebTarget();
-        Entity<Patient> entity = Entity.entity(invalidPatient, MediaType.APPLICATION_FHIR_JSON);
+        Entity<JsonObject> entity = Entity.entity(patient, MediaType.APPLICATION_JSON);
+        
         Response response = target.path("Resource/$validate").request().post(entity, Response.class);
         assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
         OperationOutcome operationOutcome = response.readEntity(OperationOutcome.class);
-
-        assertEquals(5, operationOutcome.getIssue().size());
-
-        assertTrue(operationOutcome.getIssue().get(0).getDiagnostics().getValue().startsWith("cvc-minLength-valid"));
-        assertTrue(operationOutcome.getIssue().get(1).getDiagnostics().getValue().startsWith("cvc-attribute.3"));
-        assertTrue(operationOutcome.getIssue().get(2).getDiagnostics().getValue().startsWith("cvc-pattern-valid"));
-        assertTrue(operationOutcome.getIssue().get(3).getDiagnostics().getValue().startsWith("cvc-attribute.3"));
-        assertTrue(operationOutcome.getIssue().get(4).getDiagnostics().getValue().startsWith("cpt-2"));
+        
+        assertEquals(operationOutcome.getIssue().size(), 2);
+        assertEquals(operationOutcome.getIssue().get(0).getSeverity(), IssueSeverity.WARNING);
+        assertTrue(operationOutcome.getIssue().get(0).getDetails().getText().getValue().startsWith("dom-6"));
+        assertEquals(operationOutcome.getIssue().get(1).getSeverity(), IssueSeverity.ERROR);
+        assertTrue(operationOutcome.getIssue().get(1).getDetails().getText().getValue().startsWith("cpt-2"));
     }
 
-    @Test(groups = { "validate-operation" })
-    public void testValidatePatientWithUserDefinedSchematron() {
-        Patient patient = buildPatient();
-
-        patient = patient.toBuilder()
-                .meta(Meta.builder().profile(Canonical.of("http://ibm.com/watsonhealth/fhir/profile/acme-sample"))
-                        .build())
-                .extension(Extension.builder().url("http://ibm.com/watsonhealth/fhir/extension/acme-sample/study_ID")
-                        .value(string("abc-1234")).build())
-                .build();
-
-        WebTarget target = getWebTarget();
-        Entity<Patient> entity = Entity.entity(patient, MediaType.APPLICATION_FHIR_JSON);
-        Response response = target.path("Resource/$validate").request().header("X-FHIR-TENANT-ID", "tenant1")
-                .post(entity, Response.class);
-        assertResponse(response, Response.Status.OK.getStatusCode());
-        OperationOutcome operationOutcome = response.readEntity(OperationOutcome.class);
-
-        String text = operationOutcome.getIssue().get(0).getDetails().getText().getValue();
-        assertEquals("All OK", text);
+    private JsonObject buildPatient() {
+        return Json.createObjectBuilder().add("resourceType", "Patient")
+            .add("name", Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                    .add("family", "Doe")
+                    .add("given", Json.createArrayBuilder().add("John"))))
+            .add("birthDate", "1950-08-15")
+            .add("telecom", Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                    .add("use", "home")
+                    .add("system", "phone")
+                    .add("value", "555-1234")))
+        .build();
     }
 
-    @Test(groups = { "validate-operation" })
-    public void testValidateInvalidPatientWithUserDefinedSchematron() {
-        Patient patient = buildPatient();
-
-        patient = patient.toBuilder().meta(
-                Meta.builder().profile(Canonical.of("http://ibm.com/watsonhealth/fhir/profile/acme-sample")).build())
-                .build();
-
-        // Perform validation using tenant1's user-defined rules (should fail
-        // validation).
-        WebTarget target = getWebTarget();
-        Entity<Patient> entity = Entity.entity(patient, MediaType.APPLICATION_FHIR_JSON);
-        Response response = target.path("Resource/$validate").request().header("X-FHIR-TENANT-ID", "tenant1")
-                .post(entity, Response.class);
-        assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
-        OperationOutcome operationOutcome = response.readEntity(OperationOutcome.class);
-        assertEquals(1, operationOutcome.getIssue().size());
-        assertTrue(operationOutcome.getIssue().get(0).getDiagnostics().getValue().startsWith("acme-sample-1"));
-
-        // Perform validation using default tenant's user-defined rules (should pass
-        // validation).
-        target = getWebTarget();
-        entity = Entity.entity(patient, MediaType.APPLICATION_FHIR_JSON);
-        response = target.path("Resource/$validate").request().header("X-FHIR-TENANT-ID", "default").post(entity,
-                Response.class);
-        assertResponse(response, Response.Status.OK.getStatusCode());
-        operationOutcome = response.readEntity(OperationOutcome.class);
-        assertEquals(1, operationOutcome.getIssue().size());
-        assertEquals("All OK", operationOutcome.getIssue().get(0).getDetails().getText().getValue());
-    }
-
-    private Patient buildPatient() {
-        // build patient object using fluent API
-        Patient patient = Patient.builder()
-                .name(HumanName.builder().family(string("Doe")).given(string("John")).build())
-                .birthDate(Date.of("1950-08-15")).telecom(ContactPoint.builder().system(ContactPointSystem.PHONE)
-                        .use(ContactPointUse.HOME).value(string("555-1234")).build())
-                .build();
-        return patient;
-    }
-
-    private Patient buildInvalidPatient() {
-        // build patient object using fluent API
-        Patient patient = Patient.builder().name(HumanName.builder().family(string("")).given(string("John")).build())
-                .birthDate(Date.of("19500815"))
-                .telecom(ContactPoint.builder().use(ContactPointUse.HOME).value(string("555-1234")).build()).build();
-        return patient;
+    private JsonObject buildInvalidPatient() {
+        return Json.createObjectBuilder().add("resourceType", "Patient")
+                .add("name", Json.createArrayBuilder()
+                    .add(Json.createObjectBuilder()
+                        .add("family", "Doe")
+                        .add("given", Json.createArrayBuilder().add("John"))))
+                .add("birthDate", "1950-08-15")
+                .add("telecom", Json.createArrayBuilder()
+                    .add(Json.createObjectBuilder()
+                        .add("use", "home")
+//                      .add("system", "phone")
+                        .add("value", "555-1234")))
+            .build();
     }
 }
