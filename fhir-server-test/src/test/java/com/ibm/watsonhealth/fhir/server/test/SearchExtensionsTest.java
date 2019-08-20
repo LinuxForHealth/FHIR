89447a4e7eeeb1d4908a2d902b6ddea01b0781c2
Paul Bastide <pbastide@us.ibm.com>
2019-08-20 09:25:50 -0400

    Missing extension-search-parameters.json in fhir-server #362 [ID = 61492da019d38773fcb3fc5ba266221bde495450]

--- fhir-server-test/src/test/java/com/ibm/watsonhealth/fhir/server/test/SearchExtensionsTest.java
+++ fhir-server-test/src/test/java/com/ibm/watsonhealth/fhir/server/test/SearchExtensionsTest.java
@@ -33,11 +33,7 @@ import com.ibm.watsonhealth.fhir.model.type.CodeableConcept;
 import com.ibm.watsonhealth.fhir.model.type.Integer;
 
 public class SearchExtensionsTest extends FHIRServerTestBase {
-    private static final String EXTENSION_BASE_URL =
-            "http://ibm.com/watsonhealth/fhir/extension/Patient/";
-
-    private static final boolean DEBUG = false;
-
+    private static final String EXTENSION_BASE_URL = "http://ibm.com/watsonhealth/fhir/extension/Patient/";
     private Patient savedCreatedPatientWithExtensions;
 
     @Test(groups = { "server-search" })
@@ -45,25 +41,32 @@ public class SearchExtensionsTest extends FHIRServerTestBase {
         WebTarget target = getWebTarget();
 
         Patient patient = readResource(Patient.class, "Patient_SearchExtensions.json");
-        patient = patient.toBuilder().extension(Extension.builder().url(EXTENSION_BASE_URL
-                + "favorite-color").value(string("blue")).build()).extension(Extension.builder().url(EXTENSION_BASE_URL
-                        + "favorite-number").value(Integer.of(5)).build()).extension(Extension.builder().url(EXTENSION_BASE_URL
-                                + "favorite-code").value(CodeableConcept.builder().coding(Coding.builder().code(Code.of("someCode-1234")).system(uri("http://ibm.com/fhir/system")).build()).build()).build()).extension(Extension.builder().url(EXTENSION_BASE_URL
-                                        + "favorite-uri").value(uri("http://www.ibm.com")).build()).extension(Extension.builder().url(EXTENSION_BASE_URL
-                                                + "favorite-quantity").value(Quantity.builder().unit(string("lbs")).value(Decimal.of(180)).build()).build()).extension(Extension.builder().url(EXTENSION_BASE_URL
-                                                        + "favorite-date").value(Date.of("2018-10-25")).build()).build();
+        patient = patient.toBuilder()
+                .extension(Extension.builder().url(EXTENSION_BASE_URL + "favorite-color").value(string("blue")).build())
+                .extension(Extension.builder().url(EXTENSION_BASE_URL + "favorite-number").value(Integer.of(5)).build())
+                .extension(Extension.builder().url(EXTENSION_BASE_URL + "favorite-code")
+                        .value(CodeableConcept.builder()
+                                .coding(Coding.builder().code(Code.of("someCode-1234"))
+                                        .system(uri("http://ibm.com/fhir/system")).build())
+                                .build())
+                        .build())
+                .extension(
+                        Extension.builder().url(EXTENSION_BASE_URL + "favorite-uri").value(uri("http://www.ibm.com")).build())
+                .extension(Extension.builder().url(EXTENSION_BASE_URL + "favorite-quantity")
+                        .value(Quantity.builder().unit(string("lbs")).value(Decimal.of(180)).build()).build())
+                .extension(Extension.builder().url(EXTENSION_BASE_URL + "favorite-date").value(Date.of("2018-10-25")).build())
+                .build();
 
         Entity<Patient> entity = Entity.entity(patient, MediaType.APPLICATION_FHIR_JSON);
-        Response response =
-                target.path("Patient").request().header("X-FHIR-TENANT-ID", "tenant1").post(entity, Response.class);
+        Response response = target.path("Patient").request().header("X-FHIR-TENANT-ID", "tenant1").post(entity,
+                Response.class);
         assertResponse(response, Response.Status.CREATED.getStatusCode());
 
         // Get the patient's logical id value.
         String patientId = getLocationLogicalId(response);
 
         // Next, call the 'read' API to retrieve the new patient and verify it.
-        response =
-                target.path("Patient/" + patientId).request(MediaType.APPLICATION_FHIR_JSON).get();
+        response = target.path("Patient/" + patientId).request(MediaType.APPLICATION_FHIR_JSON).get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         Patient responsePatient = response.readEntity(Patient.class);
         savedCreatedPatientWithExtensions = responsePatient;
@@ -74,16 +77,13 @@ public class SearchExtensionsTest extends FHIRServerTestBase {
     public void testSearchPatientWithExtensions() {
         WebTarget target = getWebTarget();
 
-        String favoriteColor =
-                ((com.ibm.watsonhealth.fhir.model.type.String) savedCreatedPatientWithExtensions.getExtension().get(0).getValue()).getValue();
-        Response response =
-                target.path("Patient").queryParam("favorite-color", favoriteColor).request(MediaType.APPLICATION_FHIR_JSON).header("X-FHIR-TENANT-ID", "tenant1").get();
-
+        String favoriteColor = ((com.ibm.watsonhealth.fhir.model.type.String) savedCreatedPatientWithExtensions
+                .getExtension().get(0).getValue()).getValue();
+        Response response = target.path("Patient").queryParam("favorite-color", favoriteColor)
+                .request(MediaType.APPLICATION_FHIR_JSON).header("X-FHIR-TENANT-ID", "tenant1").get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         Bundle bundle = response.readEntity(Bundle.class);
-        if (DEBUG) {
-            SearchAllTest.generateOutput(bundle);
-        }
+
         assertNotNull(bundle);
         assertTrue(bundle.getEntry().size() >= 1);
     }
@@ -92,15 +92,14 @@ public class SearchExtensionsTest extends FHIRServerTestBase {
     public void testSearchPatientWithExtensionsNotFound() throws FHIRGeneratorException {
         WebTarget target = getWebTarget();
 
-        // "red"
-        Response response =
-                target.path("Patient").queryParam("favorite-color", "red").request(MediaType.APPLICATION_FHIR_JSON).header("X-FHIR-TENANT-ID", "tenant1").get();
+        // "red" 
+        Response response = target.path("Patient").queryParam("favorite-color", "FUDGE_GUDGE")
+                .request(MediaType.APPLICATION_FHIR_JSON).header("X-FHIR-TENANT-ID", "tenant1").get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         Bundle bundle = response.readEntity(Bundle.class);
-
-        if (DEBUG) {
-            SearchAllTest.generateOutput(bundle);
-        }
+       
+        System.out.println("HERE HERE HERE");
+        SearchAllTest.generateOutput(bundle);
         assertNotNull(bundle);
         assertTrue(bundle.getEntry().size() == 0);
     }
@@ -110,43 +109,45 @@ public class SearchExtensionsTest extends FHIRServerTestBase {
         WebTarget target = getWebTarget();
 
         // in lenient mode, an unknown parameter should be ignored
-        Response response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=lenient").get();
+        Response response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=lenient").get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         Bundle bundle = response.readEntity(Bundle.class);
         assertNotNull(bundle);
 
         // lenient is the default, so leaving out the Prefer header should be equivalent
-        response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).get();
+        response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON).get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         bundle = response.readEntity(Bundle.class);
         assertNotNull(bundle);
 
         // lenient is the default, so a bogus value should be ignored
-        response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=other;strict").get();
+        response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=other;strict").get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         bundle = response.readEntity(Bundle.class);
         assertNotNull(bundle);
 
         // in strict mode, an unknown parameter should result in a 400 Bad Request
-        response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=strict").get();
+        response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "handling=strict").get();
         assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
         OperationOutcome oo = response.readEntity(OperationOutcome.class);
         assertNotNull(oo);
 
         // multiple headers should be handled appropriately
-        response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "fakeName=fakeValue").header("Prefer", "handling=strict").header("Prefer", "fakeName2=fakeValue2").get();
+        response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "fakeName=fakeValue")
+                .header("Prefer", "handling=strict").header("Prefer", "fakeName2=fakeValue2").get();
         assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
         oo = response.readEntity(OperationOutcome.class);
         assertNotNull(oo);
 
         // multiple parts to the Prefer header should be handled
-        response =
-                target.path("Patient").queryParam("fake-parameter", "fakeValue").request(MediaType.APPLICATION_FHIR_JSON).header("Prefer", "fakeName=fakeValue;handling=strict;fakeName2=fakeValue2").get();
+        response = target.path("Patient").queryParam("fake-parameter", "fakeValue")
+                .request(MediaType.APPLICATION_FHIR_JSON)
+                .header("Prefer", "fakeName=fakeValue;handling=strict;fakeName2=fakeValue2").get();
         assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
         oo = response.readEntity(OperationOutcome.class);
         assertNotNull(oo);
@@ -156,36 +157,29 @@ public class SearchExtensionsTest extends FHIRServerTestBase {
     public void testSearchPatientWithBaseParametersAndExtensions() {
         WebTarget target = getWebTarget();
         String family = savedCreatedPatientWithExtensions.getName().get(0).getFamily().getValue();
-        String favoriteColor =
-                ((com.ibm.watsonhealth.fhir.model.type.String) savedCreatedPatientWithExtensions.getExtension().get(0).getValue()).getValue();
-        Integer favoriteNumber =
-                (Integer) savedCreatedPatientWithExtensions.getExtension().get(1).getValue();
-        Coding favoriteCode =
-                ((CodeableConcept) savedCreatedPatientWithExtensions.getExtension().get(2).getValue()).getCoding().get(0);
+        String favoriteColor = ((com.ibm.watsonhealth.fhir.model.type.String) savedCreatedPatientWithExtensions
+                .getExtension().get(0).getValue()).getValue();
+        Integer favoriteNumber = (Integer) savedCreatedPatientWithExtensions.getExtension().get(1).getValue();
+        Coding favoriteCode = ((CodeableConcept) savedCreatedPatientWithExtensions.getExtension().get(2).getValue())
+                .getCoding().get(0);
         Uri favoriteUri = (Uri) savedCreatedPatientWithExtensions.getExtension().get(3).getValue();
-        Quantity favoriteQuantity =
-                (Quantity) savedCreatedPatientWithExtensions.getExtension().get(4).getValue();
-        Date favoriteDate =
-                (Date) savedCreatedPatientWithExtensions.getExtension().get(5).getValue();
+        Quantity favoriteQuantity = (Quantity) savedCreatedPatientWithExtensions.getExtension().get(4).getValue();
+        Date favoriteDate = (Date) savedCreatedPatientWithExtensions.getExtension().get(5).getValue();
 
-        /*
-         * Previously the favorite-code favorite-code=http://ibm.com/fhir/system%7CsomeCode-1234 using <code>
-         * favoriteCode.getSystem().getValue()+ "|"</code>
-         */
-
-        Response response =
-                target.path("Patient").queryParam("family", family).queryParam("favorite-color", favoriteColor).queryParam("favorite-number", favoriteNumber.getValue()).queryParam("favorite-code", favoriteCode.getCode().getValue()).queryParam("favorite-uri", favoriteUri.getValue()).queryParam("favorite-quantity", favoriteQuantity.getValue().getValue().toString()
-                        + "||"
-                        + favoriteQuantity.getUnit().getValue()).queryParam("favorite-date", favoriteDate.getValue()).request(MediaType.APPLICATION_FHIR_JSON).header("X-FHIR-TENANT-ID", "tenant1").get();
-
+        Response response = target.path("Patient").queryParam("family", family)
+                .queryParam("favorite-color", favoriteColor).queryParam("favorite-number", favoriteNumber)
+                .queryParam("favorite-code",
+                        favoriteCode.getSystem().getValue() + "|" + favoriteCode.getCode().getValue())
+                .queryParam("favorite-uri", favoriteUri.getValue())
+                .queryParam("favorite-quantity",
+                        favoriteQuantity.getValue().getValue().toString() + "||"
+                                + favoriteQuantity.getUnit().getValue())
+                .queryParam("favorite-date", favoriteDate.getValue()).request(MediaType.APPLICATION_FHIR_JSON)
+                .header("X-FHIR-TENANT-ID", "tenant1").get();
         assertResponse(response, Response.Status.OK.getStatusCode());
         Bundle bundle = response.readEntity(Bundle.class);
 
         assertNotNull(bundle);
-
-        if (DEBUG) {
-            SearchAllTest.generateOutput(bundle);
-        }
         assertTrue(bundle.getEntry().size() >= 1);
     }
 }
