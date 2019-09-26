8b949c3a78de79eb4e850a7975a1e7950108338e
Robin Arnold <robin.arnold23@ibm.com>
2019-09-26 10:09:14 -0400

    instrumenting R4 example tests for simple performance analysis
    Signed-off-by: Robin Arnold <robin.arnold23@ibm.com>

---
+++ fhir-server-test/src/test/java/com/ibm/fhir/server/test/app/TestContext.java
@@ -0,0 +1,17 @@
+/**
+ * (C) Copyright IBM Corp. 2019
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.fhir.server.test.app;
+
+import com.ibm.fhir.server.test.FHIRServerTestBase;
+
+/**
+ * @author rarnold
+ *
+ */
+public class TestContext extends FHIRServerTestBase {
+
+}
