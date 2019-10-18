001de8d846589a5793364d5ccfa6d271d3bf7775
Albert Wang <xuwang@us.ibm.com>
2019-10-18 10:26:44 -0400

    #169 added copyright header for the new file
    Signed-off-by: Albert Wang <xuwang@us.ibm.com>

--- fhir-search/src/main/java/com/ibm/fhir/search/SummaryValueSet.java
+++ fhir-search/src/main/java/com/ibm/fhir/search/SummaryValueSet.java
@@ -1,5 +1,17 @@
+/*
+ * (C) Copyright IBM Corp. 2019
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.ibm.fhir.search;
 
+/**
+ * Search _summary Constants
+ * 
+ * @author AlbertWang
+ *
+ */
 public enum SummaryValueSet {    
         TRUE("true"),
         TEXT("text"),
