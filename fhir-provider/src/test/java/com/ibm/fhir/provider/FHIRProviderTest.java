160b0e6d8b1d1614f75c77f2a96bd3b3b0dcbd3e
Paul Bastide <pbastide@us.ibm.com>
2019-11-26 16:38:23 -0500

    Support _pretty query parameter #269
    - Update conformance.md statements on _pretty
    - Update Provider to support Pretty from CDI injected URIInfo
    - Introduce new test for Pretty
    - Update SearchConstants to avoid processing _pretty as a Search
    Parameter
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-provider/src/test/java/com/ibm/fhir/provider/FHIRProviderTest.java
+++ fhir-provider/src/test/java/com/ibm/fhir/provider/FHIRProviderTest.java
@@ -6,41 +6,261 @@
 
 package com.ibm.fhir.provider;
 
-import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
 
-import com.ibm.fhir.config.FHIRConfiguration;
-import com.ibm.fhir.provider.FHIRProvider;
-
-import static org.testng.AssertJUnit.*;
-
-import javax.ws.rs.core.*;
+import java.net.URI;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * Created by rick.boyd on 11/19/18.
- */
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.testng.annotations.Test;
+
+import com.ibm.fhir.config.FHIRConfiguration;
 
 public class FHIRProviderTest {
+    
+    /*
+     * generate multivalued map
+     * @param value
+     * @return
+     */
+    public MultivaluedMap<String, String> generateMulivaluedMap(String value) {
+        return new MultivaluedMap<String, String>() {
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public List<String> get(Object key) {
+                return null;
+            }
+
+            @Override
+            public List<String> put(String key, List<String> value) {
+                return null;
+            }
+
+            @Override
+            public List<String> remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends String, ? extends List<String>> m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<String> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<List<String>> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<String, List<String>>> entrySet() {
+                return null;
+            }
+
+            @Override
+            public void putSingle(String key, String value) {
+
+            }
+
+            @Override
+            public void add(String key, String value) {
+
+            }
+
+            @Override
+            public String getFirst(String key) {
+                return value;
+            }
+
+            @Override
+            public void addAll(String key, String... newValues) {
+
+            }
+
+            @Override
+            public void addAll(String key, List<String> valueList) {
+
+            }
+
+            @Override
+            public void addFirst(String key, String value) {
+
+            }
+
+            @Override
+            public boolean equalsIgnoreValueOrder(MultivaluedMap<String, String> otherMap) {
+                return false;
+            }
+
+        };
+    }
+
+    /*
+     * generate uri info
+     */
+    public UriInfo generatePrettyParameterUriInfo(String value) {
+        return new UriInfo() {
+
+            @Override
+            public String getPath() {
+                return null;
+            }
+
+            @Override
+            public String getPath(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments() {
+                return null;
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public URI getRequestUri() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getRequestUriBuilder() {
+                return null;
+            }
+
+            @Override
+            public URI getAbsolutePath() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getAbsolutePathBuilder() {
+                return null;
+            }
+
+            @Override
+            public URI getBaseUri() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getBaseUriBuilder() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters() {
+                return generateMulivaluedMap(value);
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters(boolean decode) {
+                return generateMulivaluedMap(value);
+            }
+
+            @Override
+            public List<String> getMatchedURIs() {
+                return null;
+            }
+
+            @Override
+            public List<String> getMatchedURIs(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<Object> getMatchedResources() {
+
+                return null;
+            }
+
+            @Override
+            public URI resolve(URI uri) {
+                return null;
+            }
+
+            @Override
+            public URI relativize(URI uri) {
+                return null;
+            }
+
+        };
+    }
 
     @Test
     public void isPrettyDefaultsFalse() {
+
         HttpHeaders headers = createHeaders();
+
         // when empty
-        assertFalse(FHIRProvider.isPretty(headers));
+        assertFalse(FHIRProvider.isPretty(headers, generatePrettyParameterUriInfo("false")));
         // when populated but misspelled
         headers.getRequestHeaders().putSingle(FHIRConfiguration.DEFAULT_PRETTY_RESPONSE_HEADER_NAME, "nottrue");
-        assertFalse(FHIRProvider.isPretty(headers));
+        assertFalse(FHIRProvider.isPretty(headers, generatePrettyParameterUriInfo("false")));
     }
 
     @Test
     public void isPrettyTrueForTrue() {
         HttpHeaders headers = createHeaders();
         headers.getRequestHeaders().putSingle(FHIRConfiguration.DEFAULT_PRETTY_RESPONSE_HEADER_NAME, "true");
-        assertTrue(FHIRProvider.isPretty(headers));
+        assertTrue(FHIRProvider.isPretty(headers, generatePrettyParameterUriInfo("false")));
     }
 
     @Test
@@ -48,12 +268,13 @@ public class FHIRProviderTest {
         HttpHeaders headers = createHeaders();
         headers.getRequestHeaders().putSingle(FHIRConfiguration.DEFAULT_PRETTY_RESPONSE_HEADER_NAME, "false");
 
-        assertFalse(FHIRProvider.isPretty(headers));
+        assertFalse(FHIRProvider.isPretty(headers, generatePrettyParameterUriInfo("false")));
     }
 
     private static HttpHeaders createHeaders() {
         return new HttpHeaders() {
             private MultivaluedMap<String, String> headers = new MultivaluedHashMap<>();
+
             @Override
             public List<String> getRequestHeader(String s) {
                 return headers.get(s);
