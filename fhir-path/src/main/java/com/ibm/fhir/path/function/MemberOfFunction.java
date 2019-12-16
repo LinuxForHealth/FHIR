ec26d716beca34906a3b96f78e01c4c82f2926ce
John T.E. Timm <johntimm@us.ibm.com>
2019-12-16 11:48:25 -0500

    Issue #483 - 'memberOf' function refactor
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

--- fhir-path/src/main/java/com/ibm/fhir/path/function/MemberOfFunction.java
+++ fhir-path/src/main/java/com/ibm/fhir/path/function/MemberOfFunction.java
@@ -13,30 +13,39 @@ import static com.ibm.fhir.path.util.FHIRPathUtil.getElementNode;
 import static com.ibm.fhir.path.util.FHIRPathUtil.getString;
 import static com.ibm.fhir.path.util.FHIRPathUtil.hasElementNode;
 import static com.ibm.fhir.path.util.FHIRPathUtil.hasStringValue;
+import static com.ibm.fhir.profile.ValueSetSupport.expand;
+import static com.ibm.fhir.profile.ValueSetSupport.getContains;
+import static com.ibm.fhir.profile.ValueSetSupport.getValueSet;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.ibm.fhir.model.resource.CodeSystem;
 import com.ibm.fhir.model.resource.ValueSet;
-import com.ibm.fhir.model.resource.ValueSet.Compose;
-import com.ibm.fhir.model.resource.ValueSet.Compose.Include;
 import com.ibm.fhir.model.resource.ValueSet.Expansion;
 import com.ibm.fhir.model.resource.ValueSet.Expansion.Contains;
 import com.ibm.fhir.model.type.Code;
 import com.ibm.fhir.model.type.CodeableConcept;
 import com.ibm.fhir.model.type.Coding;
 import com.ibm.fhir.model.type.Element;
+import com.ibm.fhir.model.type.Uri;
 import com.ibm.fhir.path.FHIRPathElementNode;
 import com.ibm.fhir.path.FHIRPathNode;
+import com.ibm.fhir.path.FHIRPathType;
 import com.ibm.fhir.path.evaluator.FHIRPathEvaluator.EvaluationContext;
 import com.ibm.fhir.registry.FHIRRegistry;
 
 public class MemberOfFunction extends FHIRPathAbstractFunction {
-    private static final Map<String, Set<String>> VALUE_SET_CACHE = createLRUCache(1024);
+    private static final Logger log = Logger.getLogger(MemberOfFunction.class.getName());
+
+    private static final String VERSION_UNKNOWN = "<version unknown>";
+    private static final Map<String, Map<String, Set<String>>> CODE_SET_MAP_CACHE = createLRUCache(1024);
     
     @Override
     public String getName() {
@@ -61,6 +70,7 @@ public class MemberOfFunction extends FHIRPathAbstractFunction {
         
         FHIRPathElementNode elementNode = getElementNode(context);
         Element element = elementNode.element();
+        
         if (!isCodedElement(element)) {
             throw new IllegalArgumentException("The 'memberOf' function can only be invoked on a coded element");
         }
@@ -72,18 +82,20 @@ public class MemberOfFunction extends FHIRPathAbstractFunction {
         String url = getString(arguments.get(0));
                 
         if (FHIRRegistry.getInstance().hasResource(url)) {
-            Set<String> valueSet = getValueSet(url);
-            if (!valueSet.isEmpty()) {
+            Map<String, Set<String>> codeSetMap = getCodeSetMap(url);
+            if (!codeSetMap.isEmpty()) {
                 if (element.is(Code.class)) {
-                    Code code = element.as(Code.class);
-                    return valueSet.contains(code.getValue()) ? SINGLETON_TRUE : SINGLETON_FALSE;
+                    String system = getSystem(evaluationContext.getTree().getParent(elementNode));
+                    String version = FHIRRegistry.getInstance().getLatestVersion(system);
+                    String code = element.as(Code.class).getValue();
+                    return contains(codeSetMap, system, version, code) ? SINGLETON_TRUE : SINGLETON_FALSE;
                 } else if (element.is(Coding.class)) {
                     Coding coding = element.as(Coding.class);
-                    return valueSet.contains(coding.getCode().getValue()) ? SINGLETON_TRUE : SINGLETON_FALSE;
-                } else {
+                    return contains(codeSetMap, coding) ? SINGLETON_TRUE : SINGLETON_FALSE;
+                } else if (element.is(CodeableConcept.class)) {
                     CodeableConcept codeableConcept = element.as(CodeableConcept.class);
                     for (Coding coding : codeableConcept.getCoding()) {
-                        if (valueSet.contains(coding.getCode().getValue())) {
+                        if (contains(codeSetMap, coding)) {
                             return SINGLETON_TRUE;
                         }
                     }
@@ -95,65 +107,88 @@ public class MemberOfFunction extends FHIRPathAbstractFunction {
         return SINGLETON_TRUE;
     }
     
-    private Set<String> getValueSet(String url) {
-        return VALUE_SET_CACHE.computeIfAbsent(url, k -> computeValueSet(FHIRRegistry.getInstance().getResource(url,  ValueSet.class)));
+    private boolean contains(Map<String, Set<String>> codeSetMap, Coding coding) {
+        String system = (coding.getSystem() != null) ? coding.getSystem().getValue() : null;
+        String version = (coding.getVersion() != null) ? coding.getVersion().getValue() : FHIRRegistry.getInstance().getLatestVersion(system);
+        String code = (coding.getCode() != null) ? coding.getCode().getValue() : null;
+        return contains(codeSetMap, system, version, code);
     }
-    
-    private Set<String> computeValueSet(ValueSet valueSet) {
-        Set<String> result = new LinkedHashSet<>();
-        
-        Expansion expansion = valueSet.getExpansion();
-        if (expansion != null) {
-            for (Contains contains : expansion.getContains()) {
-                result.addAll(getValues(contains));
-            }
-        } else {
-            Compose compose = valueSet.getCompose();
-            if (compose != null) {
-                for (Include include : compose.getInclude()) {
-                    if (!include.getConcept().isEmpty()) {
-                        for (Include.Concept concept : include.getConcept()) {
-                            result.add(concept.getCode().getValue());
-                        }
-                    } else if (include.getSystem() != null){
-                        String system = include.getSystem().getValue();
-                        if (FHIRRegistry.getInstance().hasResource(system)) {
-                            CodeSystem codeSystem = FHIRRegistry.getInstance().getResource(system, CodeSystem.class);
-                            for (CodeSystem.Concept concept : codeSystem.getConcept()) {
-                                result.addAll(getValues(concept));
-                            }
-                        }
+
+    private boolean contains(Map<String, Set<String>> codeSetMap, String system, String version, String code) {
+        if (system != null && version != null) {
+            String key = system + "|" + version;
+            Set<String> codeSet = codeSetMap.get(key);
+            if (codeSet != null) {
+                if (codeSet.contains(code)) {
+                    return true;
+                } else {
+                    key = system + "|" + VERSION_UNKNOWN;
+                    codeSet = codeSetMap.get(key);
+                    if (codeSet != null) {
+                        return codeSet.contains(code);
                     }
                 }
             }
+        } else if (system != null) {
+            for (String key : codeSetMap.keySet()) {
+                if (key.startsWith(system)) {
+                    return codeSetMap.get(key).contains(code);
+                }
+            }
+        } else {
+            for (Set<String> codeSet : codeSetMap.values()) {
+                if (codeSet.contains(code)) {
+                    return true;
+                }
+            }
         }
-        
-        // TODO: add support for exclude
-        
-        return result;
+        return false;
     }
     
-    private Set<String> getValues(Contains contains) {
-        Set<String> values = new LinkedHashSet<>();
-        values.add(contains.getCode().getValue());
-        for (Contains c : contains.getContains()) {
-            values.addAll(getValues(c));
+    private String getSystem(FHIRPathNode node) {
+        if (node == null || !node.isElementNode()) {
+            return null;
         }
-        return values;
-    }
-
-    private Set<String> getValues(CodeSystem.Concept concept) {
-        Set<String> values = new LinkedHashSet<>();
-        values.add(concept.getCode().getValue());
-        for (CodeSystem.Concept c : concept.getConcept()) {
-            values.addAll(getValues(c));
+        for (FHIRPathNode child : node.children()) {
+            if ("system".equals(child.name()) && FHIRPathType.FHIR_URI.equals(node.type())) {
+                return child.asElementNode().element().as(Uri.class).getValue();
+            }
         }
-        return values;
+        return null;
     }
-
+    
+    private Map<String, Set<String>> getCodeSetMap(String url) {
+        return CODE_SET_MAP_CACHE.computeIfAbsent(url, k -> computeCodeSetMap(getValueSet(url)));
+    }
+    
     private boolean isCodedElement(Element element) {
         return (element instanceof Code) || 
                 (element instanceof Coding) || 
                 (element instanceof CodeableConcept);
+    }
+
+    private Map<String, Set<String>> computeCodeSetMap(ValueSet valueSet) {
+        try {
+            ValueSet expanded = expand(valueSet);
+            if (expanded == null || expanded.getExpansion() == null) {
+                return Collections.emptyMap();
+            }
+            Map<String, Set<String>> codeSetMap = new LinkedHashMap<>();
+            Expansion expansion = expanded.getExpansion();
+            for (Contains contains : getContains(expansion)) {
+                String system = (contains.getSystem() != null) ? contains.getSystem().getValue() : null;
+                String version = (contains.getVersion() != null) ? contains.getVersion().getValue() : VERSION_UNKNOWN;
+                String code = (contains.getCode() != null) ? contains.getCode().getValue() : null;
+                if (system != null && version != null && code != null) {
+                    codeSetMap.computeIfAbsent(system + "|" + version, k -> new LinkedHashSet<>()).add(code);
+                }
+            }
+            return codeSetMap;
+        } catch (Exception e) {
+            String url = (valueSet.getUrl() != null) ? valueSet.getUrl().getValue() : "<no url>";
+            String version = (valueSet.getVersion() != null) ? valueSet.getVersion().getValue() : "<no version>";
+            log.log(Level.WARNING, String.format("Unable to expand value set with url: %s and version: %s", url, version), e);
+        }
+        return Collections.emptyMap();
     }
 }
