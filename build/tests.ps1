4f3466d969fdd415eae1b6d59594835e32f8bab0
Paul Bastide <pbastide@us.ibm.com>
2020-02-28 13:56:09 -0500

    e2e windows tests report success even when e2e tests fail #698
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/tests.ps1
+++ build/tests.ps1
@@ -3,17 +3,8 @@
 # 
 # SPDX-License-Identifier: Apache-2.0
 ###############################################################################
-
+$ErrorActionPreference = 'Stop'
 Write-Host 'Performing Tests'
-& mvn -B test -DskipTests=false -f .\fhir-server-test\pom.xml -DskipWebSocketTest=true --log-file out.log --no-transfer-progress
-
-If ( (Get-Content out.log -Tail 10 | Select-String -pattern "BUILD SUCCESS").Length > 0) {
-    Write-Host 'Performing Tests - Success'
-    exit 0
-} else { 
-    Write-Host 'Performing Tests - Fail'
-    cat out.log
-    exit -1
-}
+& mvn -B test -DskipTests=false -f .\fhir-server-test\pom.xml -DskipWebSocketTest=true --no-transfer-progress
 
 # End of Script
