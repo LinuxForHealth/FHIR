16f960cf476556ccb0998fcb24c7632616d28d9a
Albert Wang <xuwang@us.ibm.com>
2020-03-11 13:38:39 -0400

    issue #765 update to logging per review comments
    Signed-off-by: Albert Wang <xuwang@us.ibm.com>

--- fhir-bulkimportexport-webapp/src/main/java/com/ibm/fhir/bulkcommon/BulkDataUtils.java
+++ fhir-bulkimportexport-webapp/src/main/java/com/ibm/fhir/bulkcommon/BulkDataUtils.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.ibm.cloud.objectstorage.ClientConfiguration;
