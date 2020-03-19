a966772fc847cecc13a3cb6f2385515955fa59b8
Paul Bastide <pbastide@us.ibm.com>
2020-03-19 13:28:31 -0400

    Prepopulate ResourceType Names in Tenant Allocation #811
    - The resource_types.properties map is introduces to cache the reserved
    logical_ids
    - A corresponding correctness test is introduced into the build process
    - Modify the CommonDatabaseAdapter to start at 1000
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-persistence-schema/src/main/java/com/ibm/fhir/schema/control/Db2PopulateResourceTypes.java
+++ fhir-persistence-schema/src/main/java/com/ibm/fhir/schema/control/Db2PopulateResourceTypes.java
@@ -6,11 +6,19 @@
 
 package com.ibm.fhir.schema.control;
 
-import java.sql.CallableStatement;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Types;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Logger;
 
 import com.ibm.fhir.database.utils.api.IDatabaseStatement;
 import com.ibm.fhir.database.utils.api.IDatabaseTranslator;
@@ -20,6 +28,7 @@ import com.ibm.fhir.model.type.code.FHIRResourceType;
  * Populates the Resource Types Table
  */
 public class Db2PopulateResourceTypes implements IDatabaseStatement {
+    private static final Logger LOGGER = Logger.getLogger(Db2PopulateResourceTypes.class.getName());
     private final String adminSchemaName;
     private final String schemaName;
     private final int tenantId;
@@ -32,23 +41,87 @@ public class Db2PopulateResourceTypes implements IDatabaseStatement {
 
     @Override
     public void run(IDatabaseTranslator translator, Connection c) {
-        try (Statement s = c.createStatement()) {
-            s.execute("SET " + adminSchemaName + ".SV_TENANT_ID = " + tenantId);
-            // Create tables for each resource type
+        final String stmtVariable = String.format("SET %s.SV_TENANT_ID = %d", adminSchemaName, tenantId);
+        final String stmtResourceTypeInsert =
+                String.format(
+                        "INSERT INTO %s.resource_types (mt_id, resource_type_id, resource_type) " +
+                                "VALUES %s.sv_tenant_id, ?, ?);",
+                        schemaName, adminSchemaName);
+        try (Statement s = c.createStatement(); PreparedStatement batch = c.prepareStatement(stmtResourceTypeInsert)) {
+            s.execute(stmtVariable);
+            try (InputStream fis =
+                    Db2PopulateResourceTypes.class.getClassLoader().getResourceAsStream("resource_types.properties")) {
+                Properties props = new Properties();
+                props.load(fis);
 
-            for (FHIRResourceType.ValueSet rt : FHIRResourceType.ValueSet.values()) {
-                String resourceType = rt.value();
-                String callableStr = "CALL " + schemaName + " .add_resource_type(?, ?)";
-                try (CallableStatement cStmt = c.prepareCall(callableStr);) {
-                    cStmt.setString(1, resourceType);
-                    cStmt.registerOutParameter(2, Types.INTEGER);
-                    cStmt.execute();
+                for (Entry<Object, Object> valueEntry : props.entrySet()) {
+                    Integer curVal = Integer.parseInt((String) valueEntry.getValue());
+                    String resource = (String) valueEntry.getKey();
+                    batch.setString(1, resource);
+                    batch.setLong(2, curVal);
                 }
+
+                // Check Error Codes. 
+                int[] codes = batch.executeBatch();
+                int errorCodes = 0;
+                for (int code : codes) {
+                    if (code != 0) {
+                        errorCodes++;
+                    }
+                }
+                if (errorCodes > 0) {
+                    LOGGER.warning("at least one of the Resource Types are not populated [" + errorCodes + "]");
+                }
+            } catch (IOException e) {
+                // Wrap and Send downstream 
+                throw new IllegalArgumentException(e);
             }
         } catch (SQLException x) {
             throw translator.translate(x);
         }
-
     }
 
+    /**
+     * This method is very intentional to verify the resource_types.properties on EVERY build.
+     * The mapping is intentionally managed, as these KEYS are inserted and used.
+     */
+    public static void verify() {
+        Properties props = new Properties();
+        boolean found = false;
+        try (InputStream fis =
+                Db2PopulateResourceTypes.class.getClassLoader().getResourceAsStream("resource_types.properties")) {
+            props.load(fis);
+
+            Set<String> resources = new HashSet<>();
+            for (FHIRResourceType.ValueSet rt : FHIRResourceType.ValueSet.values()) {
+                resources.add(rt.value());
+            }
+
+            // Find the Highest Value to start from:
+            Integer highestValue = 0;
+            Map<String, Integer> valueMap = new HashMap<>();
+            for (Entry<Object, Object> valueEntry : props.entrySet()) {
+                Integer curVal = Integer.parseInt((String) valueEntry.getValue());
+                String resource = (String) valueEntry.getKey();
+                valueMap.put(resource, curVal);
+                if (highestValue < curVal) {
+                    highestValue = curVal;
+                }
+                resources.remove(resource);
+            }
+
+            // Check to see if something is missing
+            for (String resource : resources) {
+                LOGGER.info(resource + "=" + highestValue++);
+                found = true;
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File access issue for resource_types");
+        }
+
+        if (found) {
+            throw new IllegalArgumentException("Resources are missing from resource_types");
+        }
+
+    }
 }
