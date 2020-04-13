b4d591500b80e9df3b8ed322c322ede12558d146
John T.E. Timm <johntimm@us.ibm.com>
2020-04-13 18:07:22 -0400

    Issue #918 - additional validation added and moved to ValidationSupport (#924)
    * Issue #918 - additional validation added and moved to ValidationSupport
    
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>
    
    * Issue #918 - fixed ordering of base64 chars, added unit tests
    
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>
    Signed-off-by: ccorley <ccorley@us.ibm.com>

--- fhir-model/src/test/java/com/ibm/fhir/model/test/Base64BinaryTest.java
+++ fhir-model/src/test/java/com/ibm/fhir/model/test/Base64BinaryTest.java
@@ -15,19 +15,64 @@ import com.ibm.fhir.model.type.Base64Binary;
 
 public class Base64BinaryTest {
     @Test
-    public void testBase64BinaryValid() throws Exception {
+    public void testBase64BinaryValid1() throws Exception {
         Base64Binary base64Binary = Base64Binary.builder().value("ABCDEA==").build();
         Assert.assertNotNull(base64Binary);
     }
 
     @Test
-    public void testBase64BinaryInvalid() throws Exception {
+    public void testBase64BinaryValid2() throws Exception {
+        Base64Binary base64Binary = Base64Binary.builder().value("ABCDEFE=").build();
+        Assert.assertNotNull(base64Binary);
+    }
+
+    @Test
+    public void testBase64BinaryValid3() throws Exception {
+        Base64Binary base64Binary = Base64Binary.builder().value("ABCDEFGH").build();
+        Assert.assertNotNull(base64Binary);
+    }
+
+    @Test
+    public void testBase64BinaryInvalidStringLength() throws Exception {
         try {
             Base64Binary.builder().value("ABCDEF").build();
             fail();
         } catch (Exception e) {
             Assert.assertTrue(e instanceof IllegalArgumentException);
-            Assert.assertEquals(e.getMessage(), "Invalid base64 encoded string: incorrect padding");
+            Assert.assertEquals(e.getMessage(), "Invalid base64 string length: 6");
+        }
+    }
+
+    @Test
+    public void testBase64BinaryUnexpectedPaddingCharacter() throws Exception {
+        try {
+            Base64Binary.builder().value("ABCDE===").build();
+            fail();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+            Assert.assertEquals(e.getMessage(), "Unexpected base64 padding character: '=' found at index: 5");
+        }
+    }
+
+    @Test
+    public void testBase64BinaryIllegalCharacter() throws Exception {
+        try {
+            Base64Binary.builder().value("ABCDE&==").build();
+            fail();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+            Assert.assertEquals(e.getMessage(), "Illegal base64 character: '&' found at index: 5");
+        }
+    }
+
+    @Test
+    public void testBase64BinaryNonZeroPaddingBits() throws Exception {
+        try {
+            Base64Binary.builder().value("ABCDEF==").build();
+            fail();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+            Assert.assertEquals(e.getMessage(), "Invalid base64 string: non-zero padding bits; character: 'F' found at index: 5 should be: 'A'");
         }
     }
 }
