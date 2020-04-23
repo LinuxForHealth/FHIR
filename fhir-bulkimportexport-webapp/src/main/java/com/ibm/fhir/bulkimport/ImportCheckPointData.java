1cdabee864bd94ed1d1b8b51b92434ba1a6ef5de
Albert(Xu) Wang <54077144+albertwang-ibm@users.noreply.github.com>
2020-04-23 15:49:42 -0400

    Apply suggestions from code review
    Signed-off-by: Albert Wang <xuwang@us.ibm.com>
    
    Co-Authored-By: Paul Bastide <pbastide@us.ibm.com>
    Signed-off-by: Albert Wang <xuwang@us.ibm.com>

--- fhir-bulkimportexport-webapp/src/main/java/com/ibm/fhir/bulkimport/ImportCheckPointData.java
+++ fhir-bulkimportexport-webapp/src/main/java/com/ibm/fhir/bulkimport/ImportCheckPointData.java
@@ -69,7 +69,7 @@ public class ImportCheckPointData implements Serializable {
         // Note: for those good imports, we don't really generate any meaningful OperationOutcome, so only error import 
         //       OperationOutcomes are supported for now.
         this.setUniqueIDForImportOperationOutcomes(importPartitionWorkitem + "_oo_success.ndjson");
-        this.setUniqueIDForImportFailureOperationOutcomes(importPartitionWorkitem + "_errors.ndjson");
+        this.setUniqueIDForImportFailureOperationOutcomes(importPartitionWorkitem + "_oo_errors.ndjson");
     }
 
 
