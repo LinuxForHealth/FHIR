b02eef1b250e07a8482fd6273a2208fed8c0fcc6
John T.E. Timm <johntimm@us.ibm.com>
2020-05-14 10:13:50 -0400

    Issue #1091 - fix broken benchmarks
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

--- fhir-benchmark/src/main/java/com/ibm/fhir/benchmark/runner/FHIRBenchmarkRunner.java
+++ fhir-benchmark/src/main/java/com/ibm/fhir/benchmark/runner/FHIRBenchmarkRunner.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2019
+ * (C) Copyright IBM Corp. 2019, 2020
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -28,15 +28,16 @@ public class FHIRBenchmarkRunner {
 
     private final Class<?> benchmarkClass;
     private final List<String> properties = new ArrayList<>();
-    
+
     public FHIRBenchmarkRunner(Class<?> benchmarkClass) {
         this.benchmarkClass = benchmarkClass;
     }
-    
+
     public FHIRBenchmarkRunner property(String key, String value) {
+        properties.add(String.format("-D%s=%s", key, value));
         return this;
     }
-    
+
     /**
      * Run without overriding any parameters
      */
@@ -53,12 +54,12 @@ public class FHIRBenchmarkRunner {
                 .shouldDoGC(true)
                 .forks(2)
                 .threads(1)
-//                .mode(Mode.AverageTime)
+//              .mode(Mode.AverageTime)
                 .addProfiler(StackProfiler.class)
                 .build();
         return new Runner(opt).run();
     }
-    
+
     /**
      * Run and override the 'exampleName' param with the passed fileName
      */
@@ -74,13 +75,13 @@ public class FHIRBenchmarkRunner {
                 .measurementTime(TimeValue.seconds(10))
                 .shouldDoGC(false)
                 .forks(1)
-//                .mode(Mode.AverageTime)
+//              .mode(Mode.AverageTime)
                 .addProfiler(StackProfiler.class)
                 .param("exampleName", fileName)
                 .build();
         return new Runner(opt).run();
     }
-    
+
     /**
      * Run the benchmark with all the examples in BenchmarkUtil.SPEC_EXAMPLE_NAMES
      */
