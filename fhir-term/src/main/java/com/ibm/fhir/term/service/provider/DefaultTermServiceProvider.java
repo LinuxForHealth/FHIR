dd318582b66acbebb71d8ae40acd040ba9e0d4a3
John T.E. Timm <johntimm@us.ibm.com>
2020-05-19 16:15:39 -0400

    Issue #1104 - expanded FHIRTermServiceProvider interface
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

--- fhir-term/src/main/java/com/ibm/fhir/term/service/provider/DefaultTermServiceProvider.java
+++ fhir-term/src/main/java/com/ibm/fhir/term/service/provider/DefaultTermServiceProvider.java
@@ -6,13 +6,73 @@
 
 package com.ibm.fhir.term.service.provider;
 
+import com.ibm.fhir.model.resource.CodeSystem;
+import com.ibm.fhir.model.resource.CodeSystem.Concept;
 import com.ibm.fhir.model.resource.ValueSet;
+import com.ibm.fhir.model.type.Code;
+import com.ibm.fhir.model.type.Coding;
+import com.ibm.fhir.model.type.code.CodeSystemHierarchyMeaning;
+import com.ibm.fhir.model.type.code.ConceptSubsumptionOutcome;
 import com.ibm.fhir.term.spi.FHIRTermServiceProvider;
+import com.ibm.fhir.term.util.CodeSystemSupport;
 import com.ibm.fhir.term.util.ValueSetSupport;
 
 public class DefaultTermServiceProvider implements FHIRTermServiceProvider {
     @Override
     public ValueSet expand(ValueSet valueSet) {
         return ValueSetSupport.expand(valueSet);
+    }
+
+    @Override
+    public Concept lookup(Coding coding) {
+        String system = (coding.getSystem() != null) ? coding.getSystem().getValue() : null;
+        String version = (coding.getVersion() != null) ? coding.getVersion().getValue() : null;
+        String code = (coding.getCode() != null) ? coding.getCode().getValue() : null;
+
+        if (system != null && code != null) {
+            String url = (version != null) ? system + "|" + version : system;
+            CodeSystem codeSystem = CodeSystemSupport.getCodeSystem(url);
+            if (codeSystem != null) {
+                return CodeSystemSupport.findConcept(codeSystem, Code.of(code));
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public ConceptSubsumptionOutcome subsumes(Coding codingA, Coding codingB) {
+        String systemA = (codingA.getSystem() != null) ? codingA.getSystem().getValue() : null;
+        String versionA = (codingA.getVersion() != null) ? codingA.getVersion().getValue() : null;
+        String codeA = (codingA.getCode() != null) ? codingA.getCode().getValue() : null;
+
+        String systemB = (codingB.getSystem() != null) ? codingB.getSystem().getValue() : null;
+        String versionB = (codingB.getVersion() != null) ? codingB.getVersion().getValue() : null;
+        String codeB = (codingB.getCode() != null) ? codingB.getCode().getValue() : null;
+
+        if (systemA != null && systemB != null && codeA != null && codeB != null && systemA.equals(systemB)) {
+            String url = systemA;
+
+            if (versionA != null || versionB != null) {
+                if (versionA != null && versionB != null && !versionA.equals(versionB)) {
+                    return null;
+                }
+                url = (versionA != null) ? (url + "|" + versionA) : (url + "|" + versionB);
+            }
+
+            CodeSystem codeSystem = CodeSystemSupport.getCodeSystem(url);
+            if (codeSystem != null && CodeSystemHierarchyMeaning.IS_A.equals(codeSystem.getHierarchyMeaning())) {
+                Concept conceptA = CodeSystemSupport.findConcept(codeSystem, Code.of(codeA));
+                if (conceptA != null) {
+                    Concept conceptB = CodeSystemSupport.findConcept(conceptA, Code.of(codeB));
+                    if (conceptA.equals(conceptB)) {
+                        return ConceptSubsumptionOutcome.EQUIVALENT;
+                    }
+                    return (conceptB != null) ? ConceptSubsumptionOutcome.SUBSUMES : ConceptSubsumptionOutcome.NOT_SUBSUMED;
+                }
+            }
+        }
+
+        return null;
     }
 }
--- fhir-term/src/main/java/com/ibm/fhir/term/spi/FHIRTermServiceProvider.java
+++ fhir-term/src/main/java/com/ibm/fhir/term/spi/FHIRTermServiceProvider.java
@@ -6,8 +6,42 @@
 
 package com.ibm.fhir.term.spi;
 
+import com.ibm.fhir.model.resource.CodeSystem.Concept;
 import com.ibm.fhir.model.resource.ValueSet;
+import com.ibm.fhir.model.type.Coding;
+import com.ibm.fhir.model.type.code.ConceptSubsumptionOutcome;
 
 public interface FHIRTermServiceProvider {
+    /**
+     * Expand the given value set per the algorithm here: http://hl7.org/fhir/valueset.html#expansion
+     *
+     * @param valueSet
+     *     the value set to be expanded
+     * @return
+     *     the expanded value set, or the original value set if already expanded or unable to expand
+     */
     ValueSet expand(ValueSet valueSet);
+
+    /**
+     * Lookup the code system concept for the given coding
+     *
+     * @param coding
+     *     the coding to lookup
+     * @return
+     *     the code system concept that matches the specified coding, or null if no such concept exists
+     */
+    Concept lookup(Coding coding);
+
+    /**
+     * Perform a subsumption test to determine if the code system concept represented by coding "A" subsumes
+     * the code system concept represented by coding "B"
+     *
+     * @param codingA
+     *     coding "A"
+     * @param codingB
+     *     coding "B"
+     * @return
+     *     the outcome of the subsumption test
+     */
+    ConceptSubsumptionOutcome subsumes(Coding codingA, Coding codingB);
 }
