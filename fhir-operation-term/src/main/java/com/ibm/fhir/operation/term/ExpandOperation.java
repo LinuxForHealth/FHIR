317c25d368e03cfe6459d17617fe8f19481e73b1
John T.E. Timm <johntimm@us.ibm.com>
2020-05-29 16:13:54 -0400

    Issue #1172 - introduced fhir-operation-term module
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

---
+++ fhir-operation-term/src/main/java/com/ibm/fhir/operation/term/ExpandOperation.java
@@ -0,0 +1,34 @@
+/*
+ * (C) Copyright IBM Corp. 2020
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.fhir.operation.term;
+
+import com.ibm.fhir.exception.FHIROperationException;
+import com.ibm.fhir.model.resource.OperationDefinition;
+import com.ibm.fhir.model.resource.Parameters;
+import com.ibm.fhir.model.resource.Resource;
+import com.ibm.fhir.operation.AbstractOperation;
+import com.ibm.fhir.operation.context.FHIROperationContext;
+import com.ibm.fhir.registry.FHIRRegistry;
+import com.ibm.fhir.rest.FHIRResourceHelpers;
+
+public class ExpandOperation extends AbstractOperation {
+    @Override
+    protected OperationDefinition buildOperationDefinition() {
+        return FHIRRegistry.getInstance().getResource("http://hl7.org/fhir/OperationDefinition/ValueSet-expand", OperationDefinition.class);
+    }
+
+    @Override
+    protected Parameters doInvoke(
+            FHIROperationContext operationContext,
+            Class<? extends Resource> resourceType,
+            String logicalId,
+            String versionId,
+            Parameters parameters,
+            FHIRResourceHelpers resourceHelper) throws FHIROperationException {
+        throw new UnsupportedOperationException(ExpandOperation.class.getName());
+    }
+}
