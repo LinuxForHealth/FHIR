1286af4c03262cbf500701f6c8fbdba8e0c77fa6
Paul Bastide <paul@bastide.org>
2020-06-16 14:43:25 -0400

    Issue 876b (#19)
    * Introduce Persistence Layer Automation Framework #876
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Postgres SQL Automation
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix Files
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix Files
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix Files
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Update
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Update
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Update
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fixup
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fixup
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fixup
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fixup
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Postgres Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Postgres Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Update postgres
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Update postgres
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix for Postgres Build
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Fix for multistore
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

---
+++ build/persistence/postgres/resources/db.sql
@@ -0,0 +1,15 @@
+-- ##############################################################################
+-- (C) Copyright IBM Corp. 2020
+--
+-- SPDX-License-Identifier: Apache-2.0
+-- ###############################################################################
+
+-- Create the users
+CREATE USER FHIRBATCH WITH LOGIN encrypted password 'change-password';
+CREATE USER FHIRSERVER WITH LOGIN encrypted password 'change-password';
+
+CREATE SCHEMA IF NOT EXISTS FHIRDATA;
+CREATE SCHEMA IF NOT EXISTS FHIR_ADMIN;
+CREATE SCHEMA IF NOT EXISTS FHIR_OAUTH;
+CREATE SCHEMA IF NOT EXISTS FHIR_JBATCH;
+-- EOF
