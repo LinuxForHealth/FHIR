6dabb94d7a9fa92a7933351b9484495b8694f781
Paul Bastide <pbastide@us.ibm.com>
2020-07-24 15:45:23 -0400

    fix: enable a tty to check status
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/pre-integration-test-docker.sh
+++ build/pre-integration-test-docker.sh
@@ -43,7 +43,9 @@ echo "Deploying the Db2 schema..."
 ./deploySchemaAndTenant.sh
 
 # Now that the schema is setup. Log out the db2pd information for the catalog cache.
-docker-compose exec -T --user db2inst1 db2 bash /opt/ibm/db2/V11.5/adm/db2pd -alldbs -catalogcache
+docker-compose exec --user db2inst1 db2 bash -c 'source ./database/config/db2inst1/sqllib/db2profile; db2 activate db fhirdb'
+docker-compose exec --user db2inst1 db2 bash -c 'source ./database/config/db2inst1/sqllib/db2profile; db2pd -alldbp -alldbs'
+docker-compose exec --user db2inst1 db2 bash -c 'source ./database/config/db2inst1/sqllib/db2profile; db2pd -alldbs -catalogcache'
 
 mkdir -p minio/miniodata/fhirbulkdata
 cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import.ndjson ./minio/miniodata/fhirbulkdata
