2a7e75b27ba98ad4131c914bd07859b074191b2b
John T.E. Timm <johntimm@us.ibm.com>
2020-08-21 11:53:43 -0400

    Issue #1436 - ensure namespace declaration is written to XHTML string (#1437)
    * Issue #1436 - ensure namespace declaration is written to XHTML string
    
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>
    
    * Issue #1436 - updated copyright information
    
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>
    
    * Issue #1436 - updated per PR feedback
    
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

--- fhir-model/src/test/java/com/ibm/fhir/model/util/test/XMLSupportTest.java
+++ fhir-model/src/test/java/com/ibm/fhir/model/util/test/XMLSupportTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2019
+ * (C) Copyright IBM Corp. 2019, 2020
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -23,18 +23,18 @@ public class XMLSupportTest {
         XMLStreamReader reader = XMLSupport.createXMLStreamReader(new StringReader(div));
         reader.next();
         Assert.assertEquals(XMLSupport.parseDiv(reader), div);
-        
+
         div = "<h:div xmlns:h=\"http://www.w3.org/1999/xhtml\"><h:p><h:b>Generated Narrative</h:b></h:p></h:div>";
         reader = XMLSupport.createXMLStreamReader(new StringReader(div));
         reader.next();
-        Assert.assertEquals(XMLSupport.parseDiv(reader), div);
-        
+        Assert.assertEquals(XMLSupport.parseDiv(reader), "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Generated Narrative</b></p></div>");
+
         div = "<div xmlns=\"http://www.w3.org/1999/xhtml\"><div><p>Anything</p></div></div>";
         reader = XMLSupport.createXMLStreamReader(new StringReader(div));
         reader.next();
         Assert.assertEquals(XMLSupport.parseDiv(reader), div);
     }
-    
+
     @Test(expectedExceptions = XMLStreamException.class)
     public void testParseInvalidDiv() throws Exception {
         String div = "<div><p><b>Generated Narrative</b></p></div>";
