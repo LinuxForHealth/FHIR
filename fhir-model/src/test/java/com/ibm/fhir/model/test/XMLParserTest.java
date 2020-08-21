290969b6bde292eba5c9f8e1963e5470a5be0620
John T.E. Timm <johntimm@us.ibm.com>
2020-08-21 09:59:06 -0400

    Issue #1436 - updated per PR feedback
    Signed-off-by: John T.E. Timm <johntimm@us.ibm.com>

--- fhir-model/src/test/java/com/ibm/fhir/model/test/XMLParserTest.java
+++ fhir-model/src/test/java/com/ibm/fhir/model/test/XMLParserTest.java
@@ -6,7 +6,10 @@
 
 package com.ibm.fhir.model.test;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.StringReader;
+import java.io.StringWriter;
 
 import org.testng.annotations.Test;
 
@@ -21,7 +24,9 @@ public class XMLParserTest {
         // FHIR elements are in default namespace (no prefix), XHTML elements are in default namespace (no prefix)
         StringReader reader = new StringReader("<Practitioner xmlns=\"http://hl7.org/fhir\"><id value=\"0\"/><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\">Narrative TBD</div></text></Practitioner>");
         Practitioner practitioner = FHIRParser.parser(Format.XML).parse(reader);
-        FHIRGenerator.generator(Format.XML, true).generate(practitioner, System.out);
+        StringWriter writer = new StringWriter();
+        FHIRGenerator.generator(Format.XML).generate(practitioner, writer);
+        assertEquals(writer.toString(), "<Practitioner xmlns=\"http://hl7.org/fhir\"><id value=\"0\"/><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\">Narrative TBD</div></text></Practitioner>");
     }
 
     @Test
@@ -29,7 +34,9 @@ public class XMLParserTest {
         // FHIR elements are prefixed, XHTML elements are prefixed, XHTML namespace is declared on root element
         StringReader reader = new StringReader("<f:Practitioner xmlns:f=\"http://hl7.org/fhir\" xmlns:h=\"http://www.w3.org/1999/xhtml\"><f:id value=\"0\"/><f:text><f:status value=\"generated\"/><h:div>Narrative TBD</h:div></f:text></f:Practitioner>");
         Practitioner practitioner = FHIRParser.parser(Format.XML).parse(reader);
-        FHIRGenerator.generator(Format.XML, true).generate(practitioner, System.out);
+        StringWriter writer = new StringWriter();
+        FHIRGenerator.generator(Format.XML).generate(practitioner, writer);
+        assertEquals(writer.toString(), "<Practitioner xmlns=\"http://hl7.org/fhir\"><id value=\"0\"/><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\">Narrative TBD</div></text></Practitioner>");
     }
 
     @Test
@@ -37,6 +44,8 @@ public class XMLParserTest {
         // FHIR elements are prefixed, XHTML elements are prefixed, XHTML namespace is declared on "div" element
         StringReader reader = new StringReader("<f:Practitioner xmlns:f=\"http://hl7.org/fhir\"><f:id value=\"0\"/><f:text><f:status value=\"generated\"/><h:div xmlns:h=\"http://www.w3.org/1999/xhtml\">Narrative TBD</h:div></f:text></f:Practitioner>");
         Practitioner practitioner = FHIRParser.parser(Format.XML).parse(reader);
-        FHIRGenerator.generator(Format.XML, true).generate(practitioner, System.out);
+        StringWriter writer = new StringWriter();
+        FHIRGenerator.generator(Format.XML).generate(practitioner, writer);
+        assertEquals(writer.toString(), "<Practitioner xmlns=\"http://hl7.org/fhir\"><id value=\"0\"/><text><status value=\"generated\"/><div xmlns=\"http://www.w3.org/1999/xhtml\">Narrative TBD</div></text></Practitioner>");
     }
 }
