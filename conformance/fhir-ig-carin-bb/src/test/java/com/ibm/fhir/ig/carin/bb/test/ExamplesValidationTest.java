d8764f70aec163d02fed306bf9622324c958d777
Lee Surprenant <lmsurpre@us.ibm.com>
2020-09-10 12:44:49 -0400

    issue #1309 - update carin-bb ig and examples
    This commit updates us to version 0.1.3 (pulled 9/9/2020 in prep for
    FHIR connecathon 25).
    The carin-bb ig is still a work-in-progress and there were a number of
    issues with the examples, mostly coding-related.
    
    Additionally, I had to fix one of the constraints that was included in
    the Inpatient and Outpatient Institutional EoB profiles.
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- conformance/fhir-ig-carin-bb/src/test/java/com/ibm/fhir/ig/carin/bb/test/ExamplesValidationTest.java
+++ conformance/fhir-ig-carin-bb/src/test/java/com/ibm/fhir/ig/carin/bb/test/ExamplesValidationTest.java
@@ -6,11 +6,21 @@
 
 package com.ibm.fhir.ig.carin.bb.test;
 
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 import org.testng.annotations.Test;
 
 import com.ibm.fhir.examples.Index;
+import com.ibm.fhir.model.resource.OperationOutcome.Issue;
+import com.ibm.fhir.model.resource.Resource;
 import com.ibm.fhir.model.spec.test.R4ExamplesDriver;
 import com.ibm.fhir.model.spec.test.SerializationProcessor;
+import com.ibm.fhir.model.test.TestUtil;
+import com.ibm.fhir.validation.FHIRValidator;
 import com.ibm.fhir.validation.test.ValidationProcessor;
 
 public class ExamplesValidationTest {
@@ -23,12 +33,11 @@ public class ExamplesValidationTest {
         driver.processIndex(Index.PROFILES_CARIN_BB_JSON);
     }
 
-    @Test
-    public void testBBValidationXML() throws Exception {
-        R4ExamplesDriver driver = new R4ExamplesDriver();
-        SerializationProcessor processor = new SerializationProcessor();
-        driver.setProcessor(processor);
-        driver.setValidator(new ValidationProcessor());
-        driver.processIndex(Index.PROFILES_CARIN_BB_XML);
+    public static void main(String[] args) throws Exception {
+        Resource r = TestUtil.readExampleResource("json/profiles/fhir-ig-carin-bb/ExplanationOfBenefit-BBEobPharmacy.json");
+        List<Issue> validate = FHIRValidator.validator().validate(r);
+        for (Issue issue : validate) {
+            System.out.println(issue);
+        }
     }
 }
