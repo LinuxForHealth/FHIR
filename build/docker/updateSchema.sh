7bada2f8bf768ac5b9965ee406a3f451fa4fccb5
Robin Arnold <robin.arnold23@ibm.com>
2020-10-25 09:46:40 -0400

    issue #1366 serialize the db2 migration step to avoid deadlocks when adding FKs to a common table
    Signed-off-by: Robin Arnold <robin.arnold23@ibm.com>

--- build/docker/updateSchema.sh
+++ build/docker/updateSchema.sh
@@ -10,6 +10,8 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 
+# For #1366 the migration hits deadlock issues if run in parallel, so
+# to avoid this, serialize the steps using --pool-size 1
 java -jar schema/fhir-persistence-schema-*-cli.jar \
   --prop-file db2.properties --schema-name FHIRDATA --update-schema \
-  --pool-size 20
+  --pool-size 1
