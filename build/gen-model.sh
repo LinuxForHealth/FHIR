61cb8a7e1599b134f91e73b2f1bac8ca54c33819
Lee Surprenant <lmsurpre@us.ibm.com>
2020-10-31 05:33:06 -0400

    fhir-examples-generator cleanup
    1. Use java.nio to work with the paths (instead of java.io)
    2. Minor refactoring in ExamplesGenerator.java
    3. Rework ExamplesPlugin to execute on `fhir-examples`
    4. Remove fhir-examples-generator/definitions (the generator uses
    ModelSupport instead)
    5. Introduce `build/gen-examples.sh` for easilly regenerating the
    examples from the terminal
    6. Renamed `build/gen-code.sh` to `build/gen-model.sh`
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

---
+++ build/gen-model.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# (C) Copyright IBM Corp. 2016, 2020
+#
+# SPDX-License-Identifier: Apache-2.0
+###############################################################################
+set -ex
+
+# The full path to the directory of this script, no matter where its called from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORKSPACE="$( dirname "${DIR}" )"
+
+# Install the latest fhir-tools
+mvn clean install -f ${WORKSPACE}/fhir-tools
+
+if [[ $? -eq 0 ]]
+then
+    mvn com.ibm.fhir:fhir-tools:generate-model -f ${WORKSPACE}/fhir-model
+fi
