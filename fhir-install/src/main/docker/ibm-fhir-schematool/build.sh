6b5e5b462e2404df8df2fe215f63af5f23279c57
Paul Bastide <pbastide@us.ibm.com>
2020-11-16 14:52:47 -0500

    build.sh updates
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-install/src/main/docker/ibm-fhir-schematool/build.sh
+++ fhir-install/src/main/docker/ibm-fhir-schematool/build.sh
@@ -14,8 +14,8 @@ docker build --build-arg FHIR_VERSION=4.5.0 -t ibm-fhir-schematool:latest .
 docker images | grep -i ibm-fhir-schematool | grep -i latest
 
 docker login
-docker tag 31d9d6f6ac89 ibmcom/ibm-fhir-schematool:4.5.0
-docker tag 31d9d6f6ac89 ibmcom/ibm-fhir-schematool:latest
+docker tag 6bd9e9fbe2a6 ibmcom/ibm-fhir-schematool:4.5.0
+docker tag 6bd9e9fbe2a6 ibmcom/ibm-fhir-schematool:latest
 docker push ibmcom/ibm-fhir-schematool:4.5.0
 docker push ibmcom/ibm-fhir-schematool:latest
 
