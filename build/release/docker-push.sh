1ef5e54da7dd417d2773cd006f6896e81c5bd1a5
Paul Bastide <pbastide@us.ibm.com>
2020-11-16 20:27:26 -0500

    ci: add automation hooks and build
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/docker-push.sh
+++ build/release/docker-push.sh
@@ -8,6 +8,11 @@
 
 # Assumes that fhir-install has already been built
 docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_TOKEN}
+
+# Executes the build for the ibm-fhir-schematool
+# This build runs first so that the FAILURE comes before and stops the subsequent builds/releases.
+${WORKSPACE}/fhir-install/src/main/docker/ibm-fhir-schematool/build.sh
+
 mvn dockerfile:build -f fhir-install -Dmaven.wagon.http.retryHandler.count=3
 mvn dockerfile:tag@tag-version -f fhir-install -Dmaven.wagon.http.retryHandler.count=3
 mvn dockerfile:push@push-version -f fhir-install -Dmaven.wagon.http.retryHandler.count=3
