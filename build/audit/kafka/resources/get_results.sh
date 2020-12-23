2f6001df182bc81ce68d2c59a12445b3181d34ac
Paul Bastide <pbastide@us.ibm.com>
2020-12-22 20:12:45 -0500

    fix: update to predefine output file
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/audit/kafka/resources/get_results.sh
+++ build/audit/kafka/resources/get_results.sh
@@ -9,6 +9,9 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
+
+ls -al /
 
 /bin/kafka-console-consumer --timeout-ms 120000 --bootstrap-server=kafka-1:19092,kafka-2:29092 \
     --topic FHIR_AUDIT --max-messages 25 --property print.timestamp=true --offset earliest \
