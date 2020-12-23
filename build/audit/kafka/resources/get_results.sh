7906ba7d1eb27f58012f04272ae963f2b8cdbdd6
Paul Bastide <pbastide@us.ibm.com>
2020-12-23 09:41:17 -0500

    fix permissions
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/audit/kafka/resources/get_results.sh
+++ build/audit/kafka/resources/get_results.sh
@@ -15,7 +15,7 @@ ls -al /
 
 /bin/kafka-console-consumer --timeout-ms 120000 --bootstrap-server=$(hostname):19092,kafka-2:29092 \
     --topic FHIR_AUDIT --max-messages 25 --property print.timestamp=true --offset earliest \
-    --consumer.config /etc/kafka/secrets/client-ssl.properties --group fhir \
+    --consumer.config /etc/kafka/secrets/client-ssl.properties \
     --partition 1 > /output/fhir_audit-messages.log
 
 # EOF
