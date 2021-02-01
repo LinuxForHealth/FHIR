0d8ea52c7fb200a4a612803d0dc8e12e3acad165
Troy Biesterfeld <tbieste@us.ibm.com>
2021-02-01 08:52:23 -0600

    Issue #1870 - Update after review comments
    Signed-off-by: Troy Biesterfeld <tbieste@us.ibm.com>

--- fhir-server-test/src/test/java/com/ibm/fhir/server/test/SearchTest.java
+++ fhir-server-test/src/test/java/com/ibm/fhir/server/test/SearchTest.java
@@ -641,7 +641,7 @@ public class SearchTest extends FHIRServerTestBase {
         assertTrue(bundle.getEntry().size() >= 1);
         // verify link does not include consecutive '&' characters
         assertTrue(bundle.getLink().size() >= 1);
-        assertTrue(!bundle.getLink().get(0).getUrl().getValue().contains("&&"));
+        assertFalse(bundle.getLink().get(0).getUrl().getValue().contains("&&"));
     }
 
     @Test(groups = { "server-search" }, dependsOnMethods = {"testCreateObservation" })
