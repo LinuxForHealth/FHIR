9c8531da2b0698d05cbb09de4b47063b2dbd14eb
Paul Bastide <pbastide@us.ibm.com>
2021-02-03 16:41:50 -0500

    Unify the Certificates including Minio
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/certificates/update-minio.sh
+++ build/certificates/update-minio.sh
@@ -11,6 +11,8 @@
 openssl x509 -in ${WORKSPACE}/build/docker/minio/public.crt -text -noout
 
 cp ${WORKSPACE}/build/certificates/tmp/fhir.key ${WORKSPACE}/build/docker/minio/private.key
+openssl rsa -passin pass:change-password -in ${WORKSPACE}/build/certificates/tmp/fhir.key  -out ${WORKSPACE}/build/docker/minio/private.key
+
 cp ${WORKSPACE}/build/certificates/tmp/fhir.crt ${WORKSPACE}/build/docker/minio/public.crt
 
 # EOF
