76a48a1aa6a97df8d59ec8fe5fa44c2feb238687
Paul Bastide <pbastide@us.ibm.com>
2021-02-12 15:34:04 -0500

    fhir-server-test relies on certificates in p12 files expire in April 2021 #1276
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/certificates/create-ca-and-certs.sh
+++ build/certificates/create-ca-and-certs.sh
@@ -47,7 +47,7 @@ openssl x509 -in fhir-server-cert.pem -text | grep 'Subject:'
 cat fhir-server-cert.pem > fhir-server-chained.pem
 cat fhir-ca-crt.pem >> fhir-server-chained.pem
 
-openssl pkcs12 -export -out fhirKeyStore.p12 -inkey fhir-server-key.pem -in fhir-server-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -name server
+openssl pkcs12 -export -out fhirKeyStore.p12 -inkey fhir-server-key.pem -in fhir-server-chained.pem -certfile fhir-ca-crt.pem -passout pass:change-password -name default
 openssl pkcs12 -export -out fhirTrustStore.p12 -in fhir-server-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -nokeys
 
 keytool -keystore fhirTrustStore.p12 -alias ca-root -import -file fhir-ca-crt.pem -storepass change-password -keypass change-password -noprompt
@@ -58,7 +58,9 @@ openssl req -newkey rsa:${BITS} -days ${DAYS} -nodes -sha512 -keyout fhir-client
 openssl x509 -req -in fhir-client-req.pem -days ${DAYS} -sha512 -CA fhir-ca-crt.pem -CAkey fhir-ca-key.pem -days 10960 -CAcreateserial -out fhir-client-cert.pem
 openssl x509 -in fhir-client-cert.pem -text | grep 'Subject:'
 
-openssl pkcs12 -export -out fhirClientKeyStore.p12 -inkey fhir-server-key.pem -in fhir-server-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -name client-auth
+openssl pkcs12 -export -out fhirClientKeyStore.p12 -inkey fhir-client-key.pem -in fhir-client-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -name client-auth
+
+keytool -keystore fhirTrustStore.p12 -alias client-auth -import -file fhir-ca-cert.pem -storepass change-password -keypass change-password -noprompt
 
 # Kafka Client
 echo "FHIR Kafka Client - certs are being generated and signed"
@@ -68,5 +70,12 @@ openssl x509 -in fhir-kafka-client-cert.pem -text | grep 'Subject:'
 
 openssl pkcs12 -export -out kafka.client.keystore.p12 -inkey fhir-kafka-client-key.pem -in fhir-kafka-client-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -name server
 openssl pkcs12 -export -out kafka.client.truststore.p12 -in fhir-kafka-client-cert.pem -certfile fhir-ca-crt.pem -passout pass:change-password -nokeys
+
+# Update the Trust Store
+keytool -keystore fhirTrustStore.p12 -alias server-ca -import -file fhir-server-cert.pem -storepass change-password -keypass change-password -noprompt
+
+keytool -keystore fhirTrustStore.p12 -alias client-ca -import -file fhir-client-cert.pem -storepass change-password -keypass change-password -noprompt
+
+keytool -keystore fhirTrustStore.p12 -alias minio-ca -import -file fhir-minio-cert.pem -storepass change-password -keypass change-password -noprompt
 
 # EOF
