29b96906330afb951ab6bdd5473e04829530e1da
Lee Surprenant <lmsurpre@us.ibm.com>
2021-02-16 08:03:08 -0500

    Move fhir-ig utils out of src/main/java and into a '.tool' package
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- conformance/fhir-ig-davinci-pdex-plan-net/src/test/java/com/ibm/fhir/ig/davinci/plannet/tool/ResourceProcessor.java
+++ conformance/fhir-ig-davinci-pdex-plan-net/src/test/java/com/ibm/fhir/ig/davinci/plannet/tool/ResourceProcessor.java
@@ -1,10 +1,9 @@
 /*
- * (C) Copyright IBM Corp. 2020
+ * (C) Copyright IBM Corp. 2020, 2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
-package com.ibm.fhir.ig.davinci.pdex.plannet.util;
+package com.ibm.fhir.ig.davinci.plannet.tool;
 
 import java.io.BufferedReader;
 import java.io.File;
