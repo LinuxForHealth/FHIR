69637751c8b9a531e8a5e6f6bbea09fc19c018bc
Lee Surprenant <lmsurpre@us.ibm.com>
2021-02-24 23:44:08 -0500

    issue #1694 - make liberty datasources the default
    1. rework the user's guide
    2. remove the fhir jars from the fhirSharedLib and move the definition
    from server.xml into the corresponding datasource dropinConfigs (this
    way we can only include the drivers that will actually be used)
    3. stop excluding fhir-core and fhir-config from our webapps since
    they're no longer in the shared lib
    4. other minor dependency tweaking to avoid packaging derby with our
    webapps
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-persistence-jdbc/src/main/java/com/ibm/fhir/persistence/jdbc/impl/FHIRPersistenceJDBCImpl.java
+++ fhir-persistence-jdbc/src/main/java/com/ibm/fhir/persistence/jdbc/impl/FHIRPersistenceJDBCImpl.java
@@ -237,9 +237,8 @@ public class FHIRPersistenceJDBCImpl implements FHIRPersistence, SchemaNameSuppl
         this.configProvider = new DefaultFHIRConfigProvider(); // before buildActionChain()
         this.schemaNameSupplier = new SchemaNameImpl(this);
 
-        // Issue #916: Still default to the old proxy datasource configuration so we don't break existing
-        // configurations. This may change in a future release.
-        if (fhirConfig.getBooleanProperty(FHIRConfiguration.PROPERTY_JDBC_ENABLE_PROXY_DATASOURCE, Boolean.TRUE)) {
+        // Now defaults to Liberty-defined JDBC datasources, but the user can still opt in to the old proxy datasource
+        if (fhirConfig.getBooleanProperty(FHIRConfiguration.PROPERTY_JDBC_ENABLE_PROXY_DATASOURCE, Boolean.FALSE)) {
             this.connectionStrategy = new FHIRDbProxyDatasourceConnectionStrategy(trxSynchRegistry, buildActionChain());
         } else {
             //  use separate JNDI datasources for each tenant/dsId (preferred approach)
@@ -444,9 +443,13 @@ public class FHIRPersistenceJDBCImpl implements FHIRPersistence, SchemaNameSuppl
 
         if (this.trxSynchRegistry != null) {
             String datastoreId = FHIRRequestContext.get().getDataStoreId();
-            return FHIRResourceDAOFactory.getResourceDAO(connection, FhirSchemaConstants.FHIR_ADMIN, schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(), this.trxSynchRegistry, this.cache, this.getTransactionDataForDatasource(datastoreId));
+            return FHIRResourceDAOFactory.getResourceDAO(connection, FhirSchemaConstants.FHIR_ADMIN,
+                    schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(),
+                    this.trxSynchRegistry, this.cache, this.getTransactionDataForDatasource(datastoreId));
         } else {
-            return FHIRResourceDAOFactory.getResourceDAO(connection, FhirSchemaConstants.FHIR_ADMIN, schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(), this.cache);
+            return FHIRResourceDAOFactory.getResourceDAO(connection, FhirSchemaConstants.FHIR_ADMIN,
+                    schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(),
+                    this.cache);
         }
     }
 
@@ -458,8 +461,11 @@ public class FHIRPersistenceJDBCImpl implements FHIRPersistence, SchemaNameSuppl
      * @throws FHIRPersistenceException
      * @throws IllegalArgumentException
      */
-    private ResourceReferenceDAO makeResourceReferenceDAO(Connection connection) throws FHIRPersistenceDataAccessException, FHIRPersistenceException, IllegalArgumentException {
-        return FHIRResourceDAOFactory.getResourceReferenceDAO(connection, FhirSchemaConstants.FHIR_ADMIN, schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(), this.cache);
+    private ResourceReferenceDAO makeResourceReferenceDAO(Connection connection)
+            throws FHIRPersistenceDataAccessException, FHIRPersistenceException, IllegalArgumentException {
+        return FHIRResourceDAOFactory.getResourceReferenceDAO(connection, FhirSchemaConstants.FHIR_ADMIN,
+                schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(),
+                this.cache);
     }
 
     /**
@@ -471,9 +477,11 @@ public class FHIRPersistenceJDBCImpl implements FHIRPersistence, SchemaNameSuppl
      */
     private ParameterDAO makeParameterDAO(Connection connection) throws FHIRPersistenceDataAccessException, FHIRPersistenceException {
         if (this.trxSynchRegistry != null) {
-            return new ParameterDAOImpl(connection, schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor(), trxSynchRegistry);
+            return new ParameterDAOImpl(connection, schemaNameSupplier.getSchemaForRequestContext(connection),
+                    connectionStrategy.getFlavor(), trxSynchRegistry);
         } else {
-            return new ParameterDAOImpl(connection, schemaNameSupplier.getSchemaForRequestContext(connection), connectionStrategy.getFlavor());
+            return new ParameterDAOImpl(connection, schemaNameSupplier.getSchemaForRequestContext(connection),
+                    connectionStrategy.getFlavor());
         }
     }
 
