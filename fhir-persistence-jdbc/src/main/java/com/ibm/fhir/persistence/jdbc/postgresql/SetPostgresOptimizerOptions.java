0d042423b1b304bd6e5cf14a01af8c7e7b17f66a
Robin Arnold <robin.arnold23@ibm.com>
2021-03-02 12:15:24 -0500

    issue-1993 use better default value for PostgreSQL query tuning options
    Signed-off-by: Robin Arnold <robin.arnold23@ibm.com>

--- fhir-persistence-jdbc/src/main/java/com/ibm/fhir/persistence/jdbc/postgresql/SetPostgresOptimizerOptions.java
+++ fhir-persistence-jdbc/src/main/java/com/ibm/fhir/persistence/jdbc/postgresql/SetPostgresOptimizerOptions.java
@@ -22,11 +22,11 @@ public class SetPostgresOptimizerOptions {
 
     private static final Logger log = Logger.getLogger(SetPostgresOptimizerOptions.class.getName());
 
-    // Configuration constants. Using 7 as the default value based on testing.
+    // Configuration constants. Further testing (issue-1993) shows 12 gives the best results overall.
     private static final String JOIN_COLLAPSE_LIMIT = "join_collapse_limit";
-    private static final int DEFAULT_JOIN_COLLAPSE_LIMIT = 7;
+    private static final int DEFAULT_JOIN_COLLAPSE_LIMIT = 12;
     private static final String FROM_COLLAPSE_LIMIT = "from_collapse_limit";
-    private static final int DEFAULT_FROM_COLLAPSE_LIMIT = 7;
+    private static final int DEFAULT_FROM_COLLAPSE_LIMIT = 12;
 
     private final Integer joinCollapseLimit;
     private final Integer fromCollapseLimit;
