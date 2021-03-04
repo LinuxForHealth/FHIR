bbb285ed35990742465b223a844e066f9f9ea096
Robin Arnold <robin.arnold23@ibm.com>
2021-03-04 14:26:40 -0500

    issue 1958 correlated update not supported in Derby so use JDBC batch updates instead
    Signed-off-by: Robin Arnold <robin.arnold23@ibm.com>

--- fhir-database-utils/src/main/java/com/ibm/fhir/database/utils/version/SchemaConstants.java
+++ fhir-database-utils/src/main/java/com/ibm/fhir/database/utils/version/SchemaConstants.java
@@ -1,10 +1,13 @@
 /*
- * (C) Copyright IBM Corp. 2019
+ * (C) Copyright IBM Corp. 2019, 2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.ibm.fhir.database.utils.version;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Schema Constants
@@ -16,4 +19,7 @@ public class SchemaConstants {
     public static final String OBJECT_TYPE = "OBJECT_TYPE";
     public static final String OBJECT_NAME = "OBJECT_NAME";
     public static final String SCHEMA_NAME = "SCHEMA_NAME";
+
+    public static final Calendar UTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+
 }
