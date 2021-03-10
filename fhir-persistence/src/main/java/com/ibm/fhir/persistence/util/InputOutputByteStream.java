a0a68e49d654f0f2596e82ea74526e4d7e9d077a
Robin Arnold <robin.arnold23@ibm.com>
2021-03-10 15:16:17 -0500

    issue 2049 added micro-benchmark for InputOutputByteStream
    Signed-off-by: Robin Arnold <robin.arnold23@ibm.com>

--- fhir-persistence/src/main/java/com/ibm/fhir/persistence/util/InputOutputByteStream.java
+++ fhir-persistence/src/main/java/com/ibm/fhir/persistence/util/InputOutputByteStream.java
@@ -6,6 +6,8 @@
 
 package com.ibm.fhir.persistence.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -203,5 +205,64 @@ public class InputOutputByteStream {
     public InputStream inputStream() {
         // ByteArrayInputStream uses synchronized, so we cut our own
         return new ByteInputStream();
+    }
+
+    /**
+     * Simple performance comparison between ByteArrayOutputStream/ByteArrayInputStream and
+     * the comparable function provided by this class. Useful to run with a profiler.
+     * On: MacBook Pro 15-inch, 2016; 2.7 GHz Quad-Core Intel Core i7; 16 GB 2133 MHz LPDDR3
+     *     the performance improvement is around 15%.
+     * @param args
+     */
+    public static void main(String[] args) {
+        final int initialSize = 4096;
+        final int iters = 100000;
+        final int writeCount = 10000; // total about 40MiB
+
+        System.out.println("Starting micro-benchmark for " + InputOutputByteStream.class.getSimpleName() + ". This should take less that 1 minute on a modern CPU (2021)");
+
+        try {
+            // Just some random text to use as a filler for the buffer
+            final byte[] src = new String("{This is the story of...}").getBytes();
+            // Get a baseline with the original implementation
+            final long origStart = System.nanoTime();
+            for (int i=0; i<iters; i++) {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream(initialSize);
+                for (int x=0; x<writeCount; x++) {
+                    bos.write(src);
+                }
+
+                ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+                byte[] tmp = new byte[4096];
+                while (bis.read(tmp) != -1) {
+                    ; // NOP
+                }
+            }
+            final long origEnd = System.nanoTime();
+
+            // Now compare with the new approach
+            final long newStart = origEnd;
+            for (int i=0; i<iters; i++) {
+                InputOutputByteStream iobs = new InputOutputByteStream(initialSize);
+                OutputStream os = iobs.outputStream();
+                for (int x=0; x<writeCount; x++) {
+                    os.write(src);
+                }
+
+                InputStream is = iobs.inputStream();
+                byte[] tmp = new byte[4096];
+                while (is.read(tmp) != -1) {
+                    ; // NOP
+                }
+            }
+            final long newEnd = System.nanoTime();
+
+            double origSeconds = (origEnd - origStart) / 1e9;
+            double newSeconds = (newEnd - newStart) / 1e9;
+
+            System.out.println(String.format("Original = %5.3fs, New = %5.3fs, Percent improvement = %5.1f%%", origSeconds, newSeconds, 100.0 * (origSeconds - newSeconds) / origSeconds));
+        } catch (IOException x) {
+            System.out.println("Test failed: " + x.getMessage());
+        }
     }
 }
