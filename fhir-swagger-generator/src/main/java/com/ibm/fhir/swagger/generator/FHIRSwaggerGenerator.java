6d6e3a1231a1546cd1d415ff99ece102a793ce4b
Troy Biesterfeld <tbieste@us.ibm.com>
2021-03-15 13:17:42 -0500

    Issue #1203 - Added openapi/swagger minItems property to arrays
    Signed-off-by: Troy Biesterfeld <tbieste@us.ibm.com>

--- fhir-swagger-generator/src/main/java/com/ibm/fhir/swagger/generator/FHIRSwaggerGenerator.java
+++ fhir-swagger-generator/src/main/java/com/ibm/fhir/swagger/generator/FHIRSwaggerGenerator.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2016, 2020
+ * (C) Copyright IBM Corp. 2016, 2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -905,21 +905,23 @@ public class FHIRSwaggerGenerator {
             Set<Class<?>> choiceElementTypes = ModelSupport.getChoiceElementTypes(modelClass, elementName);
             ElementDefinition elementDefinition = getElementDefinition(structureDefinition, modelClass, elementName + "[x]");
             String description = elementDefinition.getDefinition().getValue();
+            Integer min = elementDefinition.getMin() != null ? elementDefinition.getMin().getValue() : null;
             for (Class<?> choiceType : choiceElementTypes) {
                 if (FHIROpenApiGenerator.isApplicableForClass(choiceType, modelClass)) {
                     String choiceElementName = ModelSupport.getChoiceElementName(elementName, choiceType);
-                    generateProperty(structureDefinition, modelClass, field, properties, choiceElementName, choiceType, many, description);
+                    generateProperty(structureDefinition, modelClass, field, properties, choiceElementName, choiceType, many, description, min);
                 }
             }
         } else {
             ElementDefinition elementDefinition = getElementDefinition(structureDefinition, modelClass, elementName);
             String description = elementDefinition.getDefinition().getValue();
-            generateProperty(structureDefinition, modelClass, field, properties, elementName, (Class<?>)fieldType, many, description);
+            Integer min = elementDefinition.getMin() != null ? elementDefinition.getMin().getValue() : null;
+            generateProperty(structureDefinition, modelClass, field, properties, elementName, (Class<?>)fieldType, many, description, min);
         }
     }
 
     private static void generateProperty(StructureDefinition structureDefinition, Class<?> modelClass, Field field,
-            JsonObjectBuilder properties, String elementName, Class<?> fieldClass, boolean many, String description)
+            JsonObjectBuilder properties, String elementName, Class<?> fieldClass, boolean many, String description, Integer min)
             throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
 
         JsonObjectBuilder property = factory.createObjectBuilder();
@@ -998,7 +1000,6 @@ public class FHIRSwaggerGenerator {
             property.add("description", description);
         }
 
-
         // Include select examples to help tools avoid bumping into infinite recursion (if they try generate examples)
         JsonStructure example = null;
         if (Element.class.equals(modelClass) && Extension.class.equals(fieldClass)) {
@@ -1022,6 +1023,9 @@ public class FHIRSwaggerGenerator {
             JsonObjectBuilder wrapper = factory.createObjectBuilder();
             wrapper.add("type", "array");
             wrapper.add("items", property);
+            if (min != null && min > 0) {
+                wrapper.add("minItems", min.intValue());
+            }
             if (example != null) {
                 wrapper.add("example", example);
             }
