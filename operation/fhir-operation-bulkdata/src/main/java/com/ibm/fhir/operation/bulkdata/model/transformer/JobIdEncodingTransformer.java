3bc08293ac7b889299a907622915f939d005fead
Paul Bastide <pbastide@us.ibm.com>
2021-03-22 20:54:17 -0400

    Apply suggestions from code review
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- operation/fhir-operation-bulkdata/src/main/java/com/ibm/fhir/operation/bulkdata/model/transformer/JobIdEncodingTransformer.java
+++ operation/fhir-operation-bulkdata/src/main/java/com/ibm/fhir/operation/bulkdata/model/transformer/JobIdEncodingTransformer.java
@@ -47,7 +47,6 @@ public class JobIdEncodingTransformer {
         if (transformer == null) {
             transformer = new JobIdEncodingTransformer();
         }
-        // final SecretKeySpec BATCHJOBID_ENCRYPTION_KEY = getJobIdEncryptionKey();
         return transformer;
     }
 
