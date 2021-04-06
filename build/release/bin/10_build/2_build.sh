852b5a25596789ee0feb239547f78b40d6d11ac0
Paul Bastide <pbastide@us.ibm.com>
2021-04-06 15:26:55 -0400

    update
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/bin/10_build/2_build.sh
+++ build/release/bin/10_build/2_build.sh
@@ -12,13 +12,10 @@ set -eu -o pipefail
 # The relevant keys for this signature are published, and can be confirmed at 
 # http://keys.gnupg.net/pks/lookup?search=fhir&fingerprint=on&op=index
 
-export GPG_TTY=$(tty)
-
 # fhir-examples
 export BUILD_PROFILES=" $(jq -r '.build[] | select(.type == "fhir-examples").profiles | map(.) | join(",")' build/release/config/release.json)"
-mvn install source:jar source:test-jar \
-        javadoc:jar -DadditionalJOption=-Xdoclint:none \
-        gpg:sign -Dgpg.passphrase="${FHIR_GPG_PASSPHRASE}" \
+mvn install source:jar source:test-jar javadoc:jar gpg:sign -f fhir-examples \
+        -DadditionalJOption=-Xdoclint:none \
         -f fhir-examples -P "${BUILD_PROFILES}" -DskipTests
 
 # fhir-tools
