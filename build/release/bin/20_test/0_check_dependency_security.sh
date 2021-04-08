2ef6a7fbbb7e10a550dc7f214a150a9a23ea7b9f
Paul Bastide <pbastide@us.ibm.com>
2021-04-07 20:49:12 -0400

    Update
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/bin/20_test/0_check_dependency_security.sh
+++ build/release/bin/20_test/0_check_dependency_security.sh
@@ -9,21 +9,15 @@
 # build_security_check - Security check from a Project PATH
 # Reference https://github.com/victims/maven-security-versions
 function build_security_check { 
-    PROJECT_PATH="${1}"
     mkdir -p build/release/workarea/release_files/
-    SEC_LOGS="build/release/workarea/release_files/${PROJECT_NAME}-build_security_check.log"
-    echo " --> logging to ${LOGS}"
-    mvn -T2C com.redhat.victims.maven:security-versions:check -f "${PROJECT_PATH}" --log-file "${SEC_LOGS}"
+    SEC_LOGS="build/release/workarea/release_files/fhir-parent-build_security_check.log"
+    mvn -T2C com.redhat.victims.maven:security-versions:check -f "fhir-parent" --log-file "${SEC_LOGS}"
 
     echo "[Report]: "
-    for VULNERABLE in $(cat "${SEC_LOGS}" |  grep "is vulnerable to")
-    do
-        echo "REPORTED VULNERABLITY"
-        echo "${VULNERABLE}"
-    done
+    cat "${SEC_LOGS}" |  grep 'is vulnerable to'
     echo "[Done Report]"
 }
 
-build_security_check "fhir-parent"
+build_security_check
 
 # EOF
