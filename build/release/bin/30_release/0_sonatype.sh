38e70279d304c6082eddefa0d31a4f7c3158d240
Paul Bastide <pbastide@us.ibm.com>
2021-04-09 09:05:35 -0400

    Update for sonatype profiles
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/bin/30_release/0_sonatype.sh
+++ build/release/bin/30_release/0_sonatype.sh
@@ -10,8 +10,17 @@ set -eu -o pipefail
 
 # https://central.sonatype.org/pages/releasing-the-deployment.html
 # https://central.sonatype.org/publish/publish-maven/
-mvn deploy -f fhir-examples -DskipTests
-mvn deploy -f fhir-tools -DskipTests
-mvn deploy -f fhir-parent -DskipTests
+
+# fhir-examples
+export BUILD_PROFILES=" $(jq -r '.build[] | select(.type == "fhir-examples").profiles | map(.) | join(",")' build/release/config/release.json)"
+mvn -T2C deploy -f fhir-examples -P "${BUILD_PROFILES}" -DskipTests
+
+# fhir-tools
+export BUILD_PROFILES=" $(jq -r '.build[] | select(.type == "fhir-tools").profiles | map(.) | join(",")' build/release/config/release.json)"
+mvn -T2C deploy -f fhir-tools -P "${BUILD_PROFILES}" -DskipTests
+
+# fhir-parent
+export BUILD_PROFILES=" $(jq -r '.build[] | select(.type == "fhir-parent").profiles | map(.) | join(",")' build/release/config/release.json)"
+mvn -T2C deploy -f fhir-parent -P "${BUILD_PROFILES}" -DskipTests
 
 # EOF
