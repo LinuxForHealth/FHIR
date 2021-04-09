eae71979a1080f6f04264de0b4a638c6e48e1ec3
Paul Bastide <pbastide@us.ibm.com>
2021-04-09 11:12:22 -0400

    Update to the right oss destination
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/50_gather.sh
+++ build/release/50_gather.sh
@@ -8,6 +8,7 @@ set -eu -o pipefail
 # SPDX-License-Identifier: Apache-2.0
 ###############################################################################
 
+export WORKSPACE=$(pwd)
 bash ${WORKSPACE}/build/release/bin/50_gather/0_release_commit_details.sh
 bash ${WORKSPACE}/build/release/bin/50_gather/1_repo.sh
 bash ${WORKSPACE}/build/release/bin/50_gather/2_codecov.sh
