eaba471dd0a845e6b0f359bea0b1048afaca55b1
Corey Sanders <25621588+csandersdev@users.noreply.github.com>
2021-04-12 10:12:14 -0400

    Add configuration property defaultPageSize (#2235)
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-search/src/main/java/com/ibm/fhir/search/context/FHIRSearchContextFactory.java
+++ fhir-search/src/main/java/com/ibm/fhir/search/context/FHIRSearchContextFactory.java
@@ -1,17 +1,24 @@
 /*
- * (C) Copyright IBM Corp. 2016,2019
+ * (C) Copyright IBM Corp. 2016,2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.ibm.fhir.search.context;
 
+import java.util.logging.Logger;
+
+import com.ibm.fhir.config.FHIRConfigHelper;
+import com.ibm.fhir.config.FHIRConfiguration;
+import com.ibm.fhir.core.FHIRConstants;
+import com.ibm.fhir.search.SearchConstants;
 import com.ibm.fhir.search.context.impl.FHIRSearchContextImpl;
 
 /**
  * This factory class can be used to create instances of the FHIRSearchContext interface.
  */
 public class FHIRSearchContextFactory {
+    private static Logger log = Logger.getLogger(FHIRSearchContextFactory.class.getName());
 
     /**
      * Hide the default ctor.
@@ -23,6 +30,15 @@ public class FHIRSearchContextFactory {
      * Returns a new instance of the FHIRSearchContext interface.
      */
     public static FHIRSearchContext createSearchContext() {
-        return new FHIRSearchContextImpl();
+        int pageSize = FHIRConfigHelper.getIntProperty(FHIRConfiguration.PROPERTY_DEFAULT_PAGE_SIZE, FHIRConstants.FHIR_PAGE_SIZE_DEFAULT);
+        if (pageSize > SearchConstants.MAX_PAGE_SIZE) {
+            log.warning(String.format("Server configuration %s = %d exceeds maximum allowed page size %d; using %d",
+                FHIRConfiguration.PROPERTY_DEFAULT_PAGE_SIZE, pageSize, SearchConstants.MAX_PAGE_SIZE, SearchConstants.MAX_PAGE_SIZE));
+            pageSize = SearchConstants.MAX_PAGE_SIZE;
+        }
+        
+        FHIRSearchContext ctx = new FHIRSearchContextImpl();
+        ctx.setPageSize(pageSize);
+        return ctx;
     }
 }
