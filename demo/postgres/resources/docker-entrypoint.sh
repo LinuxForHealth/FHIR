4223fc1559058f190396d6e599f125de0460258e
Lee Surprenant <lmsurpre@us.ibm.com>
2021-05-03 13:58:53 -0400

    avoid issue with creating db in non-empty dir
    by specifying the volume (and the bind mount) one level up
    and creating the data directory during the startup script (if needed)
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- demo/postgres/resources/docker-entrypoint.sh
+++ demo/postgres/resources/docker-entrypoint.sh
@@ -12,6 +12,7 @@ set -o pipefail
 
 if [ ! -f /db/data/pg_hba.conf ]
 then
+    mkdir -p /db/data
     chown -R postgres:postgres /db/data
     su - postgres -c "/usr/local/bin/initdb -D /db/data"
 
@@ -19,17 +20,17 @@ then
     sed -ri "s!^#?(listen_addresses)\s*=\s*\S+.*!\1 = '*'!" /db/data/postgresql.conf
 
     # Configuration
-    sed -i -e"s/^max_connections = 100.*$/max_connections = 100/" /db/data/postgresql.conf
-    sed -i -e"s/^max_prepared_transactions = 100.*$/max_prepared_transactions = 100/" /db/data/postgresql.conf
-    sed -i -e"s/^max_locks_per_transaction = .*$/max_locks_per_transaction = 128/" /db/data/postgresql.conf
-    sed -i -e"s/^shared_buffers =.*$/shared_buffers = 4GB/" /db/data/postgresql.conf
-    sed -i -e"s/^#effective_cache_size = 128MB.*$/effective_cache_size = 2GB/" /db/data/postgresql.conf
-    sed -i -e"s/^#work_mem = 1MB.*$/work_mem = 1MB/" /db/data/postgresql.conf
-    sed -i -e"s/^#maintenance_work_mem = 16MB.*$/maintenance_work_mem = 512MB/" /db/data/postgresql.conf
-    sed -i -e"s/^#checkpoint_segments = .*$/checkpoint_segments = 32/" /db/data/postgresql.conf
-    sed -i -e"s/^#checkpoint_completion_target = 0.5.*$/checkpoint_completion_target = 0.7/" /db/data/postgresql.conf
-    sed -i -e"s/^#wal_buffers =.*$/wal_buffers = 16MB/" /db/data/postgresql.conf
-    sed -i -e"s/^#default_statistics_target = 100.*$/default_statistics_target = 100/" /db/data/postgresql.conf
+    sed -i -e "s/^max_connections = 100.*$/max_connections = 100/" \
+           -e "s/^max_prepared_transactions = 100.*$/max_prepared_transactions = 100/" \
+           -e "s/^max_locks_per_transaction = .*$/max_locks_per_transaction = 128/" \
+           -e "s/^shared_buffers =.*$/shared_buffers = 4GB/" \
+           -e "s/^#effective_cache_size = 128MB.*$/effective_cache_size = 2GB/" \
+           -e "s/^#work_mem = 1MB.*$/work_mem = 1MB/" \
+           -e "s/^#maintenance_work_mem = 16MB.*$/maintenance_work_mem = 512MB/" \
+           -e "s/^#checkpoint_segments = .*$/checkpoint_segments = 32/" \
+           -e "s/^#checkpoint_completion_target = 0.5.*$/checkpoint_completion_target = 0.7/" \
+           -e "s/^#wal_buffers =.*$/wal_buffers = 16MB/" \
+           -e "s/^#default_statistics_target = 100.*$/default_statistics_target = 100/" /db/data/postgresql.conf
 
     cat << EOF >> /db/data/pg_hba.conf
 
@@ -39,10 +40,8 @@ EOF
     su - postgres -c '/usr/local/bin/pg_ctl -D "/db/data" --wait --timeout=120 start'
     # Create the FHIRADMIN user
     su - postgres -c "/usr/local/bin/psql -c \"CREATE USER fhiradmin WITH LOGIN encrypted password 'change-password';\""
-
     # Create the Database
     su - postgres -c "/usr/local/bin/psql -c \"CREATE DATABASE fhirdb OWNER 'fhiradmin';\""
-
     su - postgres -c '/usr/local/bin/psql --dbname=fhirdb -v ON_ERROR_STOP=1 < /docker-entrypoint-initdb.d/db.sql'
 else
     su - postgres -c '/usr/local/bin/pg_ctl -D "/db/data" --wait --timeout=120 start'
