f9084f829facf62e210c7c3c6ca0300c75000e09
Troy Biesterfeld <tbieste@us.ibm.com>
2021-05-11 11:29:59 -0500

    Issue #1899 - Trim whitespace from values in schema tool property files
    Signed-off-by: Troy Biesterfeld <tbieste@us.ibm.com>

--- fhir-persistence-schema/src/main/java/com/ibm/fhir/schema/app/Main.java
+++ fhir-persistence-schema/src/main/java/com/ibm/fhir/schema/app/Main.java
@@ -27,6 +27,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -1533,6 +1534,14 @@ public class Main {
     public void loadPropertyFile(String filename) {
         try (InputStream is = new FileInputStream(filename)) {
             properties.load(is);
+            // Trim leading and trailing whitespace from property values
+            for (Entry<Object, Object> entry : properties.entrySet()) {
+                String trimmedValue = entry.getValue().toString().trim();
+                if (!trimmedValue.equals(entry.getValue().toString())) {
+                    logger.warning("Whitespace trimmed from value of property '" + entry.getKey() + "'");
+                    entry.setValue(trimmedValue);
+                }
+            }
         } catch (IOException x) {
             throw new IllegalArgumentException(x);
         }
@@ -1546,7 +1555,12 @@ public class Main {
     public void addProperty(String pair) {
         String[] kv = pair.split("=");
         if (kv.length == 2) {
-            properties.put(kv[0], kv[1]);
+            // Trim leading and trailing whitespace from property value
+            String trimmedValue = kv[1].trim();
+            if (!trimmedValue.equals(kv[1])) {
+                logger.warning("Whitespace trimmed from value of property '" + kv[0] + "'");
+            }
+            properties.put(kv[0], trimmedValue);
         } else {
             throw new IllegalArgumentException("Property must be defined as key=value, not: " + pair);
         }
