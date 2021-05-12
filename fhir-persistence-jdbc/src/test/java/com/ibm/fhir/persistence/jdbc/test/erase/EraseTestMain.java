b0061a79657d4759dfbe17f98ace59e82f14e3c1
Paul Bastide <pbastide@us.ibm.com>
2021-05-12 13:35:58 -0400

    Combined the Test Harnesses
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-persistence-jdbc/src/test/java/com/ibm/fhir/persistence/jdbc/test/erase/EraseTestMain.java
+++ fhir-persistence-jdbc/src/test/java/com/ibm/fhir/persistence/jdbc/test/erase/EraseTestMain.java
@@ -10,7 +10,7 @@ import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -21,56 +21,65 @@ import com.ibm.fhir.database.utils.common.JdbcPropertyAdapter;
 import com.ibm.fhir.database.utils.common.JdbcTarget;
 import com.ibm.fhir.database.utils.db2.Db2Adapter;
 import com.ibm.fhir.database.utils.db2.Db2PropertyAdapter;
-import com.ibm.fhir.database.utils.db2.Db2Translator;
 import com.ibm.fhir.database.utils.derby.DerbyAdapter;
 import com.ibm.fhir.database.utils.derby.DerbyPropertyAdapter;
 import com.ibm.fhir.database.utils.model.DbType;
 import com.ibm.fhir.database.utils.postgres.PostgresAdapter;
 import com.ibm.fhir.database.utils.postgres.PostgresPropertyAdapter;
+import com.ibm.fhir.database.utils.postgres.PostgresTranslator;
 import com.ibm.fhir.persistence.ResourceEraseRecord;
 import com.ibm.fhir.persistence.erase.EraseDTO;
 import com.ibm.fhir.persistence.jdbc.connection.FHIRDbFlavor;
 import com.ibm.fhir.persistence.jdbc.connection.FHIRDbFlavorImpl;
-import com.ibm.fhir.persistence.jdbc.connection.SchemaNameFromProps;
-import com.ibm.fhir.persistence.jdbc.connection.SchemaNameImpl;
-import com.ibm.fhir.persistence.jdbc.connection.SchemaNameSupplier;
 import com.ibm.fhir.persistence.jdbc.dao.EraseResourceDAO;
 
 /**
- * EraseTestMain is a test driver for the EraseResourceDAO
- * so it can be debugged during development.
+ * EraseTestMain is a test driver for the EraseResourceDAO so it can be debugged during development.
+ * input is ResourceType LogicalId
+ * Main Patient 1791f19a126-28b0239c-f1a1-4d15-979f-920af8d12446
+ *
+ * Example output:
+ * <code>
+ * May 12, 2021 1:36:39 PM com.ibm.fhir.persistence.jdbc.test.erase.EraseTestMain createConnection
+ * INFO: Opening connection to: jdbc:postgresql://localhost:5432/fhirdb?currentSchema=fhirdata&schemaName=fhirdata&ssl=false
+ * Got a Connection
+ * Starting the erase of 'Patient/1791f19a126-28b0239c-f1a1-4d15-979f-920af8d12446'
+ * ResourceEraseRecord [total=-1, status=NOT_FOUND]
+ * </code>
  */
-public class EraseTestDb2Main {
-    private static final String CLASSNAME = EraseTestDb2Main.class.getSimpleName();
+public class EraseTestMain {
+    private static final String CLASSNAME = EraseTestMain.class.getSimpleName();
     private static final Logger logger = Logger.getLogger(CLASSNAME);
 
-    private IDatabaseTranslator translator = new Db2Translator();
-    private DbType dbType = DbType.DB2;
-    private Properties properties = null;
+    private IDatabaseTranslator translator = new PostgresTranslator();
+    private DbType dbType = DbType.POSTGRESQL;
+    private Properties inProperties = null;
+    private Properties dbProperties = null;
+    private String[] args = null;
 
-    public EraseTestDb2Main(Properties properties) {
-        this.properties = properties;
+    private String schemaName = "fhirdata";
+    private String resourceType = null;
+    private String logicalId = null;
+
+
+    public EraseTestMain(Properties properties, String[] args) {
+        this.args = args;
+        this.inProperties = properties;
     }
 
     protected void erase() throws Exception {
         try {
             try (Connection c = createConnection()) {
-                try (Statement stmt = c.createStatement()){
-                    // Set the Tenant Id
-                    stmt.execute("SET FHIR_ADMIN.SV_TENANT_ID = 1");
-
-                    FHIRDbFlavor flavor = new FHIRDbFlavorImpl(DbType.DB2, true);
-                    EraseResourceDAO dao = new EraseResourceDAO(c, translator, "FHIRDATA", flavor, null, null);
-
-                    SchemaNameSupplier schemaNameSupplier = new SchemaNameImpl(new SchemaNameFromProps(properties));
-                    schemaNameSupplier.getSchemaForRequestContext(c);
-
+                System.out.println("Got a Connection");
+                try {
+                    FHIRDbFlavor flavor = new FHIRDbFlavorImpl(dbType, true);
+                    EraseResourceDAO dao = new EraseResourceDAO(c, translator, schemaName, flavor, null, null);
 
                     ResourceEraseRecord record = new ResourceEraseRecord();
                     EraseDTO eraseDto = new EraseDTO();
-                    eraseDto.setResourceType("Patient");
-
-                    eraseDto.setLogicalId("178f70bd344-f0a13e93-88b9-4964-86ce-6376759bab55");
+                    eraseDto.setResourceType(resourceType);
+                    eraseDto.setLogicalId(logicalId);
+                    System.out.println("Starting the erase of '" + resourceType + "/" + logicalId + "'");
                     dao.erase(record, eraseDto);
                     System.out.println(record);
                 } catch (Exception x) {
@@ -86,22 +95,59 @@ public class EraseTestDb2Main {
     }
 
     /**
+     * determines the flavor and schema of the execution.
+     */
+    public void determineFlavorAnSchema() {
+        if (inProperties.getProperty("type") != null && inProperties.getProperty("type").equals("db2")) {
+            dbType = DbType.DB2;
+            if (inProperties.getProperty("db2.schemaName") != null) {
+                schemaName = inProperties.getProperty("db2.schemaName");
+            }
+        } else {
+            if (inProperties.getProperty("postgres.schemaName") != null) {
+                schemaName = inProperties.getProperty("db2.schemaName");
+            }
+        }
+    }
+
+    /**
+     * process args
+     */
+    public void processArgs() {
+        String prefix = "db2.";
+        if (dbType == DbType.POSTGRESQL) {
+            prefix = "postgres.";
+        }
+        dbProperties = new Properties();
+        for(Entry<Object, Object> prop : inProperties.entrySet()) {
+            String key = (String) prop.getKey();
+            if (key.startsWith(prefix)) {
+                dbProperties.put(key.replace(prefix, ""), prop.getValue());
+            }
+        }
+        resourceType = args[0];
+        logicalId = args[1];
+    }
+
+    /**
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
-        properties.load(new FileInputStream("src/test/resources/test.erase.db2.properties"));
-        EraseTestDb2Main main = new EraseTestDb2Main(properties);
+        properties.load(new FileInputStream("src/test/resources/test.erase.properties"));
+        EraseTestMain main = new EraseTestMain(properties, args);
+        main.determineFlavorAnSchema();
+        main.processArgs();
         main.erase();
     }
 
     protected Connection createConnection() {
         Properties connectionProperties = new Properties();
-        JdbcPropertyAdapter adapter = getPropertyAdapter(dbType, properties);
+        JdbcPropertyAdapter adapter = getPropertyAdapter(dbType, dbProperties);
         adapter.getExtraProperties(connectionProperties);
 
-        String url = translator.getUrl(properties);
+        String url = translator.getUrl(dbProperties);
         logger.info("Opening connection to: " + url);
         Connection connection;
         try {
@@ -113,7 +159,6 @@ public class EraseTestDb2Main {
         return connection;
     }
 
-
     /**
      * Load the driver class
      */
@@ -124,7 +169,6 @@ public class EraseTestDb2Main {
             throw new IllegalStateException(e);
         }
     }
-
 
     public static JdbcPropertyAdapter getPropertyAdapter(DbType dbType, Properties props) {
         switch (dbType) {
