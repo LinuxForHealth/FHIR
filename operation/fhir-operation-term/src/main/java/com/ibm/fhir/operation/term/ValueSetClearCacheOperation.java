e5827a95b099892324f723655e91c23e621bf9b8
Corey Sanders <corey.thecolonel@gmail.com>
2021-05-24 12:10:23 -0400

    #2421 - Add terminology clear-cache operations
    The terminology subsystem caches terminology resources by URL and/or URL
    + version. If the user tries to update a CodeSystem or ValueSet resource
    keeping the same URL + version, the cache does not get cleared and some
    operations (e.g. validate-code) do not return the expected results after
    update. This change adds an operation ($clear-cache) that can be called
    on either terminology resource and will clear the appropriate caches.
    
    Signed-off-by: Corey Sanders <corey.thecolonel@gmail.com>

---
+++ operation/fhir-operation-term/src/main/java/com/ibm/fhir/operation/term/ValueSetClearCacheOperation.java
@@ -0,0 +1,72 @@
+package com.ibm.fhir.operation.term;
+
+import java.io.InputStream;
+
+import com.ibm.fhir.config.FHIRRequestContext;
+import com.ibm.fhir.core.HTTPReturnPreference;
+import com.ibm.fhir.exception.FHIROperationException;
+import com.ibm.fhir.model.format.Format;
+import com.ibm.fhir.model.parser.FHIRParser;
+import com.ibm.fhir.model.resource.OperationDefinition;
+import com.ibm.fhir.model.resource.OperationOutcome;
+import com.ibm.fhir.model.resource.Parameters;
+import com.ibm.fhir.model.resource.Resource;
+import com.ibm.fhir.model.resource.ValueSet;
+import com.ibm.fhir.model.type.Code;
+import com.ibm.fhir.model.type.CodeableConcept;
+import com.ibm.fhir.model.type.Coding;
+import com.ibm.fhir.model.type.code.IssueSeverity;
+import com.ibm.fhir.model.type.code.IssueType;
+import com.ibm.fhir.server.operation.spi.FHIROperationContext;
+import com.ibm.fhir.server.operation.spi.FHIRResourceHelpers;
+import com.ibm.fhir.server.util.FHIROperationUtil;
+import com.ibm.fhir.term.util.ValueSetSupport;
+
+public class ValueSetClearCacheOperation extends AbstractTermOperation {
+    
+    @Override
+    protected OperationDefinition buildOperationDefinition() {
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("operation-valueset-clear-cache.json")) {
+            return FHIRParser.parser(Format.JSON).parse(in);
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+    
+    @Override
+    protected Parameters doInvoke(
+            FHIROperationContext operationContext,
+            Class<? extends Resource> resourceType,
+            String logicalId,
+            String versionId,
+            Parameters parameters,
+            FHIRResourceHelpers resourceHelper) throws FHIROperationException {
+
+        try {
+            ValueSet valueSet = getResource(operationContext, logicalId, parameters, resourceHelper, ValueSet.class);
+            ValueSetSupport.clearCache(valueSet);
+            
+            OperationOutcome operationOutcome = OperationOutcome.builder().issue(
+                OperationOutcome.Issue.builder()
+                    .severity(IssueSeverity.INFORMATION)
+                    .code(IssueType.INFORMATIONAL)
+                    .details(CodeableConcept.builder().coding(
+                        Coding.builder().code(Code.of("success")).build()
+                     ).build()).build()
+                ).build();
+            
+            if (FHIRRequestContext.get().getReturnPreference() == HTTPReturnPreference.OPERATION_OUTCOME) {
+                return FHIROperationUtil.getOutputParameters(operationOutcome);
+            } else {
+                return null;
+            }
+        } catch( Throwable t ) {
+            throw new FHIROperationException("Unexpected error occurred while processing request for operation '"
+                    + getName() + "': " + getCausedByMessage(t), t);
+        } 
+    }
+    
+    private String getCausedByMessage(Throwable throwable) {
+        return throwable.getClass().getName() + ": " + throwable.getMessage();
+    }
+}
