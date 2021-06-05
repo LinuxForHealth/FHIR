6606e5397baf7e1f2ce58709124bb774ceac9e2a
Lee Surprenant <lmsurpre@us.ibm.com>
2021-06-05 16:09:34 -0400

    attempt to free disk space before our docker build
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- build/release/bin/30_release/1_dockerhub.sh
+++ build/release/bin/30_release/1_dockerhub.sh
@@ -14,7 +14,15 @@ docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_TOKEN}"
 export WORKSPACE=$(pwd)
 bash ${WORKSPACE}/fhir-install/src/main/docker/ibm-fhir-schematool/build.sh
 
-docker build fhir-install -t ibmcom/ibm-fhir-server
+# create and remove a 1 GB file to make sure we have the room needed later
+df -h
+dd if=/dev/urandom oflag=direct of=balloon.dat bs=1024k count=1000
+rm -f balloon.dat
+sudo apt clean
+docker system prune -f
+df -h
+
+mvn -B dockerfile:build -f fhir-install --no-transfer-progress -Dmaven.wagon.http.retryHandler.count=3
 mvn dockerfile:tag@tag-version -f fhir-install
 mvn dockerfile:push@push-version -f fhir-install
 mvn dockerfile:push@push-latest -f fhir-install
