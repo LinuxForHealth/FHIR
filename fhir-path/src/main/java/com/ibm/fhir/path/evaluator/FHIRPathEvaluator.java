6ed332f7f61765abf1fceb1be531eaa76af2b4a1
Lee Surprenant <lmsurpre@us.ibm.com>
2021-06-15 14:29:08 -0400

    issue #2510 - make FHIRPathUtil threadsafe (#2514)
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-path/src/main/java/com/ibm/fhir/path/evaluator/FHIRPathEvaluator.java
+++ fhir-path/src/main/java/com/ibm/fhir/path/evaluator/FHIRPathEvaluator.java
@@ -83,9 +83,14 @@ import com.ibm.fhir.path.exception.FHIRPathException;
 import com.ibm.fhir.path.function.FHIRPathFunction;
 import com.ibm.fhir.path.util.FHIRPathUtil;
 
+import net.jcip.annotations.NotThreadSafe;
+
 /**
  * A FHIRPath evaluation engine that implements the FHIRPath 2.0.0 <a href="http://hl7.org/fhirpath/N1/">specification</a>
+ *
+ * The static factory method {@link #evaluator()} is threadsafe, but the created instances are not.
  */
+@NotThreadSafe
 public class FHIRPathEvaluator {
     private static final Logger log = Logger.getLogger(FHIRPathEvaluator.class.getName());
 
