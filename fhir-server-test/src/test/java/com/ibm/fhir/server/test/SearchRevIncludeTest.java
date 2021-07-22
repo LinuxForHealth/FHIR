771d4360cd40ebfb0e53a2be84652a47d27dc037
Paul Bastide <pbastide@us.ibm.com>
2021-07-21 20:48:34 -0400

    update the copyright and debug output
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-server-test/src/test/java/com/ibm/fhir/server/test/SearchRevIncludeTest.java
+++ fhir-server-test/src/test/java/com/ibm/fhir/server/test/SearchRevIncludeTest.java
@@ -753,8 +753,8 @@ public class SearchRevIncludeTest extends FHIRServerTestBase {
                 .queryParam("_revinclude", "NutritionOrder:patient")
                 .request(FHIRMediaType.APPLICATION_FHIR_JSON)
                 .get();
-        String s = response.readEntity(String.class);
-        System.out.println("SSSS -> " + s);
+        Bundle s = response.readEntity(Bundle.class);
+        System.out.println("SSSS -> " + s.getEntry().size());
         assertResponse(response, Response.Status.BAD_REQUEST.getStatusCode());
         assertExceptionOperationOutcome(response.readEntity(OperationOutcome.class),
                 "Number of returned 'include' resources exceeds allowable limit of 1000");
