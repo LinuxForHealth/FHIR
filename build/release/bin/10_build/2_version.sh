6271c702eabaabfa9e8478b641973483421a0049
Lee Surprenant <lmsurpre@us.ibm.com>
2021-08-11 21:16:08 -0400

    compare fhir-examples.version against pom version before it changes
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- build/release/bin/10_build/2_version.sh
+++ build/release/bin/10_build/2_version.sh
@@ -8,18 +8,10 @@ set -eu -o pipefail
 # SPDX-License-Identifier: Apache-2.0
 ###############################################################################
 
-# set version per latest build_id 
-# Parameters: 
-#   PROJECT
-#   NEW_VERISON
-# Reference https://www.mojohaus.org/versions-maven-plugin/set-mojo.html
-# use versions:revert - option to revert the change. 
+# Set the version of the variables in fhir-parent.
 
-mvn versions:set  -f "fhir-examples" -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
-mvn versions:set  -f "fhir-tools" -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
-mvn versions:set  -f "fhir-parent"  -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
-
-# Reconcile the versions.
+# Only update the fhir-examples.version variable if it matches the version, usually a SNAPSHOT,
+# of the fhir-examples module.
 FHIR_PARENT_VERSION=$(cat fhir-parent/pom.xml | xpath -q -e project/properties/fhir-examples.version/text\(\))
 FHIR_EXAMPLES_VERSION=$(cat fhir-examples/pom.xml | xpath -q -e project/version/text\(\))
 if [[ "${FHIR_PARENT_VERSION}" == "${FHIR_EXAMPLES_VERSION}" ]]
@@ -27,5 +19,14 @@ then
     mvn org.codehaus.mojo:versions-maven-plugin:2.7:set-property -Dproperty=fhir-examples.version -DnewVersion="${BUILD_VERSION}" -f fhir-parent
 fi
 mvn org.codehaus.mojo:versions-maven-plugin:2.7:set-property -Dproperty=fhir-tools.version -DnewVersion="${BUILD_VERSION}" -f fhir-parent
+
+
+# Set the project version in all the poms.
+
+# Reference https://www.mojohaus.org/versions-maven-plugin/set-mojo.html
+# use versions:revert - option to revert the change.
+mvn versions:set  -f "fhir-examples" -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
+mvn versions:set  -f "fhir-tools" -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
+mvn versions:set  -f "fhir-parent"  -DoldVersion="*" -DnewVersion="${BUILD_VERSION}"
 
 # EOF
