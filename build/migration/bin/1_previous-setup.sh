f23a2bd6e92e24d8484ed06c0e05ef76eb4ea971
Paul Bastide <pbastide@us.ibm.com>
2021-08-23 19:59:53 -0400

    Account for  missing dependency in maven central
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/migration/bin/1_previous-setup.sh
+++ build/migration/bin/1_previous-setup.sh
@@ -21,6 +21,19 @@ required_build(){
 
     # build binaries
     mvn -T2C -B install --file fhir-examples --no-transfer-progress
+
+    # 12.6.0 no longer exists.
+    # com.azure:azure-storage-blob:12.13.0
+    # com.azure:azure-core-http-okhttp:1.7.2
+    # com.azure:azure-core:1.17.0
+    AZURE_STORAGE_BLOB_VERSION=$(xpath -q -e  './project/dependencyManagement/dependencies/dependency[./groupId/text() = "com.azure" and ./artifactId/text() = "azure-storage-blob"]/version/text()' fhir-parent/pom.xml)
+    if [ "${AZURE_STORAGE_BLOB_VERSION}" = "12.6.0" ]
+    then
+        mvn versions:use-dep-version -Dincludes=com.azure:azure-storage-blob -DdepVersion=12.13.0 -DforceVersion=true -f fhir-parent/pom.xml
+        mvn versions:use-dep-version -Dincludes=com.azure:azure-core-http-okhttp -DdepVersion=1.7.2 -DforceVersion=true -f fhir-parent/pom.xml
+        mvn versions:use-dep-version -Dincludes=com.azure:azure-core -DdepVersion=1.17.0 -DforceVersion=true -f fhir-parent/pom.xml
+    fi
+
     mvn -T2C -B install --file fhir-parent -DskipTests -P include-fhir-igs,integration --no-transfer-progress
 
     # create and remove a 1 GB file to make sure we have the room needed later
