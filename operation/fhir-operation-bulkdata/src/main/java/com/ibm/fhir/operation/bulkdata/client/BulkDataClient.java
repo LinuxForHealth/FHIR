6fe924ea6d6bb382c019c7a8b4be2ff0fad8efec
Paul Bastide <pbastide@us.ibm.com>
2021-08-25 08:15:53 -0400

    Update the server side exception
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- operation/fhir-operation-bulkdata/src/main/java/com/ibm/fhir/operation/bulkdata/client/BulkDataClient.java
+++ operation/fhir-operation-bulkdata/src/main/java/com/ibm/fhir/operation/bulkdata/client/BulkDataClient.java
@@ -354,7 +354,7 @@ public class BulkDataClient {
                 if (OperationConstants.FAILED_BAD_SOURCE.equals(bulkExportJobExecutionResponse.getExitStatus())) {
                     throw export.buildOperationException("A bad source input was used during a call to $import", IssueType.INVALID);
                 } else if (OperationConstants.NO_SUCH_BUCKET.equals(bulkExportJobExecutionResponse.getExitStatus())) {
-                    throw export.buildOperationException("No such bucket exists for the storageProvider", IssueType.EXCEPTION);
+                    throw export.buildOperationException("No such bucket exists for the storageProvider", IssueType.NO_STORE);
                 } else {
                     throw export.buildOperationException("The job has failed", IssueType.EXCEPTION);
                 }
