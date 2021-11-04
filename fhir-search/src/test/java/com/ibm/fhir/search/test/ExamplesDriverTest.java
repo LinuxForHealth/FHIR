5a3dcb93fd31034d29d60e2f811dae96c2e89673
Paul Bastide <pbastide@us.ibm.com>
2021-11-03 20:51:34 -0400

    Fix for Threading problem in tenant configurations in tests (#2939)
    * Small Fix
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>
    
    * Add missing import
    
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-search/src/test/java/com/ibm/fhir/search/test/ExamplesDriverTest.java
+++ fhir-search/src/test/java/com/ibm/fhir/search/test/ExamplesDriverTest.java
@@ -7,18 +7,27 @@
 package com.ibm.fhir.search.test;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 
+import com.ibm.fhir.config.FHIRConfiguration;
+import com.ibm.fhir.config.FHIRRequestContext;
 import com.ibm.fhir.examples.Index;
 import com.ibm.fhir.model.spec.test.R4ExamplesDriver;
 import com.ibm.fhir.validation.test.ValidationProcessor;
 
 public class ExamplesDriverTest {
 
+    @BeforeClass
+    public void setup() {
+        FHIRConfiguration.setConfigHome("target/test-classes");
+    }
+
     /**
      * Process all the examples in the fhir-r4-spec example library
      */
     @Test(groups = { "server-examples" })
     public void processExamples() throws Exception {
+        FHIRRequestContext.get().setTenantId("default");
         // Process each of the examples using the provided ExampleRequestProcessor. We want to
         // validate first before we try and send to FHIR
         final R4ExamplesDriver driver = new R4ExamplesDriver();
@@ -27,6 +36,7 @@ public class ExamplesDriverTest {
         String index = System.getProperty(this.getClass().getName()
             + ".index", Index.MINIMAL_JSON.name());
         driver.processIndex(Index.valueOf(index));
+        FHIRRequestContext.remove();
      }
     
 }
