c7ec030d6d064e8f6e9bc5dc3ca04cb7f514c14c
Lee Surprenant <lmsurpre@us.ibm.com>
2021-11-15 17:39:56 -0500

    issue #2066 - fhir-tools updates for generating code from R4B
    * added R4BModelGeneratorImpl and updated ModelFactory to support that.
    * moved current definitions under R4 dir and introduced R4B dir for the
    spec artifacts
      * added fhir-expansions from the expansions download of the R4B spec
      * added R4B/terminology for other one-off CodeSystem and ValueSet
    resources that used to be packaged with the base spec
    * use a set instead of a list for codesystem values to workaround
    duplicate values in the base spec ResourceType definition
    (https://jira.hl7.org/browse/FHIR-34303)
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-tools/src/main/java/com/ibm/fhir/model/plugin/generator/ModelFactory.java
+++ fhir-tools/src/main/java/com/ibm/fhir/model/plugin/generator/ModelFactory.java
@@ -8,34 +8,38 @@ package com.ibm.fhir.model.plugin.generator;
 
 import org.apache.maven.plugin.logging.Log;
 
+import com.ibm.fhir.model.plugin.generator.impl.R4BModelGeneratorImpl;
 import com.ibm.fhir.model.plugin.generator.impl.R4ModelGeneratorImpl;
 
 /**
  * Controls the access to the modelgenerator tools
- * 
+ *
  * @author pbastide
  *
  */
 public class ModelFactory {
-    
+
     private ModelFactory() {
-        // No Operation 
+        // No Operation
     }
 
     /**
      * uses generator version to find the specific implementation
-     * if the version is not found based on class name, it defaults to the r4 version. 
-     *   
+     * if the version is not found based on class name, it defaults to the r4 version.
+     *
      * @param generatorVersion
-     * @param log 
+     * @param log
      * @return
      */
     public static ModelGenerator getModelGenerator(String generatorVersion, Log log) {
-        
+
         //Default is R4
-        ModelGenerator generator = new R4ModelGeneratorImpl();
-        if("r4".compareTo(generatorVersion)!=0) {
-            // Not the default 
+        ModelGenerator generator = null;
+        if("r4b".equalsIgnoreCase(generatorVersion)) {
+            generator = new R4BModelGeneratorImpl();
+        } else if ("r4".equalsIgnoreCase(generatorVersion)) {
+            generator = new R4ModelGeneratorImpl();
+        } else {
             try {
                 generator = (ModelGenerator) Class.forName(generatorVersion).newInstance();
             } catch (InstantiationException e) {
@@ -46,9 +50,7 @@ public class ModelFactory {
                 log.warn("class is not found");
             }
         }
-        
+
         return generator;
     }
-    
-    
 }
