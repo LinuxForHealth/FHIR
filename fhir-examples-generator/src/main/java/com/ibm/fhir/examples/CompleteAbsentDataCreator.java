1de8dea8a93daeaa4e9b5521647d5e73fa55c488
Lee Surprenant <lmsurpre@us.ibm.com>
2021-12-14 22:58:07 -0500

    removed ClinicalUseIssue and fixed ModelSupport CODE_SUBTYPE_MAP init
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-examples-generator/src/main/java/com/ibm/fhir/examples/CompleteAbsentDataCreator.java
+++ fhir-examples-generator/src/main/java/com/ibm/fhir/examples/CompleteAbsentDataCreator.java
@@ -20,7 +20,6 @@ import com.ibm.fhir.model.resource.Appointment;
 import com.ibm.fhir.model.resource.AuditEvent;
 import com.ibm.fhir.model.resource.CarePlan;
 import com.ibm.fhir.model.resource.ClinicalUseDefinition;
-import com.ibm.fhir.model.resource.ClinicalUseIssue;
 import com.ibm.fhir.model.resource.Composition;
 import com.ibm.fhir.model.resource.Condition;
 import com.ibm.fhir.model.resource.CoverageEligibilityResponse;
@@ -170,11 +169,6 @@ public class CompleteAbsentDataCreator extends DataCreatorBase {
                         builder instanceof ClinicalUseDefinition.Builder && choiceIndicator % 5 != 2 && method.getName().equals("interaction") ||
                         builder instanceof ClinicalUseDefinition.Builder && choiceIndicator % 5 != 3 && method.getName().equals("undesirableEffect") ||
                         builder instanceof ClinicalUseDefinition.Builder && choiceIndicator % 5 != 4 && method.getName().equals("warning") ||
-                        builder instanceof ClinicalUseIssue.Builder && choiceIndicator % 5 != 0 && method.getName().equals("indication") ||
-                        builder instanceof ClinicalUseIssue.Builder && choiceIndicator % 5 != 1 && method.getName().equals("contraindication") ||
-                        builder instanceof ClinicalUseIssue.Builder && choiceIndicator % 5 != 2 && method.getName().equals("interaction") ||
-                        builder instanceof ClinicalUseIssue.Builder && choiceIndicator % 5 != 3 && method.getName().equals("undesirableEffect") ||
-                        builder instanceof ClinicalUseIssue.Builder && choiceIndicator % 5 != 4 && method.getName().equals("warning") ||
                         // cpl-3: Provide a reference or detail, not both
                         builder instanceof CarePlan.Activity.Builder && method.getName().equals("detail")) {
 
