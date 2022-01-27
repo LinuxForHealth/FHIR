d564a38f4678efb1470e4ba0a58d3dc70b1010a3
Lee Surprenant <lmsurpre@us.ibm.com>
2022-01-27 14:03:07 -0500

    issue #3192 - omit breaking types from 4.0.1 CapabilityStatement
    In the current draft of FHIR R4B, the following resource types have
    backwards-incompatible changes:
    * DeviceDefinition
    * Evidence
    * EvidenceVariable
    
    Now these resource types will be omitted from the FHIR 4.0.1 version of
    our CapabilityStatement (similar to the types that are newly added in
    R4B).
    
    Based on https://chat.fhir.org/#narrow/stream/179166-implementers/topic/R4B.20compatibilty/near/269414612,
    I left a TODO in the code about whether to remove ActivityDefinition and
    PlanDefinition from the 4.0.1 flavor of the CapabilityStatement as well.
    At least for now, they are still advertised and should work for R4
    clients just fine (so long as there's no R4B instances that use the new
    subjectCanonical choice type).
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-server/src/main/java/com/ibm/fhir/server/resources/Capabilities.java
+++ fhir-server/src/main/java/com/ibm/fhir/server/resources/Capabilities.java
@@ -139,6 +139,14 @@ public class Capabilities extends FHIRResource {
         R4B_ONLY_RESOURCES.add(ResourceType.Value.SUBSCRIPTION_STATUS);
         R4B_ONLY_RESOURCES.add(ResourceType.Value.SUBSCRIPTION_TOPIC);
         R4B_ONLY_RESOURCES.add(ResourceType.Value.SUBSTANCE_DEFINITION);
+        // The following resource types existed in R4, but have breaking changes in R4B.
+        // Because we only support the R4B version, we don't want to advertise these in our 4.0.1 statement.
+        R4B_ONLY_RESOURCES.add(ResourceType.Value.DEVICE_DEFINITION);
+        R4B_ONLY_RESOURCES.add(ResourceType.Value.EVIDENCE);
+        R4B_ONLY_RESOURCES.add(ResourceType.Value.EVIDENCE_VARIABLE);
+        // TODO: make final decision on whether to lump these in with the breaking resources
+        // R4B_ONLY_RESOURCES.add(ResourceType.Value.PLAN_DEFINITION);
+        // R4B_ONLY_RESOURCES.add(ResourceType.Value.ACTIVITY_DEFINITION);
     }
 
     // Error Messages
@@ -149,7 +157,7 @@ public class Capabilities extends FHIRResource {
 
     @Context
     protected HttpServletRequest httpServletRequest;
-    
+
     // Constructor
     public Capabilities() throws Exception {
         super();
@@ -205,7 +213,7 @@ public class Capabilities extends FHIRResource {
 
     /**
      * Which FHIRVersion to use for the generated CapabilityStatement
-     * 
+     *
      * @param acceptHeaderValue
      * @return 4.3.0 if the client is asking for it, otherwise 4.0.1
      */
@@ -386,7 +394,7 @@ public class Capabilities extends FHIRResource {
         }
 
         com.ibm.fhir.model.type.Boolean isUpdateCreate = com.ibm.fhir.model.type.Boolean.of(isUpdateCreateEnabled());
-        
+
         // Build the list of supported resources.
         List<Rest.Resource> resources = new ArrayList<>();
 
@@ -676,7 +684,7 @@ public class Capabilities extends FHIRResource {
      */
     private List<ResourceType.Value> getSupportedResourceTypes(PropertyGroup rsrcsGroup, FHIRVersion fhirVersion) throws Exception {
         final List<ResourceType.Value> resourceTypes = new ArrayList<>();
-        
+
         if (rsrcsGroup == null) {
             resourceTypes.addAll(ALL_RESOURCE_TYPES);
         } else {
