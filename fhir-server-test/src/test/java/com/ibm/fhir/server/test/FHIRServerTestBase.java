a39ec7b0dc3a612c7db44bca69db700d56182f48
Paul Bastide <pbastide@us.ibm.com>
2022-02-10 13:27:39 -0500

    Update per latest code in main
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- fhir-server-test/src/test/java/com/ibm/fhir/server/test/FHIRServerTestBase.java
+++ fhir-server-test/src/test/java/com/ibm/fhir/server/test/FHIRServerTestBase.java
@@ -39,7 +39,6 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.tyrus.client.SslContextConfigurator;
 import org.glassfish.tyrus.client.SslEngineConfigurator;
-import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -417,27 +416,21 @@ public abstract class FHIRServerTestBase {
         return null;
     }
 
-    @BeforeClass(dependsOnMethods = {"setUp"})
-    public void cacheCapabilityStatement(ITestContext context) throws Exception {
-        Object obj = context.getAttribute("conformance");
-        if (obj == null) {
-            FHIRClient client = getFHIRClient();
-            FHIRResponse response = client.metadata();
-            assertNotNull(response);
-            assertEquals(200, response.getStatus());
-            conformanceStmt = response.getResource(CapabilityStatement.class);
-            assertNotNull(conformanceStmt);
-            context.setAttribute("conformance", conformanceStmt);
-        } else {
-            conformanceStmt = (CapabilityStatement) obj;
-        }
-    }
-
     /**
      * Common function to invoke the 'metadata' operation and return the
      * CapabilityStatement object.
      */
     protected CapabilityStatement retrieveConformanceStatement() throws Exception {
+        if (conformanceStmt == null) {
+            FHIRClient client = getFHIRClient();
+            FHIRResponse response = client.metadata();
+            assertNotNull(response);
+            assertEquals(200, response.getStatus());
+
+            conformanceStmt = response.getResource(CapabilityStatement.class);
+            assertNotNull(conformanceStmt);
+        }
+
         return conformanceStmt;
     }
 
--- fhir-server-test/src/test/java/com/ibm/fhir/server/test/cpg/ServerCqlOperationTest.java
+++ fhir-server-test/src/test/java/com/ibm/fhir/server/test/cpg/ServerCqlOperationTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2021
+ * (C) Copyright IBM Corp. 2021, 2022
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,6 +7,7 @@ package com.ibm.fhir.server.test.cpg;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
 import java.util.Properties;
 
@@ -33,7 +34,7 @@ public class ServerCqlOperationTest extends BaseCPGOperationTest {
     public void setup() throws Exception {
         Properties testProperties = TestUtil.readTestProperties("test.properties");
         setUp(testProperties);
-
+        
         JsonObject jsonObject = TestUtil.readJsonObject("testdata/Patient_SallyFields.json");
         Entity<JsonObject> entity = Entity.entity(jsonObject, FHIRMediaType.APPLICATION_FHIR_JSON);
         Response response = getWebTarget().path("/" + TEST_PATIENT_ID).request().put( entity );
@@ -42,49 +43,65 @@ public class ServerCqlOperationTest extends BaseCPGOperationTest {
         // Add to a registry so the AfterClass method deletes it.
         this.addToResourceRegistry("Patient", "sally-fields");
     }
-
+    
     @Test
     public void testEvaluateArbitraryCql() {
         Response response = getWebTarget().path("$cql").queryParam("expression", "Patient.gender").queryParam("subject", TEST_PATIENT_ID).request().get();
         assertResponse( response, 200 );
-
+        
         Parameters parameters = response.readEntity(Parameters.class);
         assertNotNull(parameters.getParameter(), "Null parameters list");
         assertEquals(parameters.getParameter().size(), 1);
-
+        
         Parameter pReturn = parameters.getParameter().get(0);
         assertEquals(pReturn.getName().getValue(), "return");
     }
-
+    
     @Test
     public void testEvaluateArbitraryCqlGenderComparison() {
         Response response = getWebTarget().path("$cql").queryParam("expression", "[Patient] p where p.gender = 'female'").queryParam("subject", TEST_PATIENT_ID).request().get();
         assertResponse( response, 200 );
-
+        
         Parameters parameters = response.readEntity(Parameters.class);
         assertNotNull(parameters.getParameter(), "Null parameters list");
         assertEquals(parameters.getParameter().size(), 1);
-
+        
         Parameter pReturn = parameters.getParameter().get(0);
         assertEquals(pReturn.getName().getValue(), "return");
     }
-
+    
     @Test
     public void testEvaluateArbitraryCqlCompileError() {
         Response response = getWebTarget().path("$cql").queryParam("expression", "[NonResource]").queryParam("subject", TEST_PATIENT_ID).request().get();
         assertResponse( response, 400 );
-
+        
         OperationOutcome outcome = response.readEntity(OperationOutcome.class);
+        System.out.println(outcome.toString());
+        
         assertEquals( outcome.getIssue().get(0).getCode(), IssueType.INVALID);
     }
-
+    
     @Test
     public void testEvaluateArbitraryCqlWithDebug() {
         Response response = getWebTarget().path("$cql").queryParam("expression", "Patient.gender").queryParam("subject", TEST_PATIENT_ID).queryParam("debug", "true").request().get();
         assertResponse( response, 200 );
-
+        
         Parameters parameters = response.readEntity(Parameters.class);
         assertNotNull(parameters.getParameter(), "Null parameters list");
         assertEquals(parameters.getParameter().size(), 2);
+    }
+    
+    @Test
+    public void testUsefulDetailIncludedInMissingPatientResponse() {
+        Response response = getWebTarget()
+                .path("$cql")
+                .queryParam("expression", "Patient.gender")
+                .queryParam("subject", "Patient/does-not-exist")
+                .request()
+                .get();
+        assertResponse(response, 500);
+
+        String responseBody = response.readEntity(String.class);
+        assertTrue(responseBody.contains("Resource 'Patient/does-not-exist' not found."), responseBody);
     }
 }
