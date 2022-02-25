aeed709ce8fe3e4a47b32d902d29d342030a552d
Lee Surprenant <lmsurpre@us.ibm.com>
2022-02-24 22:54:47 -0500

    complete-mock update to avoid accidental override of Patient compartment
    I finally got to the bottom of the strange test failure I was seeing.
    1. We have serverRegistryResourceProviderEnabled
    2. Conditionally, we load the database with all the sample data from
    fhir-examples
    3. Now the maps in CompartmentUtil are member variables and they get
    populated when the instance is constructed
    4. Our EraseOperation implementation instantiates its own
    CompartmentUtil for checking whether the target of $erase is in the
    Patient compartment or not
    5. The EraseOperationTest fires AFTER the spec/example
    tests, so this instance of CompartmentUtil was loading the Patient
    CompartmentDefinition uploaded from fhir-examples in step number 2.
    
    To work around the issue, I updated our CompleteMockDataGenerator to
    omit the CompartmentDefinition.code CompartmentType and provide only an extension
    instead. I also had to add CompartmentDefinition cleanup for our migration
    tests since the previous/last releases had the CompartmentDefinition
    instance that causes the trouble.
    
    I also updated the CompleteMockDataGenerator to skip Period.start
    elements to avoid violating the `per-1` constraint.
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- build/migration/bin/2_previous-integration-test.sh
+++ build/migration/bin/2_previous-integration-test.sh
@@ -9,11 +9,11 @@
 set -ex
 
 run_tests(){
-    # The integration tests may be overriden completely, or fall through to the default. 
+    # The integration tests may be overriden completely, or fall through to the default.
     migration="${1}"
 
     if [ ! -z "${migration}" ] && [ -f "build/migration/${migration}/2_previous-integration-test.sh" ]
-    then 
+    then
         echo "Running [${migration}] previouos specific integration tests"
         bash ${WORKSPACE}/fhir/build/migration/${migration}/2_previous-integration-test.sh
     else
@@ -25,7 +25,7 @@ run_tests(){
         echo "fhir container are:"
         docker container inspect $(docker container ls | grep fhir_1 | awk '{print $NF}' ) | jq -r '.[]'
         echo ""
-    
+
         mkdir -p ${WORKSPACE}/fhir/build/migration/integration-test-results
         echo "Running Integration tests: "
         mvn -B test -f fhir-server-test -DskipWebSocketTest=true --no-transfer-progress \
@@ -34,8 +34,32 @@ run_tests(){
         echo "Done Running Tests"
         echo ""
 
-        # docker container logs "db2_fhir_1" 
+        # docker container logs "db2_fhir_1"
     fi
+}
+
+clean_up(){
+    # Remove problematic resources
+    fhir_base_url=https://localhost:9443/fhir-server/api/v4
+    problem_types=CompartmentDefinition
+    for r in $(curl -k -u fhiruser:change-password "${fhir_base_url}?_type=${problem_types}&_count=1000" | jq -r '.entry[].fullUrl'); do
+      echo "erasing ${r}"
+      curl -i -k -u fhiruser:change-password -H "Content-Type: application/json" ${r}'/$erase' -d '
+        {
+          "resourceType": "Parameters",
+          "parameter": [
+            {
+              "name": "patient",
+              "valueString": "test"
+            },
+            {
+              "name": "reason",
+              "valueString": "previous CompartmentDefinition instances from fhir-examples are bad"
+            }
+          ]
+        }'
+    done
+
 }
 
 ###############################################################################
@@ -48,6 +72,7 @@ cd "prev/"
 
 bash ${WORKSPACE}/fhir/build/common/wait_for_it.sh
 run_tests "${1}"
+clean_up
 
 # Reset to Original Directory
 popd > /dev/null
