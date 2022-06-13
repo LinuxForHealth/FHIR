758a42f535f37ee659718adaf61fcee84ecfef2f
Lee Surprenant <lmsurpre@us.ibm.com>
2022-06-13 13:17:20 -0400

    convert BPConstraintGeneratorTest main into a test
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-profile/src/test/java/com/ibm/fhir/profile/test/BPConstraintGeneratorTest.java
+++ fhir-profile/src/test/java/com/ibm/fhir/profile/test/BPConstraintGeneratorTest.java
@@ -7,12 +7,9 @@
 package com.ibm.fhir.profile.test;
 
 import java.util.List;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import com.ibm.fhir.model.annotation.Constraint;
 import com.ibm.fhir.model.resource.StructureDefinition;
@@ -21,34 +18,23 @@ import com.ibm.fhir.profile.ProfileSupport;
 import com.ibm.fhir.registry.FHIRRegistry;
 
 public class BPConstraintGeneratorTest {
+    private static final boolean DEBUG = true;
+
     @BeforeClass
     public void before() {
         FHIRRegistry.getInstance();
         FHIRRegistry.init();
     }
 
-    public static void main(String[] args) throws Exception {
-        Logger logger = Logger.getLogger(ConstraintGenerator.class.getName());
-        logger.setLevel(Level.FINEST);
-        Handler handler = new Handler() {
-            @Override
-            public void publish(LogRecord record) {
-                System.out.println(record.getMessage());
-            }
-
-            @Override
-            public void flush() {
-                System.out.flush();
-            }
-
-            @Override
-            public void close() throws SecurityException { }
-        };
-        handler.setLevel(Level.FINEST);
-        logger.addHandler(handler);
+    @Test
+    public void generateConstraints() throws Exception {
         StructureDefinition profile = ProfileSupport.getProfile("http://hl7.org/fhir/StructureDefinition/bp");
         ConstraintGenerator generator = new ConstraintGenerator(profile);
         List<Constraint> constraints = generator.generate();
-        constraints.forEach(System.out::println);
+        for (Constraint constraint : constraints) {
+            if (DEBUG) {
+                System.out.println(constraint.id() + "\t" + constraint.expression());
+            }
+        }
     }
 }
