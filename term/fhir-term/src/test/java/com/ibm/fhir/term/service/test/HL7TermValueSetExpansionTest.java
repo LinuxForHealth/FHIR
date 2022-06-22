deaff353e5bf9f86c1c4f7e30f17f8270a63a1bd
Lee Surprenant <lmsurpre@us.ibm.com>
2022-06-21 23:48:43 -0400

    issue #3448 - convert property-based concept hiearchies in getConcepts
    1. added `hasPropertyHierarchy` and `convertToSimpleCodeSystem`
    utilities to CodeSystemSupport. the conversion utility uses JGraphT to
    simplify the creation of an acyclic directed graph and then traverses
    that structure to build an updated CodeSystem resource with nested
    concepts. this allows the rest of our CodeSystemSupport to stay the
    same.
    
    2. call `convertToSimpleCodeSystem` from
    RegistryTermServiceProvider.getConcepts to convert property-based
    hierarchy into the more-common nested concept flavor so that we can use
    our existing CodeSystemSupport filter approach for CodeSystems in the
    registry
    
    3. added hand-crafted CodeSystem resources that test parent-to-child and
    child-to-parent edges with the conversion logic
    
    4. added tests to verify our updated expansions match the expansions
    from tx.fhir.org for select ValueSets that draw from polyhierarchical
    CodeSystems like v3-ActCode and v3-RoleCode
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

---
+++ term/fhir-term/src/test/java/com/ibm/fhir/term/service/test/HL7TermValueSetExpansionTest.java
@@ -0,0 +1,79 @@
+/*
+ * (C) Copyright IBM Corp. 2022
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.ibm.fhir.term.service.test;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.testng.annotations.Test;
+
+import com.ibm.fhir.model.format.Format;
+import com.ibm.fhir.model.parser.FHIRParser;
+import com.ibm.fhir.model.resource.ValueSet;
+import com.ibm.fhir.term.util.ValueSetSupport;
+
+public class HL7TermValueSetExpansionTest {
+    FHIRParser parser = FHIRParser.parser(Format.JSON);
+
+    /**
+     * Compare FHIRTermService ValueSet expansion to expanded ValueSets obtained from https://tx.fhir.org
+     *
+     * Note:  tx.fhir.org has seems to have some stale expansions. In all of the following cases, our expansion
+     * differed and I think our expansion is right for the version of the hl7.terminology package we're using (3.1.0).
+     *
+     * <ul>
+     * <li> ValueSet-parent-relationship-codes.json
+     * <li> ValueSet-v3-Confidentiality.json
+     * <li> ValueSet-v3-PurposeOfUse.json
+     * </ul>
+     */
+    @Test
+    void testHl7TermValueSetExpansion() throws Exception {
+        Path expandedValueSetsDir = Path.of("src/test/resources/tx-expanded-valuesets");
+
+        Set<Path> valueSetFiles = Files.list(expandedValueSetsDir).collect(Collectors.toSet());
+        for (Path path : valueSetFiles) {
+            try (Reader reader = Files.newBufferedReader(path)) {
+                ValueSet txExpandedValueSet = parser.parse(reader);
+                String url = txExpandedValueSet.getUrl().getValue();
+
+                // the version might not match, but these ValueSets shouldn't change much and so thats probably ok
+                ValueSet ourValueSet = ValueSetSupport.getValueSet(url);
+                ValueSet ourExpandedValueSet = ValueSetSupport.expand(ourValueSet);
+
+                ValueSet.Expansion ourExpansion = ourExpandedValueSet.getExpansion();
+                assertNotNull(ourExpansion);
+
+                Set<String> ourConcepts = new HashSet<>();
+                for (ValueSet.Expansion.Contains concept : ourExpandedValueSet.getExpansion().getContains()) {
+                    ourConcepts.add(concept.getSystem().getValue() + "|" + concept.getCode().getValue());
+                }
+
+                Set<String> theirConcepts = new HashSet<>();
+                for (ValueSet.Expansion.Contains concept : txExpandedValueSet.getExpansion().getContains()) {
+                    theirConcepts.add(concept.getSystem().getValue() + "|" + concept.getCode().getValue());
+                }
+
+                Set<String> diffSet;
+
+                diffSet = new HashSet<>(theirConcepts);
+                diffSet.removeAll(ourConcepts);
+                assertTrue(diffSet.isEmpty(), "all their codes are in our set: " + diffSet.toString());
+
+                diffSet = new HashSet<>(ourConcepts);
+                diffSet.removeAll(theirConcepts);
+                assertTrue(diffSet.isEmpty(), "all our codes are in their set: " + diffSet.toString());
+            }
+        }
+    }
+}
---
+++ term/fhir-term/src/test/java/com/ibm/fhir/term/service/test/PolyhierarchyExpansionTest.java
@@ -0,0 +1,96 @@
+/*
+ * (C) Copyright IBM Corp. 2022
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.ibm.fhir.term.service.test;
+
+import static com.ibm.fhir.term.util.ValueSetSupport.getValueSet;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertSame;
+import static org.testng.Assert.fail;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.ibm.fhir.model.resource.CodeSystem;
+import com.ibm.fhir.model.resource.CodeSystem.Concept;
+import com.ibm.fhir.model.resource.ValueSet;
+import com.ibm.fhir.model.resource.ValueSet.Expansion.Contains;
+import com.ibm.fhir.model.type.Code;
+import com.ibm.fhir.term.service.FHIRTermService;
+import com.ibm.fhir.term.util.CodeSystemSupport;
+
+public class PolyhierarchyExpansionTest {
+    @Test
+    void testConvertCodeSystemPropertiesToNested() {
+        CodeSystem codeSystem = CodeSystemSupport.getCodeSystem("http://ibm.com/fhir/CodeSystem/poly|1.0.0");
+
+        CodeSystem expand = CodeSystemSupport.convertToSimpleCodeSystem(codeSystem);
+        assertNotNull(expand);
+
+        List<Concept> concepts = expand.getConcept();
+        assertEquals(concepts.size(), 2);
+
+        // confirm that the same concept ("C") is referenced under both "letter" and "numeral"
+        Concept c1 = null;
+        Concept c2 = null;
+        for (Concept concept1 : concepts) {
+            if ("letter".equals(concept1.getCode().getValue())) {
+                for (Concept concept2 : concept1.getConcept()) {
+                    if ("uppercase".equals(concept2.getCode().getValue())) {
+                        c1 = concept2.getConcept().get(2);
+                    }
+                }
+            } else {
+                for (Concept concept2 : concept1.getConcept()) {
+                    if ("C".equals(concept2.getCode().getValue())) {
+                        c2 = concept2;
+                    }
+                }
+            }
+        }
+        assertSame(c1, c2);
+    }
+
+    @Test(expectedExceptions = IllegalStateException.class)
+    void testConvertCodeSystemPropertiesWithCycle() {
+        CodeSystem codeSystem = CodeSystemSupport.getCodeSystem("http://ibm.com/fhir/CodeSystem/poly|1.0.0");
+        // add a bogus concept that introduces a cycle
+        codeSystem = codeSystem.toBuilder()
+                .concept(CodeSystem.Concept.builder()
+                        .code(Code.of("test1"))
+                        .property(CodeSystem.Concept.Property.builder()
+                                .code(Code.of("subsumedBy"))
+                                .value(Code.of("test2"))
+                                .build())
+                        .build())
+                .concept(CodeSystem.Concept.builder()
+                        .code(Code.of("test2"))
+                        .property(CodeSystem.Concept.Property.builder()
+                                .code(Code.of("subsumedBy"))
+                                .value(Code.of("test1"))
+                                .build())
+                        .build())
+                .build();
+
+        CodeSystemSupport.convertToSimpleCodeSystem(codeSystem);
+        fail("Should have thrown but didn't");
+    }
+
+    @Test
+    void testExpandValueSetWithCodeSystemPolyhierarchy() {
+        ValueSet valueSet = getValueSet("http://ibm.com/fhir/ValueSet/poly|1.0.0");
+
+        ValueSet expand = FHIRTermService.getInstance().expand(valueSet);
+        assertNotNull(expand);
+        assertNotNull(expand.getExpansion());
+        assertNotNull(expand.getExpansion().getContains());
+
+        List<Contains> concepts = expand.getExpansion().getContains();
+
+        assertEquals(concepts.size(), 55); // 26 + 1("ch") + 1("lowercase") + 26 + 1("uppercase")
+    }
+}
