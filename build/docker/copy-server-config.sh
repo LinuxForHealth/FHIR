dd943dc16eb0f58f6796ebf387157202d1b0f027
Lee Surprenant <lmsurpre@merative.com>
2022-07-21 23:42:32 -0400

    issue #3676 - first pass at replacing db2 with postgres in the containerized e2e tests
    Signed-off-by: Lee Surprenant <lmsurpre@merative.com>

--- build/docker/copy-server-config.sh
+++ build/docker/copy-server-config.sh
@@ -12,43 +12,24 @@ if [[ -z "${WORKSPACE}" ]]; then
 fi
 
 echo "Removing the old server config..."
-CONFIG="${WORKSPACE}/build/docker/fhir-server/volumes/config"
-rm -rf $CONFIG/* 2> /dev/null
-mkdir -p $CONFIG
-
-BULKDATA="${WORKSPACE}/build/docker/fhir-server/volumes/bulkdata/"
-mkdir -p ${BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import.ndjson ${BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-skip.ndjson ${BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-neg.ndjson ${BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-mismatch.ndjson ${BULKDATA}
-
-S3_BULKDATA="${WORKSPACE}/build/docker/minio/miniodata/fhirbulkdata/"
-mkdir -p ${S3_BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import.ndjson ${S3_BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-skip.ndjson ${S3_BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-neg.ndjson ${S3_BULKDATA}
-cp ${WORKSPACE}/fhir-server-test/src/test/resources/testdata/import-operation/test-import-mismatch.ndjson ${S3_BULKDATA}
-
-# Appending the path
-echo "test.bulkdata.path = ${BULKDATA}" >> ${WORKSPACE}/fhir-server-test/src/test/resources/test.properties
+CONFIG="${WORKSPACE}/build/docker/fhir-server/config"
+rm -rf ${CONFIG}/* 2> /dev/null
+mkdir -p ${CONFIG}
 
 echo "Copying the server config files..."
 cp -r ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/config/* ${CONFIG}
 cp -r ${WORKSPACE}/fhir-server-webapp/src/test/liberty/config/config/* ${CONFIG}
-bash ${WORKSPACE}/build/update-server-registry-resource.sh ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/config/default/fhir-server-config-db2.json
-cp -r ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/config/default/fhir-server-config-db2.json ${CONFIG}/default/fhir-server-config.json
+bash ${WORKSPACE}/build/update-server-registry-resource.sh ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/config/default/fhir-server-config-postgresql.json
+cp -r ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/config/default/fhir-server-config-postgresql.json ${CONFIG}/default/fhir-server-config.json
 
 echo "Replacing datasource content in server configDropins..."
-OVERRIDES="${WORKSPACE}/build/docker/fhir-server/volumes/overrides"
+OVERRIDES="${WORKSPACE}/build/docker/fhir-server/configDropins/overrides"
+rm -rf ${OVERRIDES}/* 2> /dev/null
 mkdir -p ${OVERRIDES}
 
-# Just in case it already exists, let's wipe the datsource*.xml files
-rm -f ${OVERRIDES}/datasource*.xml
-
-# Copy over both the db2 (default_default) and derby (tenant1_*) datasource definitions
-cp ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/configDropins/disabled/datasource-db2.xml ${OVERRIDES}/
-cp ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/configDropins/disabled/db2/bulkdata.xml ${OVERRIDES}/
+# Copy over both the postgres (default_default) and derby (tenant1_*) datasource definitions
+cp ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/configDropins/disabled/datasource-postgresql.xml ${OVERRIDES}/
+cp ${WORKSPACE}/fhir-server-webapp/src/main/liberty/config/configDropins/disabled/postgres/bulkdata.xml ${OVERRIDES}/
 cp ${WORKSPACE}/fhir-server-webapp/src/test/liberty/config/configDropins/overrides/datasource-derby.xml ${OVERRIDES}/
 
 echo "Finished copying the server config."
