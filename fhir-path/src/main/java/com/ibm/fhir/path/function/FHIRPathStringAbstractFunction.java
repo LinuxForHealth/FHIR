bfdf4f5daf644fa79e59919b75666557e65afbe5
Prasanna Hegde <prasanna.hegde1@ibm.com>
2022-07-29 18:22:05 +0530

    issue #3805 - fixed extensions check

--- fhir-path/src/main/java/com/ibm/fhir/path/function/FHIRPathStringAbstractFunction.java
+++ fhir-path/src/main/java/com/ibm/fhir/path/function/FHIRPathStringAbstractFunction.java
@@ -13,6 +13,8 @@ import static com.ibm.fhir.path.util.FHIRPathUtil.isSingleton;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.ibm.fhir.path.FHIRPathNode;
 import com.ibm.fhir.path.FHIRPathType;
@@ -22,6 +24,8 @@ import com.ibm.fhir.path.evaluator.FHIRPathEvaluator.EvaluationContext;
  * Abstract class which contains common apply logic for all abstract string manipulation functions
  */
 public abstract class FHIRPathStringAbstractFunction extends FHIRPathAbstractFunction {
+    
+    private static final Logger logger = Logger.getLogger(FHIRPathStringAbstractFunction.class.getName());
     
     /**
      * @implSpec Common apply logic for all abstract string manipulation functions. Delegates to doApply()
@@ -36,7 +40,14 @@ public abstract class FHIRPathStringAbstractFunction extends FHIRPathAbstractFun
             
         } else if (!hasStringValue(context)) {
             FHIRPathNode node = getSingleton(context);
-            if (node.type() == FHIRPathType.FHIR_STRING) {
+            FHIRPathType nodeChildType = 
+                   node.children() != null && !node.children().isEmpty() 
+                     && node.children().iterator().hasNext() && node.children().iterator().next().type() != null ? node.children().iterator().next().type() : FHIRPathType.FHIR_ANY;
+                    
+            if (nodeChildType == FHIRPathType.FHIR_EXTENSION) {
+                if( logger.isLoggable(Level.FINE) ) {
+                    logger.fine(String.format("collection item is of extension type %s", node.type().getName()));
+                }
                 return empty();
             }
             throw new IllegalArgumentException("Input collection item must be of type String, but found '"+node.type().getName()+ "'");
