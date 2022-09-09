c74708f6ccd78ab498b69a80f63b0ef823c4ee51
Prasanna Hegde <prasanna.hegde1@ibm.com>
2022-09-09 19:09:10 +0530

    issue-3152 - changes for Update IG DaVinci Payer Data Exchange (PDex) US Drug Formulary | STU2
    Signed-off-by: Prasanna Hegde <prasanna.hegde1@ibm.com>

--- conformance/fhir-ig-davinci-pdex-formulary/src/test/java/org/linuxforhealth/fhir/ig/davinci/pdex/formulary/test/ConstraintGeneratorTest.java
+++ conformance/fhir-ig-davinci-pdex-formulary/src/test/java/org/linuxforhealth/fhir/ig/davinci/pdex/formulary/test/ConstraintGeneratorTest.java
@@ -11,6 +11,8 @@ import static org.linuxforhealth.fhir.path.util.FHIRPathUtil.compile;
 import org.testng.annotations.Test;
 
 import org.linuxforhealth.fhir.ig.davinci.pdex.formulary.Formulary101ResourceProvider;
+import org.linuxforhealth.fhir.ig.davinci.pdex.formulary.Formulary110ResourceProvider;
+import org.linuxforhealth.fhir.ig.davinci.pdex.formulary.Formulary200ResourceProvider;
 import org.linuxforhealth.fhir.model.annotation.Constraint;
 import org.linuxforhealth.fhir.model.resource.StructureDefinition;
 import org.linuxforhealth.fhir.model.type.Extension;
@@ -41,7 +43,26 @@ public class ConstraintGeneratorTest {
 
     @Test
     public static void test110ConstraintGenerator() throws Exception {
-        FHIRRegistryResourceProvider provider = new Formulary101ResourceProvider();
+        FHIRRegistryResourceProvider provider = new Formulary110ResourceProvider();
+        for (FHIRRegistryResource registryResource : provider.getRegistryResources()) {
+            if (StructureDefinition.class.equals(registryResource.getResourceType())) {
+                String url = registryResource.getUrl() + "|" + registryResource.getVersion();
+                System.out.println(url);
+                Class<?> type = ModelSupport.isResourceType(registryResource.getType()) ? ModelSupport.getResourceType(registryResource.getType()) : Extension.class;
+                for (Constraint constraint : ProfileSupport.getConstraints(url, type)) {
+                    System.out.println("    " + constraint);
+                    if (!Constraint.LOCATION_BASE.equals(constraint.location())) {
+                        compile(constraint.location());
+                    }
+                    compile(constraint.expression());
+                }
+            }
+        }
+    }
+    
+    @Test
+    public static void test200ConstraintGenerator() throws Exception {
+        FHIRRegistryResourceProvider provider = new Formulary200ResourceProvider();
         for (FHIRRegistryResource registryResource : provider.getRegistryResources()) {
             if (StructureDefinition.class.equals(registryResource.getResourceType())) {
                 String url = registryResource.getUrl() + "|" + registryResource.getVersion();
