395a6a00af2ff02bc70a9d7f5cda25c22e8cebe5
PrasannaHegde1 <109784806+PrasannaHegde1@users.noreply.github.com>
2022-10-25 19:04:13 +0530

    issue-3716 - validate securityContext for a Binary resource (#4034)
    * issue-3716 - validate securityContext for a Binary resource
    
    Signed-off-by: PrasannaHegde1 <phegde@merative.com>
    
    * issue-3716 - code clean up
    
    Signed-off-by: PrasannaHegde1 <phegde@merative.com>
    
    * issue-3590 - fixing review comments on configuration key and unit test failure validation.
    
    Signed-off-by: PrasannaHegde1 <phegde@merative.com>
    
    Signed-off-by: PrasannaHegde1 <phegde@merative.com>

--- fhir-smart/src/main/java/org/linuxforhealth/fhir/smart/AuthzPolicyEnforcementPersistenceInterceptor.java
+++ fhir-smart/src/main/java/org/linuxforhealth/fhir/smart/AuthzPolicyEnforcementPersistenceInterceptor.java
@@ -24,8 +24,10 @@ import java.util.stream.Stream;
 import javax.ws.rs.core.Response;
 
 import org.linuxforhealth.fhir.config.FHIRConfigHelper;
+import org.linuxforhealth.fhir.config.FHIRConfiguration;
 import org.linuxforhealth.fhir.config.FHIRRequestContext;
 import org.linuxforhealth.fhir.core.ResourceType;
+import org.linuxforhealth.fhir.model.resource.Binary;
 import org.linuxforhealth.fhir.model.resource.Bundle;
 import org.linuxforhealth.fhir.model.resource.Parameters;
 import org.linuxforhealth.fhir.model.resource.Parameters.Parameter;
@@ -445,6 +447,7 @@ public class AuthzPolicyEnforcementPersistenceInterceptor implements FHIRPersist
     public void beforeCreate(FHIRPersistenceEvent event) throws FHIRPersistenceInterceptorException {
         DecodedJWT jwt = JWT.decode(getAccessToken());
         enforce(event.getFhirResource(), getPatientIdFromToken(jwt), Permission.WRITE, getScopesFromToken(jwt));
+        validateBinarySecurityContext(event.getFhirResourceType(), event.getFhirResource());
     }
 
     @Override
@@ -463,6 +466,7 @@ public class AuthzPolicyEnforcementPersistenceInterceptor implements FHIRPersist
         // the user doesn't have access to
         enforce(event.getPrevFhirResource(), patientIdFromToken, Permission.READ, scopesFromToken);
         enforce(event.getFhirResource(), patientIdFromToken, Permission.WRITE, scopesFromToken);
+        validateBinarySecurityContext(event.getFhirResourceType(), event.getFhirResource());
     }
 
     @Override
@@ -1104,5 +1108,30 @@ public class AuthzPolicyEnforcementPersistenceInterceptor implements FHIRPersist
             }
         }
         return null;
+    }
+    
+    /**
+     * Validate if a FHIR Binary resource passed by the user for create/update operation has the securityContext field.
+     * If the securityContext field exists, throw an error or log a warning message based on the validateBinarySecurityContext configuration.
+     * By default, validateBinarySecurityContext is set to true and will throw an error if securityContext field is passed in the request.
+     * If validateBinarySecurityContext configuration is set to false, then log a warning message.
+     * @param resourceType the FHIR resource type.
+     * @param resource the FHIR Resource instance.
+     * @throws FHIRPersistenceInterceptorException when validateBinarySecurityContext configuration is set to true and securityContext field is passed in the request.
+     */
+    private void validateBinarySecurityContext(String resourceType, Resource resource) throws FHIRPersistenceInterceptorException {
+        if (resource instanceof Binary) {
+            Binary binaryResource = (Binary) resource;
+            if (binaryResource.getSecurityContext() != null &&
+                    binaryResource.getSecurityContext().getReference() != null) {
+                boolean validateBinarySecurityContext = FHIRConfigHelper.getBooleanProperty(FHIRConfiguration.PROPERTY_SECURITY_VALIDATE_BINARY_SECURITY_CONTEXT, true);
+                String msg = "securityContext is not supported for resource type " + resourceType;    
+                if (validateBinarySecurityContext) {
+                    throw new FHIRPersistenceInterceptorException(msg)
+                        .withIssue(FHIRUtil.buildOperationOutcomeIssue(msg, IssueType.FORBIDDEN));
+                }
+                log.warning(msg);
+            } 
+        }
     }
 }
