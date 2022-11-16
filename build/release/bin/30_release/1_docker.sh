c2f18378e8c1a2aaabe8f682649632cc668a9ae5
Lee Surprenant <lmsurpre@merative.com>
2022-11-15 21:39:52 -0500

    issue #4081 - add optional '--latest' flag to docker push script
    and only set it from the tag-release workflow
    
    Signed-off-by: Lee Surprenant <lmsurpre@merative.com>

--- build/release/bin/30_release/1_docker.sh
+++ build/release/bin/30_release/1_docker.sh
@@ -9,6 +9,14 @@
 # Assumes that fhir-install has already been built
 # and that the user is logged into the configured container registry.
 
+# If the '--latest' flag is passed, then tag the docker image as the latest when we push it later
+# Reminder: in bash true evaluates to '0' and false evaluates to '1'; that lets us use 'if $LATEST' below
+if [[ "$1" = "--latest" ]]; then
+  LATEST=true
+else
+  LATEST=false
+fi
+
 # Executes the build for the linuxforhealth/fhir-schematool.
 # This build runs first so that the FAILURE comes before and stops the subsequent builds/releases.
 export WORKSPACE=$(pwd)
@@ -29,6 +37,8 @@ df -h
 mvn -B dockerfile:build -f fhir-install --no-transfer-progress -Dmaven.wagon.http.retryHandler.count=3
 mvn dockerfile:tag@tag-version -f fhir-install
 mvn dockerfile:push@push-version -f fhir-install
-mvn dockerfile:push@push-latest -f fhir-install
 
+if $LATEST; then
+  mvn dockerfile:push@push-latest -f fhir-install
+fi
 # EOF
